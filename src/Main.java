c6ef8309e45dacb10cd6f29b4ec3c998b7ef544e
Name <thdgorhkd97@naver.com>
2021-09-26 23:36:11 +0900

    0926

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,102 @@
+import javax.swing.text.View;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
 
-    // programmers level 2 - 삼각 달팽이
-    // 아래 -> 오른쪽 -> 왼쪽 대각선 이 방향으로 계속 하여
-    // 1부터 증가하면서 수를 쓰는 건데
-    // 이동하는 기준에 해당할 때를 판별하는 게 상당히 번거롭다.
-    // while 문 속에서 기준을 판별해야 하기 때문에 해당 조건을 만족할 때 방향 전환
-    // 이런식으로 정리해야 할 것 같다.
-    // 한화시스템 코딩테스트를 봤는 데 bfs를 코딩에 적용하여 문제를 해결하는 것에 대해서 다시 해야 할 것 같다.
-    // bfs를 활용하는 방법부터 확실히 잡아야겠다.
-
+    // programmers level 2 - 게임 맵 최단거리
+    // 정확성 성공, 효율성 실패
+    // bfs를 문제에 적용하는 게 어려워서 구글링과 유튜브를 통해 공부하고 다시 해보았다.
+    // queue를 활용하여 이동가능한 방향을 큐에 넣고 방문처리를 하는 방식을 문제에 적용하였다.
+    // 다만 이동할 수 있는 모든 경우의 수를 구해서 하다 보니 효율성은 떨어진 것 같다.
+    // 내일도 효율성에 대해서만 고민하는 것이 아니라 처음부터 다시 해봐야겠다.
 
     public static void main(String[] args) {
-        int n = 4;
 
-        int[][] arr = new int[n][n];
-
-        int max = 0;
-
-        for(int i=1;i<=n;i++) {
-            max += i;
-        }
-
-        int[] answer = new int[max];
+        int[][] maps = {
+                {1,0,1,1,1},
+                {1,0,1,0,1},
+                {1,0,1,1,1},
+                {1,1,1,0,1},
+                {0,0,0,0,1}
+        };
 
 
-        for (int i = 0; i < n; i++) { // 모두 -1로 세팅
-            for (int j = 0; j <= i; j++) {
-                arr[i][j] = -1;
-            }
-        }
 
-        int i = 0, j = 0, k = 1;
+        boolean[][] visited = new boolean[maps.length+1 ][maps[0].length+1];
+        int answer = 0;
 
-        arr[i][j] = k;
+        answer = bfs(0,0,visited,maps);
 
-        /*
+        System.out.println(answer);
 
-        while (k < max) {
-            while (i + 1 < n && k < max && arr[i + 1][j] < 0) {
-                arr[++i][j] = ++k;
-            }
-
-            while (j + 1 < n && k < max && arr[i][j + 1] < 0) {
-                arr[i][++j] = ++k;
-            }
-
-         */
 
     }
 
+    public static int bfs(int y, int x, boolean[][] visited, int[][] maps){
+        Queue<Integer> qy = new LinkedList<>();
+        Queue<Integer> qx = new LinkedList<>();
+        Queue<Integer> qc = new LinkedList<>();
+
+        qy.offer(y);
+        qx.offer(x);
+        qc.offer(1);
+        visited[x][y] = true;
+        int cnt = -1;
+
+        while(!qy.isEmpty()){
+            int cy = qy.poll();
+            int cx = qx.poll();
+            int cc = qc.poll();
+
+            visited[cy][cx] = true;
+
+            if(cy == maps.length-1 && cx == maps[0].length-1) {
+                cnt = cc;
+                break;
+            }
+
+            // 상
+            if(can_move(cy-1,cx,maps,visited)) {
+//                System.out.println((cy-1) + " , " + cx + " 로 위로 이동 ");
+                qy.offer(cy - 1);
+                qx.offer(cx);
+                qc.offer(cc + 1);
+            }
+            // 하
+            if(can_move(cy+1,cx,maps,visited)){
+//                System.out.println((cy+1) + " , " + cx + " 로 아래로 이동 ");
+                qy.offer(cy+1);
+                qx.offer(cx);
+                qc.offer(cc+1);}
+            // 좌
+            if(can_move(cy,cx-1,maps,visited)) {
+
+//                System.out.println((cy) + " , " + (cx-1) + " 로 왼쪽으로 이동 ");
+                qy.offer(cy);
+                qx.offer(cx - 1);
+                qc.offer(cc + 1);
+            }
+            // 우
+            if(can_move(cy,cx+1,maps,visited)) {
+
+//                System.out.println((cy) + " , " + (cx+1) + " 로 오른쪽으로 이동 ");
+                qy.offer(cy);
+                qx.offer(cx+1);
+                qc.offer(cc+1);
+            }
+        }
 
 
+        return cnt;
+    }
 
+    public static boolean can_move(int y, int x, int[][] maps,boolean[][] visited){
+        if(y<0 || y >= maps.length || x <0 || x >= maps[0].length) return false;
+        if(maps[y][x] == 0) return false;
+        if(visited[y][x] == true) return false;
+        return true;
+    }
 }

6d7d988d5b21968535e0257e3da3668b9393b3c9
Name <thdgorhkd97@naver.com>
2021-09-27 23:44:16 +0900

    0927

--- src/Main.java
+++ src/Main.java
@@ -6,97 +6,69 @@ import java.util.Scanner;
 
 public class Main {
 
-    // programmers level 2 - 게임 맵 최단거리
-    // 정확성 성공, 효율성 실패
-    // bfs를 문제에 적용하는 게 어려워서 구글링과 유튜브를 통해 공부하고 다시 해보았다.
-    // queue를 활용하여 이동가능한 방향을 큐에 넣고 방문처리를 하는 방식을 문제에 적용하였다.
-    // 다만 이동할 수 있는 모든 경우의 수를 구해서 하다 보니 효율성은 떨어진 것 같다.
-    // 내일도 효율성에 대해서만 고민하는 것이 아니라 처음부터 다시 해봐야겠다.
+    // N사 코딩테스트 3번 문제
+    // 선두에 선 트럭은 a만큼 뒤따르는 트럭은 b만큼 연료를 소비할 때 가장 오래 달릴 수 있는 시간 구하기
+    // while문에서 for문을 4번이나 돌린 게 맘에 들지 않는다
+    // 1. 최대값 구하기 -> 선두에서 가장 많은 연료를 소모할 트럭
+    // 2. 전체 트럭중에서 선두 트럭은 a만큼 나머지는 b만큼 연료를 뺀다
+    // 3. (종료 조건) 뒤따르는 트럭 중 b만큼의 연료가 없는지 확인
+    // 4. (종료 조건) 선두에서 a만큼 연료를 소모할 수 있는 트럭이 없는지 확인
+    // 이렇게 while문에서 for문으로 확인하였는데 테스트 케이스에는 짧은 것 밖에 없어서 잘 나왔지만
+    // 문제의 조건에서 최대길이가 짧은 편이 아니었던 걸로 기억하는 데 효율성이 괜찮을지 걱정이다.
+    // 특히 3,4번은 종료조건을 보는데다가 for문의 범위도 일치했는데 이를 잘 묶어서 코드를 간결하게 만들 수 있지 않았을까하는 아쉬움도 있다.
 
     public static void main(String[] args) {
 
-        int[][] maps = {
-                {1,0,1,1,1},
-                {1,0,1,0,1},
-                {1,0,1,1,1},
-                {1,1,1,0,1},
-                {0,0,0,0,1}
-        };
+        int[] v = {4,5,5};
+        int a = 2;
+        int b = 1;
 
-
-
-        boolean[][] visited = new boolean[maps.length+1 ][maps[0].length+1];
         int answer = 0;
+        boolean flag = true;
 
-        answer = bfs(0,0,visited,maps);
+        while( flag ){
 
-        System.out.println(answer);
+            int max = 0;
+            int max_idx = 0;
+            for(int i=0;i<v.length;i++){
+                if(max < v[i]){
+                    max_idx = i;
+                    max = v[i];
+                }
+            }
+
+            for(int i=0;i<v.length;i++){
+                if(max_idx == i){
+                    v[i] = v[i] - a;
+                }
+                else{
+                    v[i] = v[i] - b;
+                }
+            }
 
 
-    }
+            answer++;
 
-    public static int bfs(int y, int x, boolean[][] visited, int[][] maps){
-        Queue<Integer> qy = new LinkedList<>();
-        Queue<Integer> qx = new LinkedList<>();
-        Queue<Integer> qc = new LinkedList<>();
+            for(int i=0;i<v.length;i++){
+                if(v[i] < b){
+                    flag = false;
+                    break;
+                }
+            }
 
-        qy.offer(y);
-        qx.offer(x);
-        qc.offer(1);
-        visited[x][y] = true;
-        int cnt = -1;
+            boolean first = false;
+            for(int i=0;i<v.length;i++){
+                if(v[i] >= a){
+                    first = true;
+                    break;
+                }
+            }
 
-        while(!qy.isEmpty()){
-            int cy = qy.poll();
-            int cx = qx.poll();
-            int cc = qc.poll();
-
-            visited[cy][cx] = true;
-
-            if(cy == maps.length-1 && cx == maps[0].length-1) {
-                cnt = cc;
+            if( !flag || !first){
                 break;
             }
-
-            // 상
-            if(can_move(cy-1,cx,maps,visited)) {
-//                System.out.println((cy-1) + " , " + cx + " 로 위로 이동 ");
-                qy.offer(cy - 1);
-                qx.offer(cx);
-                qc.offer(cc + 1);
-            }
-            // 하
-            if(can_move(cy+1,cx,maps,visited)){
-//                System.out.println((cy+1) + " , " + cx + " 로 아래로 이동 ");
-                qy.offer(cy+1);
-                qx.offer(cx);
-                qc.offer(cc+1);}
-            // 좌
-            if(can_move(cy,cx-1,maps,visited)) {
-
-//                System.out.println((cy) + " , " + (cx-1) + " 로 왼쪽으로 이동 ");
-                qy.offer(cy);
-                qx.offer(cx - 1);
-                qc.offer(cc + 1);
-            }
-            // 우
-            if(can_move(cy,cx+1,maps,visited)) {
-
-//                System.out.println((cy) + " , " + (cx+1) + " 로 오른쪽으로 이동 ");
-                qy.offer(cy);
-                qx.offer(cx+1);
-                qc.offer(cc+1);
-            }
         }
+        System.out.println(answer);
 
-
-        return cnt;
-    }
-
-    public static boolean can_move(int y, int x, int[][] maps,boolean[][] visited){
-        if(y<0 || y >= maps.length || x <0 || x >= maps[0].length) return false;
-        if(maps[y][x] == 0) return false;
-        if(visited[y][x] == true) return false;
-        return true;
     }
 }

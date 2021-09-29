d33b9a9368ae1b877d87579cd11eaa43e7d68d2c
Name <thdgorhkd97@naver.com>
2021-09-29 23:52:56 +0900

    0929

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,47 @@
 import javax.swing.text.View;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    // programmers 위클리 코드 8주차
-    // 직사각형의 사이즈의 배열을 가지고 모든 사이즈를 커버할 수 있는 사이즈를 구한다.
-    // 명함을 돌리는 게 가능하기 때문에 긴 쪽을 sizes[i][0]으로 놔두고
-    // sizes[i][0] 중의 최대값과 sizes[i][1]의 최대값을 구한다.
+    // 무한히 많은 회의실에서 회의한 시간이 주어질 때, 가장 많은 회의실이 사용된 시간을 모두 구하라
+    // 무한히 많은 회의실이라는 문제 조건이 있었는 데 회의실에서의 회의 시간인 info[][] 가 행이 10000이하라는
+    // 문제 조건이 있어서 회의실을 10001개라고 잡고 풀었는데 이게 문제 조건을 활용한 건지, 무시한 건지 애매하다.
+    // 문제 자체는 그리 어렵지 않았는데 무한한 회의실을 문제의 조건때문에 10000개로 한정했다는 점과
+    // 회의시간만큼 모두 더했다는 점에서 조금 석연찮은 부분이 있다.
+
 
     public static void main(String[] args) {
 
-        int[][] sizes = {{60,50},{30,70},{60,30},{80,40}};
+        int[][] info = {{3,5},{1,8},{5,14}};
 
-        int answer = 0;
+        int[] meeting = new int[10001];
 
-        int max = 0;
-        for(int i=0;i< sizes.length;i++){
-            int tmp = 0;
-            if(sizes[i][0] <= sizes[i][1]){ // 긴쪽을 sizes[i][0]으로 놔둔다
-                tmp = sizes[i][0];
-                sizes[i][0] = sizes[i][1];
-                sizes[i][1] = tmp;
-            }
-
-            if(max < sizes[i][0]){
-                max = sizes[i][0];
-            }
-
-        } // 최대값을 구한다 반드시 한쪽길이는 최대길이가 되야 하니까
-
-        int max2 = 0;
-        for(int i=0;i<sizes.length;i++){ // 명함의 짧은 쪽인 sizes[i][1] 중에서 최대값을 구한다.
-            if(max2 < sizes[i][1]){
-                max2 = sizes[i][1];
+        for(int i=0;i<info.length;i++){
+            for(int j=info[i][0];j<=info[i][1];j++){
+                meeting[j] += 1;
             }
         }
 
-        answer = max * max2; // answer = 모든 명함을 커버할 수 있는 지갑
+        int max = 0;
+        meeting[0] = max;
+
+        for(int i=1;i<10001;i++){
+            if(max < meeting[i]){
+                max = meeting[i];
+            }
+        }
+
+        ArrayList<Integer> max_meeting = new ArrayList<>();
+        for(int i=0;i<meeting.length;i++){
+            if(meeting[i] == max){
+                max_meeting.add(i);
+            }
+        }
+
+        int[] answer = new int[max_meeting.size()];
+        for(int i=0;i<answer.length;i++){
+            answer[i] = max_meeting.get(i);
+        }
+
     }
 }

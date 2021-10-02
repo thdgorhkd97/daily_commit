54e0acc408257d729475d11850c8b265f4a62be2
Name <thdgorhkd97@naver.com>
2021-10-02 23:36:45 +0900

    1002

--- src/Main.java
+++ src/Main.java
@@ -3,61 +3,60 @@ import java.util.*;
 
 public class Main {
 
-    // programmers level 2 - 삼각 달팽이
-    // 아래 오른쪽 왼쪽위 이 규칙에 따라서 방향이 전환되고 그에 따라 n에서부터 숫자가 달라지는 걸 알고 나서 풀렸다
-    // n = 4이면 아래 오른쪽 왼쪽위 아래 이렇게 4번 방향이 전환되고
-    // 아래일 때 4 오른쪽일때 3 왼쪽위일때 2 아래 1 이렇게
-    // n에서부터 1씩 감소하며 숫자를 쓰기 때문이다.
-    // n X n 행렬에 규칙에 따라 채우고 채워진 걸 answer행렬로 옮긴다.
+    // programmers level 2 - 쿼드 압축 후 개수 세기
+    // 분할 정복이라는 방법을 활용해야 했다.
+    // 배열을 4로 나누고 각 구역을 나눠가면서 1칸만 남을때까지 계속한다.
+    // 와중에 각 구역에 0이나 1로만 이뤄졌는지 확인한다.
+    // 이때, 0이나 1로만 이루어진걸 어떻게 해야 하는지 몰랐는데 그냥
+    // 정답배열에다가 1을 더하고 기본배열 자체에는 손을 대지 않는 방법이 있다.
 
     public static void main(String[] args) {
 
-        int n = 5;
+        int[][] arr = {{1,1,0,0},{1,0,0,0},{1,0,0,1},{1,1,1,1}};
 
-        int max = 0;
-        for(int i=n;i>0;i--){
-            max += i;
-        }
+        int[] answer = new int[2];
+        quad(arr.length, 0, 0, arr, answer);
 
-        int[] answer = new int[max];
-
-        int[][] map = new int[n][n];
-
-        int x = 0, y = -1;
-        int num = 1;
-
-        for(int i=0;i<n;i++){ // 방향전환이 n번 이루어짐
-            for(int j=i;j<n;j++){ // 방향으로 n개의 숫자만큼 있다.
-                // n = 4면 방향 전환 4번, 방향 전환때마다 4 3 2 1
-                if(i % 3 == 0){ // 아래로 이동
-                    y++;
-                }
-                else if(i % 3 == 1){ // 오른쪽 이동
-                    x++;
-                }
-                else{ // 왼쪽 대각선
-                    y--;
-                    x--;
-                }
-                map[y][x] = num++;
-            }
-        }
-
-        int idx = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(map[i][j] != 0){
-                    answer[idx++] = map[i][j];
-                }
-            }
-        }
-
-        for(int i=0;i<max;i++){
-            System.out.print(answer[i] + " ");
-        }
-
-
-
+        System.out.println(answer[0] + " " + answer[1]);
     }
 
-}
+
+        public static void quad ( int length, int y, int x, int[][] arr, int[] answer){
+            if (length == 1) {
+                if (arr[y][x] == 1) {
+                    answer[1]++;
+                } else {
+                    answer[0]++;
+                }
+
+                return;
+            }
+
+            if (isZip(length, y, x, arr, answer)) return ;
+
+            quad(length / 2, y, x, arr, answer);
+            quad(length / 2, y + length / 2, x, arr, answer);
+            quad(length / 2, y, x + length / 2, arr, answer);
+            quad(length / 2, y + length / 2, x + length / 2, arr, answer);
+        }
+
+        public static boolean isZip ( int length, int y, int x, int[][] arr, int[] answer){
+            int number = arr[y][x];
+
+            for (int i = y; i < y+length; i++) {
+                for (int j = x; j < x+length; j++) {
+                    if (number != arr[i][j]) {
+                        return false;
+                    }
+                }
+            }
+
+            if (arr[y][x] == 1) {
+                answer[1]++;
+            } else {
+                answer[0]++;
+            }
+            return true;
+
+        }
+    }

b7e0e81b6eefe5cf09c86843170f7b13f860f712
Name <thdgorhkd97@naver.com>
2021-10-05 22:28:55 +0900

    1005

--- src/Main.java
+++ src/Main.java
@@ -3,56 +3,92 @@ import java.util.*;
 
 public class Main {
 
-    // 알고리즘 - bfs 실습
-    // bfs 실습문제가 보여서 bfs를 구현하였다.
-    // 단, 미리 행렬을 만들어놓은게 아니라 입력을 받아서 실행한다.
-    // 한 번에 구현할 수 있을 줄 알았는데 bfs 함수내에서
-    // for문의 범위를 graph의 길이만큼으로 했어야 하는데 num으로 했었다.
-    // 이 부분이 아쉬웠다.
-
+    // programmers level 2 - 전력망을 둘로 나누기
+    // 트리구조에 대해서 최대한 개수를 비슷하게 나눠야 하는데
+    // 가장 많이 연결된 정점에 대해서 하나씩 간선을 끊어가면서
+    // 개수를 확인하려고 했는데
+    // 정점과 직접적으로 연결된 정점은 확인가능한데
+    // 연결 연결 이런식인게 조금 어렵다....
 
     public static void main(String[] args) {
 
+        int n = 9;
+        int[][] wires = {{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},
+                {7,8},{7,9}};
 
-        Scanner sc = new Scanner(System.in);
+        int answer = n;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int s = sc.nextInt();
+        int[] many = new int[n+1];
 
-        int[][] graph = new int[n+1][n+1];
-
-        for(int i=0;i<m;i++){
-            int edge1 = sc.nextInt();
-            int edge2 = sc.nextInt();
-
-            graph[edge1][edge2] = 1;
-            graph[edge2][edge1] = 1;
+        for(int i=0;i<wires.length;i++){
+            many[wires[i][0]]++;
+            many[wires[i][1]]++;
         }
 
-        boolean[] visited = new boolean[n+1];
+        int max = 0;
+        int max_idx = 0;
+        for(int i=0;i<many.length;i++){
+            if(max < many[i]){
+                max_idx = i;
+                max = many[i];
+            }
+        }
 
-        bfs(s,visited,graph);
+        if(max_idx == 0){
+            max_idx = many.length / 2;
+        }
+
+        System.out.println(max + " " + max_idx);
+
+        for(int i=0;i<wires.length;i++){
+            if(wires[i][0] == max_idx || wires[i][1] == max_idx){
+                cut(answer,wires,i);
+            }
+        }
+
+        System.out.println(answer);
+
 
     }
 
-    public static void bfs(int start,boolean[] visited,int[][] graph){
+    public static void cut(int answer, int[][] wires, int i){
+        int left = wires[i][0];
+        int right = wires[i][1];
+        int left_cnt =0;
+        int right_cnt=0;
 
-        visited[start] = true;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+        ArrayList<Integer> left_chain = new ArrayList<>();
+        ArrayList<Integer> right_chain = new ArrayList<>();
 
-        while(!q.isEmpty()){
-            int num = q.poll();
-            System.out.println(num);
+        for(int j=0;j<wires.length;j++){
+            if(wires[j][0] == left){
+                left_chain.add(wires[j][1]);
+            }
+            else if(wires[j][1] == left){
+                left_chain.add(wires[j][0]);
+            }
 
-            for(int i=1;i< graph.length; i++){ // <- i의 범위를 <= num 으로 했었다
-                if(!visited[i] && graph[num][i] == 1){
-                    q.add(i);
-                    visited[i] = true;
-                }
+            if(wires[j][0] == right){
+                right_chain.add(wires[j][1]);
+            }
+            else if(wires[j][1] == right){
+                right_chain.add(wires[j][0]);
             }
         }
+
+        System.out.println(left + " 일때 " + left_chain.size() + " ");
+
+        for(int j=0;j<left_chain.size();j++){
+            System.out.println(" "+left_chain.get(j));
+        }
+        System.out.println();
+        System.out.print(right + " 일때 " + right_chain.size());
+        for(int j=0;j<right_chain.size();j++){
+            System.out.print(" "+right_chain.get(j));
+        }
+
+        answer = Math.min(answer,Math.abs(left_cnt-right_cnt));
+
 
     }
 }

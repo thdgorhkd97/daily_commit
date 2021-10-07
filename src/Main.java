1327fb42d94bba5f4a886c8cc14eea478ff5d39c
Name <thdgorhkd97@naver.com>
2021-10-07 22:26:25 +0900

    1007

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,63 @@
 import javax.swing.text.View;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
 
-    // programmers level 2 - N진수 게임
-    // while문에서 String에 문자를 붙일 때 + 쓰기 싫었는데 (시간이 오래 걸리므로)
-    // 그때그때 하나씩 붙이는 걸 StringBuffer로 하기 어려워서
-    // 구현에 초점을 두고 +로 구현하였다.
-    // t * m + p (미리 구할 숫자 개수 * 참여 인원) + 튜브의 순서
-    // 까지만 문자열을 구하면 되기 때문에 제한 조건을 두고 구한다.
+    // programmers 월간 코드 챌린지 2
+    // long 의 범위를 int 형 배열에 넣으려다 메모리 초과가 나는데
+    // int 형 배열이 고정이다 보니 이를 어떻게 해결해야 할 지 모르겠다
+    // long - long 의 크기 만큼 int 형 배열에 집어 넣어야 하는데
+    // 그래서 BigInteger를 고려했지만 이 역시 int형으로 고정된 answer 배열에
+    // 들어가지 않는다..
 
 
     public static void main(String[] args) {
-        int n = 2; // 진법
-        int t = 4; // 미리 구할 숫자 개수
-        int m = 2; // 참여 인원
-        int p = 1; // 튜브의 순서
 
-        String str = "0";
-        int count = 0;
+        int n = 3;
+        long left = 2;
+        long right = 5;
 
-        while (str.length() < (t * m + p)) {
-            String byNum = "";
-            int num = count++;
+        int[][] map = new int[n][n];
 
-            while (num != 0) {
-                if (num % n >= 10)
-                    byNum += String.valueOf((char) (num % n + 55));
-                else
-                    byNum += String.valueOf(num % n);
+        map[0][0] = 1;
 
-                num /= n;
+        int idx=2;
+        while(idx != n+1){
+            for(int i=0;i<idx;i++){
+                for(int j=0;j<idx;j++){
+                    if(map[i][j] == 0){
+                        map[i][j] = idx;
+                    }
+                }
             }
 
-            str += new StringBuffer(byNum).reverse().toString();
+            idx++;
         }
 
-        String answer = "";
+        ArrayList<Long> arr = new ArrayList<>();
+        ArrayList<Long> arr2 = new ArrayList<>();
 
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < t; i++)
-            sb.append(String.valueOf(str.charAt(m * i + p - 1)));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr.add((long)map[i][j]);
+            }
+        }
+        BigInteger[] answer = new BigInteger[(int) (right - left + 1)];
 
-        answer = sb.toString();
+        idx = 0;
+        for(long i=left;i<=right;i++){
+            arr2.add(arr.get((int) i));
+        }
 
-        System.out.println(answer);
-        //return answer
+        for(int i=0;i<arr2.size();i++){
+            answer[idx++] = BigInteger.valueOf((arr2.get(i)));
+        }
+
+        for(int i=0;i<answer.length;i++){
+            System.out.print(answer[i] + " ");
+        }
+
 
     }
 }

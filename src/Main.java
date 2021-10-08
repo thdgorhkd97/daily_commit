70521d55d66d698f549a3e7eaad8395fcc811aea
Name <thdgorhkd97@naver.com>
2021-10-08 22:39:59 +0900

    1008

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,72 @@
 import javax.swing.text.View;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
 
-    // programmers 월간 코드 챌린지 2
-    // long 의 범위를 int 형 배열에 넣으려다 메모리 초과가 나는데
-    // int 형 배열이 고정이다 보니 이를 어떻게 해결해야 할 지 모르겠다
-    // long - long 의 크기 만큼 int 형 배열에 집어 넣어야 하는데
-    // 그래서 BigInteger를 고려했지만 이 역시 int형으로 고정된 answer 배열에
-    // 들어가지 않는다..
-
+    // programmers - 복서 정렬하기
+    // 승률이나 무거운 상대와 이긴 횟수 등 정렬에 필요한 기준을 구하는 건 했는데
+    // 처음에는 이를 비교하기 위해 if-else문을 사용하려다가
+    // 너무 코드가 복잡해져서 comparator 문을 공부해서 정렬하였다.
+    // o2 - o1 은 내림차순 o1 - o2 는 올림차순
 
     public static void main(String[] args) {
 
-        int n = 3;
-        long left = 2;
-        long right = 5;
+        int[] weights = {50,82,75,120};
 
-        int[][] map = new int[n][n];
+        int[][] boxer = new int[weights.length][4];
 
-        map[0][0] = 1;
+        String[] head2head = {"NLWL","WNLL","LWNW","WWLN"};
 
-        int idx=2;
-        while(idx != n+1){
-            for(int i=0;i<idx;i++){
-                for(int j=0;j<idx;j++){
-                    if(map[i][j] == 0){
-                        map[i][j] = idx;
+        int[] VS_heavy = new int[head2head.length];
+
+        for(int i=0;i< weights.length;i++){
+            int cnt_win = 0;
+            int cnt_winVS_heavy = 0;
+            for(int j=0;j< head2head[i].length();j++){
+                if(head2head[i].charAt(j) == 'W'){
+                    cnt_win++;
+                    if(weights[i] < weights[j]){
+                        cnt_winVS_heavy++;
                     }
                 }
             }
+            VS_heavy[i] = cnt_winVS_heavy;
 
-            idx++;
+            boxer[i][0] = i+1;
+            boxer[i][1] = weights[i];
+            boxer[i][2] = (int) ((1.0 * cnt_win/head2head[i].length()) * 100);
+            boxer[i][3] = VS_heavy[i];
+
+
         }
 
-        ArrayList<Long> arr = new ArrayList<>();
-        ArrayList<Long> arr2 = new ArrayList<>();
+        int[] answer = new int[weights.length];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr.add((long)map[i][j]);
+        Arrays.sort(boxer, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[2] != o2[2]) return o2[2] - o1[2];
+                if(o1[3] != o2[3]) return o2[3] - o1[3];
+                if(o1[1] != o2[1]) return o2[1] - o1[1];
+
+
+                return o1[0] - o2[0];
+
             }
-        }
-        BigInteger[] answer = new BigInteger[(int) (right - left + 1)];
+        });
 
-        idx = 0;
-        for(long i=left;i<=right;i++){
-            arr2.add(arr.get((int) i));
-        }
 
-        for(int i=0;i<arr2.size();i++){
-            answer[idx++] = BigInteger.valueOf((arr2.get(i)));
+        for(int i=0;i< answer.length;i++){
+            //System.out.println(boxer[i][0] + " " + boxer[i][1] + " " + boxer[i][2] + " " + boxer[i][3]);
+            answer[i] = boxer[i][0];
+            System.out.println(answer[i]);
         }
 
-        for(int i=0;i<answer.length;i++){
-            System.out.print(answer[i] + " ");
-        }
+
+
+
 
 
     }

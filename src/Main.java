584d7cd6eaf7c8d4dcc4859d9b74224a0a5c8a3b
Name <thdgorhkd97@naver.com>
2021-10-11 23:44:50 +0900

    1011

--- src/Main.java
+++ src/Main.java
@@ -3,50 +3,46 @@ import java.util.*;
 public class Main {
 
     // programmers level 2 - 튜플
-    // 46.7 / 100
-    // 문자열을 잘라서 배열로 넣는 과정까지는 잘 구현했는데
-    // 그걸 길이별로 정렬해야 한다는 생각을 못해서 조금 버벅였고
-    // 길이순으로 정렬해서 뒤의 것중에서 앞의 인덱스에 없는 것을
-    // 넣는 로직으로 구현했는데 테스트 케이스는 통과하는데
-    // 정확성에서 조금 부족하다.
+    // 로직을 처음부터 다시 구현해 보았다.
+    // ","를 기준으로 자르는 것이 아니라 애초에 "},{" 를 기준으로
+    // 자르니까 배열 기준으로 자를 수 있었다.( 이부분은 참고하였다)
+    // 그리고 길이 순으로 정렬한 다음에
+    // 앞에 포함되지 않는다면 정답에 추가한다.
 
     public static void main(String[] args) {
 
-        String s = "{{20,111},{111}}";
+        String s = "{{2},{2,1,3},{2,1},{2,1,3,4}}";
 
-        String[] strs = s.replaceAll("[{}]", " ").trim().split(" , ");
+        s = s.substring(2,s.length()-2).replace("},{","/");
 
-        Arrays.sort(strs, new Comparator<String>() {
+        System.out.println(s);
+
+        String[] arr = s.split("/");
+        Arrays.sort(arr, new Comparator<String>() { // 길이순으로 정렬
             @Override
             public int compare(String o1, String o2) {
-                return o1.length()-o2.length();
+                return o1.length() - o2.length();
             }
         });
 
-        ArrayList<String> str = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(Integer.valueOf(arr[0]));
+        for(int i=0;i<arr.length;i++){
+            String[] str = arr[i].split(","); // 숫자별로 나눔
 
-        str.add(strs[0]);
+            for(int j=0;j<str.length;j++){ // 숫자를 하나씩 체크하면서
+                int num = Integer.valueOf(str[j]);
 
-        for(int i=1;i<strs.length;i++){
-
-            String[] list;
-            list = strs[i].split(",");
-
-            for(int j=0;j<list.length;j++){
-                if(!strs[i-1].contains(list[j])){
-                    str.add(list[j]);
+                if(!list.contains(num)){ // 앞에 포함되지 않는다면
+                    list.add(num);
                     break;
                 }
             }
         }
 
-        int[] answer = new int[str.size()];
-        for(int i=0;i<str.size();i++){
-            answer[i] = Integer.valueOf(str.get(i));
-        }
-
-        for(int i=0;i<answer.length;i++){
-            System.out.println(answer[i]);
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
         }
 
 

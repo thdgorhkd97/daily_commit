a79e7d79bcc8c494a1a39de3f23d891bf67cba2a
Name <thdgorhkd97@naver.com>
2021-10-21 22:50:49 +0900

    1021

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,76 @@ import java.util.*;
 
 public class Main {
 
-    // programmers level 2 - 멀쩡한 사각형
-    // 코드는 매우 간단하지만 공식을 구하는 게 어려웠다.
-    // w와 h가 주어질 때 대각선으로 관통하는 직선을 그어서 멀쩡한 사각형을 구하는 문제였다.
-    // 최대공약수를 활용해야 하는 건 알았는데 모든 상황에서 활용될 수 있는 공식을 구하기
-    // 어려워서 뭔가 조금씩 오류가 나서 풀이법을 조금 찾아보고 해결했다.
-    // (전체 크기) - (한 패턴 직사각형 당 사용하지 못하는 정사각형 크기 * 반복횟수)
-    // 패턴 직사각형은 가로로 2 세로로 3인 직사각형을 의미하는데 이렇게 반복되는
-    // 최소 크기를 고려하는 식으로 생각했어야 하는 것 같다.
+    // N사 코딩테스트 예시문제
+    // 행렬의 영역을 구해서 오름차순으로 정렬하는 문제
+    // bfs를 활용해서 문제를 해결하였다.
+    // bfs 함수에서 해당 좌표의 상하좌우를 확인하는 부분을 조금 중복되게 코드를 짠 것 같은데
+    // 이 부분을 함수로 빼면 조금 더 코드가 간결해질 것 같다.
+    // 붙어있는 부분을 재귀를 통해 bfs를 계속 돌리면서 상하좌우로 연결된 영역을 확인하고
+    // 그 연결이 끊긴다면 영역크기를 재는 변수를 1로 초기화하여
+    // 다른 영역의 크기를 잰다.
+
+    static int cnt = 1;
 
     public static void main(String[] args) {
 
-        int w = 8;
-        int h = 12;
+        int n = 6;
+        ArrayList<Integer> list = new ArrayList<>();
 
-        int gcd = gcd(w,h); // 최대공약수
+        int[][] matrix = {
+                {0,1,1,0,0,0},
+                {0,1,1,0,1,1},
+                {0,0,0,0,1,1},
+                {0,0,0,0,1,1},
+                {1,1,0,0,1,0},
+                {1,1,1,0,0,0}
+        };
 
-        System.out.println(((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd));
+        boolean[][] check = new boolean[n][n];
 
-    }
+        for(int i=0;i<n;i++){
+            for(int j =0; j<n;j++){
+                if(!check[i][j] && matrix[i][j] == 1) {
+                    bfs(matrix,i,j,check);
 
-    public static int gcd(int w, int h){
-        int small = Math.min(w,h);
-        while(true) {
-            if (w % small == 0 && h % small == 0) {
-                return small;
+                    list.add(cnt);
+                    cnt=1;
+                }
             }
-            small -= 1;
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.size());
+        for(int i =0;i<list.size();i++){
+            System.out.print(list.get(i) + " ");
         }
     }
 
+    public static void bfs(int[][] matrix, int row, int column, boolean[][] check){
+        check[row][column] = true;
+
+        if(row-1 >=0 && !check[row-1][column] && matrix[row-1][column] == 1){
+            cnt++;
+            check[row-1][column] = true;
+            bfs(matrix,row-1,column,check);
+        }
+        if(row+1 <= matrix.length-1 && !check[row+1][column] && matrix[row+1][column] == 1){
+            cnt++;
+            check[row+1][column] = true;
+            bfs(matrix,row+1,column,check);
+        }
+        if(column-1 >= 0 && !check[row][column-1] && matrix[row][column-1]==1){
+            cnt++;
+            check[row][column-1] = true;
+            bfs(matrix,row,column-1,check);
+        }
+        if(column+1 <= matrix[0].length-1 && !check[row][column+1] && matrix[row][column+1] == 1){
+            cnt++;
+            check[row][column+1] = true;
+            bfs(matrix,row,column+1,check);
+        }
+
+
+    }
 }

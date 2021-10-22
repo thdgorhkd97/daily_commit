9dd450a26064d5db27461338b5f2bab9675d2298
Name <thdgorhkd97@naver.com>
2021-10-22 21:41:37 +0900

    1022

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,91 @@ import java.util.*;
 
 public class Main {
 
-    // N사 코딩테스트 예시문제
-    // 행렬의 영역을 구해서 오름차순으로 정렬하는 문제
-    // bfs를 활용해서 문제를 해결하였다.
-    // bfs 함수에서 해당 좌표의 상하좌우를 확인하는 부분을 조금 중복되게 코드를 짠 것 같은데
-    // 이 부분을 함수로 빼면 조금 더 코드가 간결해질 것 같다.
-    // 붙어있는 부분을 재귀를 통해 bfs를 계속 돌리면서 상하좌우로 연결된 영역을 확인하고
-    // 그 연결이 끊긴다면 영역크기를 재는 변수를 1로 초기화하여
-    // 다른 영역의 크기를 잰다.
+    // programmers level 2 - 컬러링 북
+    // 어제 했던 N사 코딩테스트 예제와 비슷해서 해결해 보았다.
+    // 연결된 같은 숫자의 개수를 구하는 문제였는데 어제와 달리 로직의 실행순서를 더 확인해보았다.
+    // 0 0
+    // 1 0
+    // 2 0
+    // 3 0
+    // 4 0
+    // 3 1
+    // 0 1
+    // 0 2
+    // 이런식으로 아래로 쭉 확인한 다음 위로 올라오면서 우측을 확인하여 개수를 센다.
+    // bfs를 활용하여 하는 방식에 대해서 이제 확실해 진 것 같다.
 
     static int cnt = 1;
 
     public static void main(String[] args) {
 
-        int n = 6;
-        ArrayList<Integer> list = new ArrayList<>();
-
-        int[][] matrix = {
-                {0,1,1,0,0,0},
-                {0,1,1,0,1,1},
-                {0,0,0,0,1,1},
-                {0,0,0,0,1,1},
-                {1,1,0,0,1,0},
-                {1,1,1,0,0,0}
+        int m = 6;
+        int n = 4;
+        int[][] picture = {
+                {1,1,1,0},
+                {1,2,2,0},
+                {1,0,2,1},
+                {1,1,0,1},
+                {1,0,0,3},
+                {0,3,3,3}
         };
 
-        boolean[][] check = new boolean[n][n];
+        int[] answer = new int[2];
 
-        for(int i=0;i<n;i++){
-            for(int j =0; j<n;j++){
-                if(!check[i][j] && matrix[i][j] == 1) {
-                    bfs(matrix,i,j,check);
+        boolean[][] check = new boolean[m][n];
 
-                    list.add(cnt);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!check[i][j] && picture[i][j] !=0){
+                    list.add(bfs(check,picture,i,j));
                     cnt=1;
                 }
             }
         }
 
-        Collections.sort(list);
-
-        System.out.println(list.size());
-        for(int i =0;i<list.size();i++){
-            System.out.print(list.get(i) + " ");
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
         }
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+        answer[0] = list.size();
+        answer[1] = list.get(0);
+
+        System.out.println(answer[0] + " " +  answer[1]);
+
     }
+    public static int bfs(boolean[][] check, int[][] picture,int row,int column){
 
-    public static void bfs(int[][] matrix, int row, int column, boolean[][] check){
         check[row][column] = true;
+        System.out.println(row + " " + column);
 
-        if(row-1 >=0 && !check[row-1][column] && matrix[row-1][column] == 1){
+        if(row-1 >=0 && !check[row-1][column] && picture[row][column] == picture[row-1][column]){
             cnt++;
             check[row-1][column] = true;
-            bfs(matrix,row-1,column,check);
+            bfs(check,picture,row-1,column);
         }
-        if(row+1 <= matrix.length-1 && !check[row+1][column] && matrix[row+1][column] == 1){
+        if(row+1 <= picture.length-1 && !check[row+1][column] && picture[row][column] == picture[row+1][column]){
             cnt++;
             check[row+1][column] = true;
-            bfs(matrix,row+1,column,check);
+            bfs(check,picture,row+1,column);
         }
-        if(column-1 >= 0 && !check[row][column-1] && matrix[row][column-1]==1){
+        if(column-1 >=0 && !check[row][column-1] && picture[row][column] == picture[row][column-1]){
             cnt++;
             check[row][column-1] = true;
-            bfs(matrix,row,column-1,check);
+            bfs(check,picture,row,column-1);
         }
-        if(column+1 <= matrix[0].length-1 && !check[row][column+1] && matrix[row][column+1] == 1){
+        if(column+1 <=picture[0].length-1 && !check[row][column+1] && picture[row][column] == picture[row][column+1]){
             cnt++;
             check[row][column+1] = true;
-            bfs(matrix,row,column+1,check);
+            bfs(check,picture,row,column+1);
         }
 
-
+        return cnt;
     }
 }

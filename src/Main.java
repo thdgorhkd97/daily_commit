d3dbf0a88c3f19c84c0acba60c8c2d54bc046ed9
Name <thdgorhkd97@naver.com>
2021-10-27 23:11:54 +0900

    1027

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,73 @@
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.*;
 
 public class Main {
 
-    // programmers 커뮤러닝 1일차
-    // 그리디 알고리즘
-    // 전체 구역을 true로 바뀌면 완료했다고 생각하는 로직을 구현했는데
-    // 이런 식으로 하는 게 아니라 index를 이용해서 전체를 index가 넘어가면
-    // 완료하는 식으로 짜야 코드가 간략해지는 것으로 보인다.
+    // 커뮤러닝 2일차 - 가장 큰수
+    // {6,10,2} -> 6210 이 가장 큰 수가 된다.
+    // 순열을 이용해서 해결하고자 했는 데 해당 방식이 정확성을 맞는데 시간을 초과한다.
+    // 온라인 강의를 통해서 해결방법에 대해서 알아보고 공부했는데
+    // stream을 이용해서 코드를 간략하게 바꿔서 해결하는 것을 보았다.
+    // stream을 코드에 직접 활용해 본적은 없는데 해당 스킬이 코드를 상당히 간략히
+    // 만드는 것을 보고 stream을 활용하는 방식을 공부해야 할 것 같다.
+
+    static ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        int n = 16;
-        int[] stations = {9};
-        int w = 2;
+        int[] numbers = {6,10,2};
 
-        boolean[] check = new boolean[n+1];
+        String[] str = new String[numbers.length];
 
-        int answer = 0;
-        for(int i=0; i<stations.length;i++){
-            for(int j=stations[i]-w; j<=stations[i]+w; j++){
-                check[j] = true;
-            }
+        for(int i=0;i<str.length;i++){
+            str[i] = String.valueOf(numbers[i]);
         }
 
-        while(!all_cover(check)){
+        int depth = 0;
+        int n = str.length;
+        int r = n;
 
-            for(int i=1;i<=n;i++){
-                if(!check[i]){
-                    for(int j=i;j<=i+2*w;j++){
-                        check[j] = true;
-                    }
-                    answer += 1;
-                }
-            }
+        permu(str, depth, n, r);
 
+        int max = 0;
+        for(int i=0;i<list.size();i++){
+            max = Math.max(max,Integer.parseInt(list.get(i)));
         }
 
+        String answer = String.valueOf(max);
         System.out.println(answer);
 
+
+//        String answer = IntStream.of(numbers)
+//                .mapToObj(String::valueOf)
+//                .sorted((s1,s2)->(s2+s1).compareTo(s1+s2))
+//                .collect(Collectors.joining());
+
+
     }
-    public static boolean all_cover(boolean[] check){
-        int flag = 0;
-        for(int i=1;i< check.length;i++){
-            if(!check[i]){
-                flag = 1;
-                break;
+
+    public static void permu(String[] str, int depth, int n, int r){
+        if(depth == r){
+            StringBuffer sb = new StringBuffer();
+            for(int i=0;i<str.length;i++){
+                sb.append(str[i]);
             }
+            System.out.println(sb.toString());
+            list.add(sb.toString());
         }
 
-        if(flag == 0) return true;
-        else return false;
+        for(int i=depth;i<n;i++){
+            swap(str,depth,i);
+            permu(str,depth+1,n,r);
+            swap(str,depth,i);
+        }
+    }
+
+    private static void swap(String[] str, int depth, int i) {
+        String st = str[depth];
+        str[depth] = str[i];
+        str[i] = st;
     }
 }

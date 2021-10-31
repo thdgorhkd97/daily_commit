999b200cb0aabd003ce35de8a44dd24eeae26d79
Name <thdgorhkd97@naver.com>
2021-10-31 23:59:34 +0900

    1031

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,100 @@ import java.util.stream.*;
 
 public class Main {
 
-    // B사 코딩테스트
-    // {1,1,1,0,0} -> {1,0,1,0,1}
-    // {0,1,1,1,0,0,0,0,1} -> {0,1,0,1,0,1,0,0,1}
-    // 1과 1사이에 0을 둬야 하고 1을 앞으로 당기지 않는다.
-
-    // 금일 코딩테스트를 하면서 접했던 문제인데 확실치 않아 다시 풀어보았다.
-    // 테스트 케이스는 모두 통과하는데 내가 해본 테스트는 모두 통과하는데
-    // 수많은 테스트를 넣어본 게 아니라서 100퍼센트 완벽한지는 확실하지 않다
-    // 코드 자체는 간단하지만 ArrayList를 활용하다가 index를 고치다가 등등
-    // 시간을 상당히 많이 잡아먹었던 문제다.
-
-    // programmers 사이트에서 진행하다 보니 오류가 났을 때 어디서 오류가 난 건지 확인이
-    // 불가능했고 코드를 짤때 인덱스와 범위, loop에 대해서 유의하면서 지속해야겠다.
+    // 빙고문제인데
+    // 세로와 대각을 구현하지못했다.
+    // 잘못한것같다..
 
     public static void main(String[] args) {
 
-        int[] arr = {0,1,1,1,0,0,0,0,1};
-
-        int pos=0;
-        for(int i=pos;i<arr.length-1;i++){
-            if(arr[i] == 1 && arr[i+1] == 1){
-                arr[i+1] = 0;
-                pos = i+2;
-                while(pos<arr.length && arr[pos] == 1){
-                    pos++;
+        //int[][] board = [[11,13,15,16],[12,1,4,3],[10,2,7,8],[5,14,6,9]]
+        // int[] nums = [14,3,2,4,13,1,16,11,5,15]
+        // answer = 3;
+        int answer = 0;
+        final int n = board.length;
+        for(int number:nums){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(board[i][j] == number){
+                        board[i][j] = 0;
+                        break;
+                    }
                 }
-                arr[pos] = 1;
-                i = pos-2;
             }
-            System.out.println(i);
-        }
-
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
         }
 
 
+
+        answer = garo(board) + sero(board) + daegak(board);
+        return answer;
+    }
+    public static int garo(int[][] board){
+        boolean flag = false;
+        int answer=0;
+        for(int i=0;i<board.length;i++){
+            flag = false;
+            for(int j=0;j<board.length;j++){
+                if(board[i][j] != 0){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) answer++;
+        }
+        System.out.println(" garo : " + answer);
+        return answer;
+    }
+    public static int sero(int[][] board){
+        boolean flag = false;
+        int answer=0;
+        int column = 0;
+        for(int i=0;i<board[0].length;i++){
+            column = 0;
+            while(column < board.length){
+                if(board[column][i] != 0){
+                    flag = true;
+                    break;
+                }
+                column++;
+            }
+            if(!flag) answer++;
+        }
+
+        System.out.println(" sero : " + answer);
+        return answer;
+    }
+
+    public static int daegak(int[][] board){
+        boolean flag = false;
+        int answer=0;
+        int i=0;
+        int j=0;
+
+        while(i < board.length && j < board[0].length){
+            if(board[i][j] != 0){
+                flag = true;
+                break;
+            }
+            i++;
+            j++;
+        }
+        if(!flag) answer++;
+
+        i=board.length-1;
+        j=0;
+        while(i >=0 && j < board[0].length){
+            if(board[i][j] != 0){
+                flag = true;
+                break;
+            }
+            i--;
+            j++;
+        }
+        if(!flag) answer++;
+
+
+
+        System.out.println(" daegak : " + answer);
+        return answer;
     }
 }

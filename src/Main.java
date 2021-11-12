370d26f66cf58ee53166fd02cb75a03b2012566c
Name <thdgorhkd97@naver.com>
2021-11-12 23:24:57 +0900

    1112

--- src/Main.java
+++ src/Main.java
@@ -6,57 +6,45 @@ import java.util.stream.*;
 
 public class Main {
 
-    // programmers level 3 - 가장 먼 노드
-    // 그래프에서 1과 가장 거리가 먼 노드의 개수를 구하는 문제
-    // bfs를 활용해서 1과의 거리 차이를 구했는 데 테스트 케이스 9개중 2개가
-    // 메모리 초과가 나왔다.
-    // bfs를 활용할 때 변수를 써서 +1을 했는데 그렇게 안하고 distance 변수를 활용하여
-    // 지금 큐에 담겨있는 변수에 +1을 해야 햇다.
+    // programmers 등굣길
+    // 동적계획법으로 위와 왼쪽을 더해가면서 채워나가는 식으로 풀이하였는데
+    // 자꾸 조금씩 어긋났다.
+    // 아마 어떤 특수한 경우가 있는 것 같은데 그걸 모두 다 커버하는
+    // 전체식을 구하기 힘들었다ㅠㅠ (내가 넣어본 테케는 모두 맞는데)
+    // 결국 해답을 찾아서 해결했다.. if 문으로 위와 왼쪽 값을 파악해 가면서 더해 나가는데
+    // 나는 1열과 1행 모두를 1로 세팅해놓고 해서 문제가 발생한 듯 싶기도 하다.
 
     public static void main(String[] args) {
 
-        int n = 6;
-        int[][] edge = {{3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2}};
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2,2}};
 
-        int[][] graph = new int[n+1][n+1];
-        int[] distance = new int[n+1];
-        boolean[] check = new boolean[n+1];
-        int num=1;
+        int[][] map = new int[n+1][m+1];
 
-        for(int i=0;i<edge.length;i++){
-            graph[edge[i][0]][edge[i][1]] = 1;
-            graph[edge[i][1]][edge[i][0]] = 1;
+        for(int i=0;i< puddles.length;i++){
+            map[puddles[i][1]][puddles[i][0]] = -1;
         }
 
-        bfs(1,distance,check,num,graph);
-
-        int answer = 0;
-        Arrays.sort(distance);
-        for(int i=0;i< distance.length;i++){
-            if(distance[i] == distance[n]){
-                answer++;
+        map[1][1] = 1;
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(map[i][j] == -1) continue;
+                if(map[i - 1][j] > 0) map[i][j] += map[i - 1][j] % 1000000007;
+                if(map[i][j - 1] > 0) map[i][j] += map[i][j - 1] % 1000000007;
             }
         }
 
-    }
-    public static void bfs(int start, int[] distance, boolean[] check,int num,int[][] graph){
-        Queue<Integer> que = new LinkedList<>();
-        check[start] = true;
-        que.offer(start);
-
-        while(!que.isEmpty()){
-            int cur = que.poll();
-
-            System.out.println(cur + " " + distance[cur]);
-
-            for(int i=1;i<graph.length;i++){
-                if(graph[cur][i] == 1 && !check[i]){
-                    que.add(i);
-                    distance[i] = distance[cur] + 1;
-                    check[i] = true;
-                }
+        for(int i=1;i<map.length;i++){
+            for(int j=1;j<map[0].length;j++){
+                System.out.print(map[i][j] + " ");
             }
+            System.out.println();
         }
+
+        System.out.println(map[n][m]);
+
+
 
     }
 }

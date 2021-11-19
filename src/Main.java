ddf27adcbca89fa6789b7af3c937ae61566385b1
Name <thdgorhkd97@naver.com>
2021-11-19 23:34:36 +0900

    1119

--- src/Main.java
+++ src/Main.java
@@ -6,74 +6,77 @@ import java.util.stream.*;
 
 public class Main {
 
-    // programmers level 2 - 게임 맵 최단거리
-    // bfs를 활용한 대표적인 거리 찾기 문제였다.
-    // 좌표를 활용하다 보니 bfs만 이용하는 것에서 조금 더 나아가는 문제였다.
-    // 나는 class P를 추가하였는데 찾아보니 그냥 x,y로 2번 체크하신 분도 있고 cnt같은 변수로해서 거리까지 한 번에
-    // 계산하는 식으로 하신 분들도 계셨다.
-    // 현재 좌표에서 상하좌우 한칸씩 이동했을 때 막혀있거나 맵을 벗어나는 경우가 아니라면
-    // 큐에 넣고 그 곳으로 이동하는 식으로 진행하면서 cnt를 1씩 더해 나간다.
-    // 아예 클래스를 하나 추가해서 해본 건 처음이었는데 생각보다 까다로웠다.
-
-    static class P{
-        int x;
-        int y;
-        P(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+    // programmers level 3 - 가장 먼 노드
+    // bfs를 통해 그래프를 훑어가면서 1로부터 최대 거리인 점의 수를 찾는 문제
+    // bfs를 구현하여 거리는 모두 구했지만 메모리 초과가 났다.
+    // 처음에 graph[][] 2중 배열을 사용했을 때가 메모리 초과가 생겻는데
+    // ArrayList<ArrayList<Integer>> 이렇게 2중 리스트를 구현하니 메모리 이슈가 해결되었다.
+    // 아무래도 bfs를 위해서 이중 배열의 모든 원소를 확인하여 1인지 확인하다 보니
+    // 이중 배열로 구현하니까 큰 그래프에서 순회가 많아져 메모리 초과가 발생한 것으로 보인다.
+    // 단순히 배열과 list가 순회와 삽입 삭제 에서만 속도 차이가 있다고 알고 있는데
+    // 순회적인 측면을 강조해야 하는 bfs이다 보니 처음부터 이중 배열을 선택했는데
+    // 절대적인 게 아니라는 걸 또 깨닫게 되었다.
 
     public static void main(String[] args) {
 
-        int[][] maps = {
-                {1,0,1,1,1},
-                {1,0,1,0,1},
-                {1,0,1,1,1},
-                {1,1,1,0,0},
-                {0,0,0,0,1}
-        };
+        int n = 6;
+        int[][] edge = {{3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2}};
 
-        boolean[][] check = new boolean[maps.length][maps[0].length];
-
-        int[] dx = {-1,0,1,0};
-        int[] dy = {0,-1,0,1};
-
-        Queue<P> que = new LinkedList<>();
-
-        que.offer(new P(0,0));
-        check[0][0] = true;
-
-        while(!que.isEmpty()){
-            P p = que.poll();
-            int curX = p.x;
-            int curY = p.y;
-
-            for(int i=0;i<4;i++){
-                int nowX = curX + dx[i];
-                int nowY = curY + dy[i];
-
-                if(nowX<0||nowX>= maps.length||nowY<0||nowY>=maps[0].length) continue;
-                if(maps[nowX][nowY]==0) continue;
-                if(!check[nowX][nowY]){
-                    que.offer(new P(nowX,nowY));
-                    maps[nowX][nowY]=maps[curX][curY]+1;
-                    check[nowX][nowY]= true;
-                }
-            }
+        ArrayList<ArrayList<Integer>> list=new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<edge.length;i++){
+            list.add(new ArrayList<Integer>());
+        }
+        //노드 연결
+        int a, b;
+        for(int[] node:edge){
+            a=node[0];
+            b=node[1];
+            list.get(a).add(b);
+            list.get(b).add(a);
         }
 
-//        for(int i=0;i<maps.length;i++){
-//            for(int j=0;j<maps[0].length;j++){
-//                System.out.print(maps[i][j] + " ");
+//        for(int i=1;i<graph.length;i++){
+//            for(int j=1;j<graph.length;j++){
+//                System.out.print(graph[i][j] + " ");
 //            }
 //            System.out.println();
 //        }
 
-        int answer = 0;
-        answer = maps[maps.length-1][maps[0].length-1];
-        System.out.println(answer);
+        Queue<Integer> que = new LinkedList<>();
+        boolean[] check = new boolean[n+1];
+        int[] distance = new int[n+1];
 
+        distance[1] = 1;
+        que.offer(1);
+        check[1] = true;
+
+        while(!que.isEmpty()){
+            int idx = que.poll();
+            System.out.println(idx + " 에 대해서 확인해보자 ");
+
+            for(int v : list.get(idx)){
+                if(!check[v]){
+                    que.offer(v);
+                    check[v] = true;
+                    distance[v] = distance[idx] + 1;
+                }
+            }
+
+        }
+
+        int answer = 0;
+        int max = 0;
+        for(int num : distance){
+            if(num > max){
+                max = num;
+                answer = 1;
+            }
+            else if(num == max){
+                answer ++;
+            }
+        }
+
+        System.out.println(answer);
 
 
 

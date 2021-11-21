82ca2fac1706646c7f533b483545c80d36ce24d6
Name <thdgorhkd97@naver.com>
2021-11-21 18:39:56 +0900

    1121

--- src/Main.java
+++ src/Main.java
@@ -6,78 +6,46 @@ import java.util.stream.*;
 
 public class Main {
 
-    // programmers level 3 - 여행 경로
-    // 50 / 100 절반 성공 절반 시간 초과
-    // 2개 이상이 가능할 경우 알파벳 순으로 가장 빠른 도시를 방문해야 하는 조건에서
-    // 시간이 조금 오래 걸렸다.
-    // 그래서 전체를 순회하여 도착 가능한 모든 도시를 구하고 도시를 정렬한 후에
-    // 위치하고 그 도시를 다시 큐에 넣는 bfs를 활용한 방식을 구현하였는데
-    // 모두 순회하고 도시가 새로 들어올 때마다 모두 순회하다 보니
-    // 경우의 수가 많아져서 시간 초과가 발생한 것으로 보인다.
-    // 근데 도착 가능한 모든 경우의 수를 구해야 하기 때문에 모두 순회하는 것으로 구현한 것인데
-    // 시간을 줄이기 위한 방법을 더 고민해 봐야 할 것으로 보인다.
+    // programmers level 3 - 가장 긴 팰린드롭
+    // 부분 문자열 중 가장 긴 앞뒤가 똑같은 부분 문자열의 길이 찾기 문제
+    // 중첩 반복문을 활용하여 길이를 조절해서 부분 문자열을 구해가며 구하는 데는 성공했는데
+    // 정확성에서 통과를 하지 못하였다..
+    // 그래서 arraylist를 char[] 배열에 복사하는 걸 크기 조정을 통해 순회를 한 번 없애고
+    // 반복문으로 부분 문자열을 복사하는 걸 substring(start, start + len + 1)을 통해
+    // 없앴는 데도 한 가지 효율성 테스트 케이스를 통과하지 못한다 ㅠㅠㅠ
+    // 시간을 줄이기 위해서 코드를 리팩토링 해보았는데 계속 안되는 거면
+    // 아마 내 로직 자체가 시간이 오래 걸리는 것으로 보인다.
+    // 자료구조는 char[] 만 사용했기에 자료구조 자체에 시간이 오래 걸리는 것은 아닐 것으로
+
 
     public static void main(String[] args) {
 
-        String[][] tickets = {{"ICN","SFO"},{"ICN","ATL"},{"SFO","ATL"},{"ATL","ICN"},
-                {"ATL","SFO"}};
-//        String[][] tickets = {{"ICN","JFK"},{"HND","IAD"},{"JFK","HND"}};
+        String s = "abcdcba";
 
-        Queue<String> que = new LinkedList<>();
-        ArrayList<String> result = new ArrayList<>();
-        String[] answer;
-        boolean[] check = new boolean[tickets.length];
+        for(int len = s.length(); len >= 1; len--){ // 7 6 5 4 3 2 1
+            int pos = s.length() - len;
+            for(int i=0;i<pos;i++) {
+                int start = i;
+                int cnt = 0;
 
-        que.offer("ICN");
-        result.add("ICN");
+                char[] ch = s.substring(start,start + len + 1).toCharArray();
 
-        while(!cover(check)){
-            String str = que.poll();
-            ArrayList<String> list = new ArrayList<>();
-            int idx = 0;
-
-            for(int i=0;i< tickets.length; i++){
-                if(tickets[i][0].equals(str) && !check[i]){
-                    list.add(tickets[i][1]);
-                    idx = i;
-                }
-            }
-
-            if(list.size() == 1){
-                que.offer(list.get(0));
-                result.add(list.get(0));
-                check[idx] = true;
-            }
-            else{ // 가능한 경로가 2개 이상일 때
-                Collections.sort(list);
-
-                for(int i=0;i<tickets.length;i++){
-                    if(tickets[i][0].equals(str) &&tickets[i][1].equals(list.get(0)) && !check[i]){
-                        que.offer(list.get(0));
-                        result.add(list.get(0));
-                        check[i] = true;
-                        break;
+                    int mid = ch.length / 2;
+                    boolean flag = true;
+                    for(int idx=0;idx<mid;idx++){
+                        if(ch[idx] != ch[ch.length - idx - 1]){
+                            flag = false;
+                            break;
+                        }
                     }
-                }
+                    if(flag) {
+                        System.out.println(ch.length);
+                        return ;
+                    }
+
             }
-
         }
 
-        answer = new String[result.size()];
-        for(int i=0;i<answer.length;i++){
-            answer[i] = result.get(i);
-        }
-        for(int i=0;i<answer.length;i++){
-            System.out.println(answer[i]);
 
-        }
-
-    }
-
-    public static boolean cover(boolean[] check){
-        for(int i=0;i<check.length;i++){
-            if(!check[i]) return false;
-        }
-        return true;
     }
 }

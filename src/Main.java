bc0f327c38ba5c4914b50e76046ba5cc3dc61f26
Name <thdgorhkd97@naver.com>
2021-11-22 23:06:34 +0900

    1122

--- src/Main.java
+++ src/Main.java
@@ -8,43 +8,40 @@ public class Main {
 
     // programmers level 3 - 가장 긴 팰린드롭
     // 부분 문자열 중 가장 긴 앞뒤가 똑같은 부분 문자열의 길이 찾기 문제
-    // 중첩 반복문을 활용하여 길이를 조절해서 부분 문자열을 구해가며 구하는 데는 성공했는데
-    // 정확성에서 통과를 하지 못하였다..
-    // 그래서 arraylist를 char[] 배열에 복사하는 걸 크기 조정을 통해 순회를 한 번 없애고
-    // 반복문으로 부분 문자열을 복사하는 걸 substring(start, start + len + 1)을 통해
-    // 없앴는 데도 한 가지 효율성 테스트 케이스를 통과하지 못한다 ㅠㅠㅠ
-    // 시간을 줄이기 위해서 코드를 리팩토링 해보았는데 계속 안되는 거면
-    // 아마 내 로직 자체가 시간이 오래 걸리는 것으로 보인다.
-    // 자료구조는 char[] 만 사용했기에 자료구조 자체에 시간이 오래 걸리는 것은 아닐 것으로
+    // 통과했는데 효율성을 결국 검색해서 알아보고 해결했다 ㅠㅠ
+    // for(int i=0;i + len <= s.length(); i++) 이 부분에 차이가 있었다.
+    // 나는 s.length() - len 이렇게 했는데 이런식으로 해서 사용하는 index를 편하게 활용한 것이
+    // 대부분 사람들의 풀이 방식이었다.
+    // 이를 제외하고는 코드 자체가 다르지 않은 것으로 보아 내가 한 방식으로도 해결이 가능했을 터인데
+    // 내가 for문을 깨끗하게 하려는 습관이 있어서 for문을 깨끗하게 하니 for 문 내에서
+    // 인덱스를 순회하는 과정이 번거롭게 되어 실패했던 것이 아닐까 싶다.
 
 
     public static void main(String[] args) {
 
         String s = "abcdcba";
 
+        char[] ch = s.toCharArray();
+
         for(int len = s.length(); len >= 1; len--){ // 7 6 5 4 3 2 1
-            int pos = s.length() - len;
-            for(int i=0;i<pos;i++) {
-                int start = i;
-                int cnt = 0;
+            for(int i=0;i + len <= s.length(); i++) {
 
-                char[] ch = s.substring(start,start + len + 1).toCharArray();
-
-                    int mid = ch.length / 2;
-                    boolean flag = true;
-                    for(int idx=0;idx<mid;idx++){
-                        if(ch[idx] != ch[ch.length - idx - 1]){
-                            flag = false;
-                            break;
-                        }
+                boolean flag = true;
+                int mid = len / 2;
+                for(int idx=0;idx<mid;idx++){
+                    if(ch[i + idx] != ch[i + len - idx - 1]){
+                        flag = false;
+                        break;
                     }
-                    if(flag) {
-                        System.out.println(ch.length);
-                        return ;
-                    }
+                }
+                if(flag) {
+                    System.out.println(len);
+                }
 
             }
+
         }
+
 
 
     }

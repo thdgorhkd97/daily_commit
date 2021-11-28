c2985f96b6e27214ceb57af253e72bfb1f7fd75c
Name <thdgorhkd97@naver.com>
2021-11-28 23:47:14 +0900

    1128

--- src/Main.java
+++ src/Main.java
@@ -6,57 +6,58 @@ import java.util.stream.*;
 
 public class Main {
 
-    // L 사 코딩테스트 1번 문제
-    // source 문자열에서 겹치지않게 하나씩 문자를 빼고 사전순으로 정렬한 다음 문자열을 이어나간다.
-    // 처음에는 source 문자열에서 꺼내고 source 문자열 내에서 제외해나가면서 처리하려 했는데
-    // String 형을 제외해 나가면서 하기가 쉽지 않았다( 사용했으면 아예 다른 문자로 바꾸는 식으로 치환하려 함)
-    // 근데 그렇게 하면 source의 길이만큼 계속 순회를 했어야 해서 그런 방식은 포기했다.
-    // 그래서 source 문자열을 arraylist로 복사해서 거기서 제외해 나가며 새로운 문자열을 만들었다.
-    // 처음에 source 문자열을 처리해서 해결해야 겠다는 생각에 시간을 조금 소요했었는데
-    // 진작 arraylist로 했으면 훨씬 빠르게 처리할 수 있었을 것 같다.
-
+    // programmers level 3 - 보석 쇼핑
+    // 정확성은 모두 통과하는데 효율성에서 통과하지 못하였다
+    // 보석의 모든 종류를 포함하는 가장 짧은 범위를 구하는 문제
+    // 전체 종류 개수를 파악한 다음 시작 포인트부터 for문을 돌며 전체 종류를 포함하게 되면
+    // 해당 인덱스까지의 범위를 이전과 비교하며 더 짧으면 갱신한다.
+    // gems 배열이 100000까지인데 이중배열을 돌면서 확인하다 보니까 효율성에서
+    // 통과하지 못하는 것으로 보인다.
+    // 전체를 도는 것이 아니라 중간에서 끊을 수 있는 조건을 찾아서
+    // n^2 만큼 도는 지금 로직을 변경해야 할 것 같다.
 
     public static void main(String[] args){
 
-        String source = "execute";
+        String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
+        HashMap<String,Integer> map = new HashMap<>();
 
-        String dest = "";
-
-        Set<Character> set = new HashSet<>();
-        ArrayList<Character> list = new ArrayList<>();
-        ArrayList<Character> s = new ArrayList<>();
-
-        for(int i=0;i<source.length();i++){
-            s.add(source.charAt(i));
+        Set<String> set = new HashSet<>(); // 전체 종류 개수
+        for(String str : gems){
+            set.add(str);
         }
 
-        while(true){
-//if(source.equals("")) return dest;
+        int[] answer = new int[2];
+        answer[1] = 100000;
+        int length = 0;
 
-            if(s.isEmpty()) break;
-
-            for(int i=0;i<s.size();i++){
-                if(set.add(s.get(i))){ // 겹치지 않으면
-                    list.add(s.get(i));
-                    s.remove(s.get(i));
-                    i--;
+        for(int i=0;i<gems.length;i++){
+            map.clear();
+            int start = i;
+            int end = 0;
+            boolean flag = true;
+            for(int j=i;j<gems.length;j++){
+                map.put(gems[j], map.getOrDefault(gems[i],0)+1);
+                if(map.size() == set.size()){
+                    end = j;
+                    flag = false;
+                    break;
                 }
             }
 
-            Collections.sort(list);
+            if(!flag) {
 
-            StringBuffer sb = new StringBuffer();
-            for(int i=0;i<list.size();i++){
-                sb.append(list.get(i));
+                System.out.println(i + " 일 때 " + start + " " + end);
+                length = end - start;
+                if (answer[1] - answer[0] > length) {
+                    answer[1] = end;
+                    answer[0] = start;
+                }
             }
-            dest += sb.toString();
-
-            set.clear();
-            list.clear();
 
         }
 
-        System.out.println(dest);
+        System.out.println(answer[0] + " " + answer[1]);
+
 
     }
 }

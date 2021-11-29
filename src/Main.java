8046639ef4264f03101d214771390fedccea3ac7
Name <thdgorhkd97@naver.com>
2021-11-29 23:16:51 +0900

    1129

--- src/Main.java
+++ src/Main.java
@@ -7,57 +7,51 @@ import java.util.stream.*;
 public class Main {
 
     // programmers level 3 - 보석 쇼핑
-    // 정확성은 모두 통과하는데 효율성에서 통과하지 못하였다
-    // 보석의 모든 종류를 포함하는 가장 짧은 범위를 구하는 문제
-    // 전체 종류 개수를 파악한 다음 시작 포인트부터 for문을 돌며 전체 종류를 포함하게 되면
-    // 해당 인덱스까지의 범위를 이전과 비교하며 더 짧으면 갱신한다.
-    // gems 배열이 100000까지인데 이중배열을 돌면서 확인하다 보니까 효율성에서
-    // 통과하지 못하는 것으로 보인다.
-    // 전체를 도는 것이 아니라 중간에서 끊을 수 있는 조건을 찾아서
-    // n^2 만큼 도는 지금 로직을 변경해야 할 것 같다.
+    // 효율성에서 문제가 생겨서 인터넷에서 검색 후 해결하였다.
+    // 원래는 이중배열을 이용해서 풀어서 n^2의 시간을 소모해야 했는데 투 포인터를
+    // 활용해야 하는 문제였다.
+    // 모든 보석을 포함하되 큐에 가장 먼저 넣은 보석이 1개보다 많아지면 1개가 될 때까지
+    // 앞의 시작 포인트를 당기는 식으로 해야 했다.
+    // 정확성은 좋지만 효율성에서 문제가 생기는 경우가 꽤 많다.
+    // 알고 있지만 이를 해결하기가 쉽지 않은 것 같다....
+    // 이번 문제도 이중 배열이 아니라 애초에 투포인터를 이용해서 인덱스를 바꿔가며 확인하는 등
+    // 시간을 좀 더 신경쓰면서 해결해보자
 
     public static void main(String[] args){
 
         String[] gems = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        HashMap<String,Integer> map = new HashMap<>();
+        int[] answer = new int[2];
 
-        Set<String> set = new HashSet<>(); // 전체 종류 개수
-        for(String str : gems){
-            set.add(str);
+        HashSet<String> set = new HashSet<>();
+
+        for (String s : gems) {
+            set.add(s);
         }
 
-        int[] answer = new int[2];
-        answer[1] = 100000;
-        int length = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        Queue<String> q = new LinkedList<>();
+        int start = 0;
+        int idx = 0;
+        int len = Integer.MAX_VALUE;
 
         for(int i=0;i<gems.length;i++){
-            map.clear();
-            int start = i;
-            int end = 0;
-            boolean flag = true;
-            for(int j=i;j<gems.length;j++){
-                map.put(gems[j], map.getOrDefault(gems[i],0)+1);
-                if(map.size() == set.size()){
-                    end = j;
-                    flag = false;
-                    break;
-                }
+            map.put(gems[i],map.getOrDefault(gems[i],0) + 1);
+            q.add(gems[i]);
+
+            while(map.get(q.peek()) > 1){
+                map.put(q.peek(),map.get(q.poll())-1);
+                idx++;
             }
 
-            if(!flag) {
-
-                System.out.println(i + " 일 때 " + start + " " + end);
-                length = end - start;
-                if (answer[1] - answer[0] > length) {
-                    answer[1] = end;
-                    answer[0] = start;
-                }
+            if(map.size() == set.size() && len > (i-idx)){
+                len = i - idx;
+                start = idx + 1;
             }
-
         }
 
+        answer[0] = start;
+        answer[1] = len+start;
         System.out.println(answer[0] + " " + answer[1]);
-
 
     }
 }

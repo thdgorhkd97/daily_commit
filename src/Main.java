301336eff956ac186d74e7d119fcf6f5a3aeb720
Name <thdgorhkd97@naver.com>
2021-12-09 23:35:05 +0900

    1209

--- src/Main.java
+++ src/Main.java
@@ -6,80 +6,64 @@ import java.util.stream.*;
 
 public class Main {
 
-    // B사 코딩테스트 5번 문제
-    // 전체 map 배열에 0이 없다면 이 조건을 !fullCover로 해서
-    // map 배열을 전체를 돌면서 0이 없는지 있는지를 확인하는 식으로 while문을 돌았는데
-    // 이게 만약 rows와 columns가 커지면 시간 이슈가 생길 수도 있지 않나 싶다..
-    // 해결하는 데는 조금 시간이 걸리긴 했지만 문제 없었다고 생각하는데
-    // while문을 돌면서 fullCover를 도는 부분이 조금 걱정이다.git
+    // programmers level 3 - 줄 서는 방법
+    // 순열을 통해서 조합을 구하는 데 순서를 유지하기 위한 방법을 공부했다.
+    // 사전순을 유지하면서 조합을 구하는 방법을 사용했는데 그렇게 만들어진 배열을
+    // ArrayList<long[]> 에 넣었는데 이걸 출력하는 방식에서 막힌다..
+    // list.get(i)를 해서 for문으로 j를 list.get(i)[j]로 표현하는데
+    // 이렇게 표현하면 항상 마지막으로 list에 넣은 값에 대해서만 출력이 되기 때문에
+    // 어떻게 해결해야 할 지 막히고 말았다..
+    // arraylist를 다시 구현해야 하는지 혹은 arraylist에서 꺼내서 출력하는 방식을
+    // 다시 고민해야 하는지 모르겠다.
+
+    static ArrayList<long[]> list =  new ArrayList<>();
+    static long[] answer;
 
     public static void main(String[] args) {
 
-        int rows = 3;
-        int columns = 4;
+        int n = 3;
+        long k = 5;
 
-        int[][] map = new int[rows+1][columns+1];
-
-        int r = 1;
-        int c = 1;
-        int num = 1;
-        map[r][c] = num;
-        while(!fullCover(map) ){
-
-            System.out.println(r+"행 " + c+ "열 " + " 에 " + num);
-            if(!fullCover(map) && r==1 && c== 1){
-                if(num % 2 == 0){
-                    if(map[r+1][c] != 0){
-                        break;
-                    }
-                }
-                else{
-                    if(map[r][c+1] != 0){
-                        break;
-                    }
-                }
-            }
-
-            map[r][c] = num;
-
-            if(num % 2 == 0){
-                if(r < rows){
-                    r= r+1;
-                }
-                else{
-                    r = 1;
-                }
-            }
-            else{
-                if(c < columns){
-                    c = c+1;
-                }
-                else{
-                    c = 1;
-                }
-            }
-
-            num++;
+        long[] arr = new long[n];
+        for(int i=0;i<n;i++){
+            arr[i] = i+1;
         }
 
-        int[][] answer = new int[rows][columns];
-        for(int i=1;i<=rows;i++){
-            for(int j=1;j<=columns;j++){
-                answer[i-1][j-1] = map[i][j];
+        long[] output = new long[n];
+        boolean[] visited = new boolean[n];
+
+
+        long[] answer = new long[n];
+
+        perm(arr,output,visited,0,n,n,k);
+
+
+        for(int i=0;i<list.size();i++){
+            long[] array = Arrays.stream(list.get(i)).toArray();
+            for(int j=0;j<n;j++){
+                System.out.print(array[j] + " ");
             }
+            System.out.println();
         }
 
 
     }
 
-    public static boolean fullCover(int[][] map){
-        for(int i=1;i<map.length;i++){
-            for(int j=1;j<map[0].length;j++) {
-                if(map[i][j] == 0){
-                    return false; // 0이 있을 때
-                }
+    static void perm(long[] arr, long[] output, boolean[] visited, int depth, int n, int r,long k) {
+        if (depth == r) {
+
+            list.add(output);
+            return ;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                perm(arr, output, visited, depth + 1, n, r,k);
+                visited[i] = false;
             }
         }
-        return true; // 0이 없을 때
+
     }
 }

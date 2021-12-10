60c485b5ad60ecd6fb31d5c729dc5f717796dd56
Name <thdgorhkd97@naver.com>
2021-12-10 23:11:27 +0900

    1210

--- src/Main.java
+++ src/Main.java
@@ -6,64 +6,68 @@ import java.util.stream.*;
 
 public class Main {
 
-    // programmers level 3 - 줄 서는 방법
-    // 순열을 통해서 조합을 구하는 데 순서를 유지하기 위한 방법을 공부했다.
-    // 사전순을 유지하면서 조합을 구하는 방법을 사용했는데 그렇게 만들어진 배열을
-    // ArrayList<long[]> 에 넣었는데 이걸 출력하는 방식에서 막힌다..
-    // list.get(i)를 해서 for문으로 j를 list.get(i)[j]로 표현하는데
-    // 이렇게 표현하면 항상 마지막으로 list에 넣은 값에 대해서만 출력이 되기 때문에
-    // 어떻게 해결해야 할 지 막히고 말았다..
-    // arraylist를 다시 구현해야 하는지 혹은 arraylist에서 꺼내서 출력하는 방식을
-    // 다시 고민해야 하는지 모르겠다.
-
-    static ArrayList<long[]> list =  new ArrayList<>();
-    static long[] answer;
+    // programmers level 3 - 기지국 설치
+    // 내가 한 방식과 일반적인 풀이 방식에 차이가 있어서 2가지 방법을 사용해 보았다.(나는 50% 정도 였고 풀이방식은 참고해서 다시 했다.)
+    // 풀이 방식은 idx와 pos을 활용해서 모든 아파트를 지나치면 끝나는 방식을 활용하여 인덱스를 통해 종료조건을 걸었고
+    // 나는 boolean[] 배열을 활용해서 모두 true로 바뀌었는지를 확인했다.(인덱스 활용 방식이 메모리, 시간 측에서 이득이다.)
+    // 풀이방식과 나의 방식 중 가장 큰 차이점은 stations[] 배열에 미리 주어지는 설치 지점들을 별도의 변수를 활용해서
+    // 특정 조건일 때만(설치된 기지국을 지나가야 할 때) 인덱스를 더해가는 식으로 처리한 것이다.
+    // 즉, 전체 아파트를 순회하는 변수와 미리 설치된 기지국을 확인하는 변수를 따로 관리하는 것이 포인트였다.
 
     public static void main(String[] args) {
 
-        int n = 3;
-        long k = 5;
+        int n = 16;
+        int[] stations = {9};
+        int w = 2;
 
-        long[] arr = new long[n];
-        for(int i=0;i<n;i++){
-            arr[i] = i+1;
-        }
-
-        long[] output = new long[n];
-        boolean[] visited = new boolean[n];
+        int answer = 0;
+        int pos = 1;
+        int idx = 0;
 
 
-        long[] answer = new long[n];
+        while(pos <= n){
 
-        perm(arr,output,visited,0,n,n,k);
-
-
-        for(int i=0;i<list.size();i++){
-            long[] array = Arrays.stream(list.get(i)).toArray();
-            for(int j=0;j<n;j++){
-                System.out.print(array[j] + " ");
+            if(idx < stations.length&&pos >= stations[idx] - w){
+                pos = stations[idx] + w + 1;
+                idx++;
             }
-            System.out.println();
-        }
-
-
-    }
-
-    static void perm(long[] arr, long[] output, boolean[] visited, int depth, int n, int r,long k) {
-        if (depth == r) {
-
-            list.add(output);
-            return ;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1, n, r,k);
-                visited[i] = false;
+            else{
+                answer++;
+                pos += 2*w + 1;
             }
         }
+
+        System.out.println(answer);
+
+        /*
+        boolean[] check = new boolean[n+1];
+
+        for(int i=0;i<stations.length;i++){
+            for(int j=stations[i]-w;j<=stations[i]+w;j++){
+                check[i] = true;
+            }
+        }
+
+        int pos = 1;
+
+        while(pos < n+1){
+            if(!check[pos]){
+                for(int i=pos;i<pos+2*w;i++){
+                    if(i > n){
+                        break;
+                    }
+                    check[i] = true;
+                }
+                answer++;
+                pos = pos + 2 * w + 1;
+            }
+            pos++;
+        }
+
+        System.out.println(answer);
+        */
+
+
 
     }
 }

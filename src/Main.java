6a8577c81ae58c647e952b387fdefb3d629d0eab
Name <thdgorhkd97@naver.com>
2021-12-11 23:57:54 +0900

    1211

--- src/Main.java
+++ src/Main.java
@@ -6,68 +6,53 @@ import java.util.stream.*;
 
 public class Main {
 
-    // programmers level 3 - 기지국 설치
-    // 내가 한 방식과 일반적인 풀이 방식에 차이가 있어서 2가지 방법을 사용해 보았다.(나는 50% 정도 였고 풀이방식은 참고해서 다시 했다.)
-    // 풀이 방식은 idx와 pos을 활용해서 모든 아파트를 지나치면 끝나는 방식을 활용하여 인덱스를 통해 종료조건을 걸었고
-    // 나는 boolean[] 배열을 활용해서 모두 true로 바뀌었는지를 확인했다.(인덱스 활용 방식이 메모리, 시간 측에서 이득이다.)
-    // 풀이방식과 나의 방식 중 가장 큰 차이점은 stations[] 배열에 미리 주어지는 설치 지점들을 별도의 변수를 활용해서
-    // 특정 조건일 때만(설치된 기지국을 지나가야 할 때) 인덱스를 더해가는 식으로 처리한 것이다.
-    // 즉, 전체 아파트를 순회하는 변수와 미리 설치된 기지국을 확인하는 변수를 따로 관리하는 것이 포인트였다.
+    // L사 코딩테스트 2번 문제
+    // ( 문제 ) x와 y 배열에 좌표로 주어지는 점들을 최소 길이로 연결하였을 때 그 중 가장 긴 선분의 길이 구하기
+    // 좌표 배열을 정렬해서 다음 인덱스가 오른쪽이나 오른쪽 위에 위치하도록 만든 다음
+    // 다음 인덱스와의 거리를 계산해서 그 중 최대값의 ceil 값을 구한다.
+    // 이게 각 좌표값을 우상향으로 정렬한 다음에 다음 인덱스와의 거리를 구하는 게 각 지점을 연결하는 방법이라고 판단해서
+    // 이렇게 한 건데 문제의 조건 중에서 각 지점을 연결하는 거리를 최소로 한다는 조건을 만족시키는 지에 대해서
+    // 미지수인 것이 걸리는 부분이다.
+    // 처음에 int[] x와 int[] y의 i번째 좌표가 지점을 만든다는 걸 이해하는데 조금 걸렸고
+    // 유클리드 거리를 구하는 식에서 조금 애먹은 것 같다.
+    //              for(int j=0;j<2;j++){
+    //                sum += Math.pow(matrix[i][j]-matrix[i+1][j], 2); // 각 좌표의 차의 제곱값
+    //            }
+    //            max = Math.max(max,(int)Math.ceil(Math.sqrt(sum)));
 
     public static void main(String[] args) {
 
-        int n = 16;
-        int[] stations = {9};
-        int w = 2;
+        int[] x = {1,2,4,2};
+        int[] y = {1,1,4,2};
 
-        int answer = 0;
-        int pos = 1;
-        int idx = 0;
+        int[][] matrix = new int[x.length][2];
 
+        for(int i=0;i<x.length;i++){
+            matrix[i][0] = x[i];
+            matrix[i][1] = y[i];
 
-        while(pos <= n){
-
-            if(idx < stations.length&&pos >= stations[idx] - w){
-                pos = stations[idx] + w + 1;
-                idx++;
-            }
-            else{
-                answer++;
-                pos += 2*w + 1;
-            }
         }
 
-        System.out.println(answer);
-
-        /*
-        boolean[] check = new boolean[n+1];
-
-        for(int i=0;i<stations.length;i++){
-            for(int j=stations[i]-w;j<=stations[i]+w;j++){
-                check[i] = true;
+        Arrays.sort(matrix, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                return o1[0]-o2[0];
             }
+        });
+
+        int max = 0;
+        for(int i=0;i<matrix.length-1;i++){ // 유클리드 거리 구하기
+            double sum = 0;
+            for(int j=0;j<2;j++){
+                sum += Math.pow(matrix[i][j]-matrix[i+1][j], 2);
+            }
+            max = Math.max(max,(int)Math.ceil(Math.sqrt(sum)));
+
         }
 
-        int pos = 1;
-
-        while(pos < n+1){
-            if(!check[pos]){
-                for(int i=pos;i<pos+2*w;i++){
-                    if(i > n){
-                        break;
-                    }
-                    check[i] = true;
-                }
-                answer++;
-                pos = pos + 2 * w + 1;
-            }
-            pos++;
-        }
-
-        System.out.println(answer);
-        */
-
-
+        System.out.println(max);
 
     }
 }

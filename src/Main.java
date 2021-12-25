2776b45976b82c1fe5a106438f01b59ea115cc98
Name <thdgorhkd97@naver.com>
2021-12-25 21:49:32 +0900

    1225

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,75 @@ import java.util.*;
 
 public class Main {
 
-    // programmers level 3 - 여행 경로
-    // 정확성은 맞는데 또 효율성에서 문제가 생긴다 ㅠㅠㅠ
-    // 출발지를 두고 for문을 돌리면서 가능한 도착 루트를 정렬하여 다음 도착지를 정했는데
-    // 이런 방식이 너무 시간이 오래 걸린다.
-    // 효율성을 맞추기 위해서 서치해본 결과 dfs 방식을 활용해야 했다.
-    // 도착하지 않았고 출발지가 일치한다면 그 다음 도착지를 넣고
-    // 다시 visited[i]를 false로 바꿔서 모든 경로를 다시 볼 수 있도록 한다.
-    // programmers를 돌려보면서 공부 한 뒤라서 이제 남은 문제들이 거의 다 내가 어디가 막혔느지
-    // 알거나 해결하기 어려운 문제들만 남아있다.
-    // 계속 찾아보면서 이해하고 공부하는 게 과연 공부가 되는 건지 싶지만 이해하고 습득하면서
-    // 비슷한 문제가 나온다면 해결할 수 있도록 하는 게 좋지 않을까 싶다.
-
-    static boolean[] visited;
-    static ArrayList<String> answers;
+    // B 사 코딩테스트 6번 문제
+    // 월요일과 금요일의 정해진 출퇴근 시간이 주어지면 여행지와 해당 여행지를 갈때 소모되는 휴가시간을
+    // 최대로 사용할 수 있는 경로를 구하라.
+    // 여행지별로 사용되는 휴가 시간을 map으로 넣어서 비교했다.
+    // 그 후에 사용되는 휴가 시간을 정렬하여 가장 휴가 시간을 잘 활용할 수 있는 여행지를 골라서
+    // 정답으로 선정한다.
+    // 이때 am과 pm으로 시간이 나누어지기 때문에 각각 am과 pm일 때를 구분하고
+    // string으로 주어지는 시간을 계산하여 int로 비교했다.
+    // 문제 자체를 해결하는 데는 큰 어려움은 없었다고 생각한다.
+    // 근데 문제에서의 조건을 조금 놓친 건 아닐까 싶은 생각도 있다.
+    // 휴가 시간을 잘 활용하는 마지막 경로를 찾아야 하고 월요일 오전과 금요일 오후를 따로
+    // 계산해서 시간을 계산하는 방식이 조금 번거로웠다.
 
     public static void main(String[] args) {
 
-        String[][] tickets = {
-                {"ICN","SFO"},
-                {"ICN","ATL"},
-                {"SFO","ATL"},
-                {"ATL","ICN"},
-                {"ATL","SFO"}};
+        String[][] plans = {{"홍콩","06:45 am"},{"일본","10:10 pm"}};
+        int time = 75;
 
-        visited = new boolean[tickets.length];
-        answers = new ArrayList<>();
 
-        int cnt = 0;
-        dfs(cnt,"ICN","ICN",tickets);
+        HashMap<String, Double> map = new HashMap<>(); // 도시=키 / 도시로 출발할 때 사용하는 휴가시간을 value
 
-        Collections.sort(answers);
+        for(int i=0;i<plans.length;i++){
+            int startTime = 0; // 출발 시간에 관한 변수
+            map.put(plans[i][0],0.0); // 해시에 도시명을 키로 해서 저장
+            if(plans[i][1].contains("P")) { // 출발 시간이 PM이면
+                startTime = Integer.parseInt(plans[i][1].substring(0, plans[i][1].indexOf("P")));
+                // P 이전의 숫자(시간) 을 startTime 변수에 지정
+                if(startTime < 6){ // 만약 6시 이전에 출발하면 휴가 시간을 사용하는 것이니까 map 갱신
+                    map.put(plans[i][0],map.get(plans[i][0])+(double)6-startTime);
+                }
+            }
+            else{ // 출발 시간이 AM이면
+                startTime = Integer.parseInt(plans[i][1].substring(0, plans[i][1].indexOf("A")));
+                if(startTime > 9.5){ // 9시 반 넘어서 출발하면 12 ~ 6시까지의 6시간과 (12-startTime) 오전 빼는 시간을 더해야 한다.
+                    map.put(plans[i][0],map.get(plans[i][0])+(double)6+12-startTime);
+                }
+                else{ // 9시 이전에 출발하는 거면 금요일을 통째로 안 가니까 8.5시간을 더한다.
+                    map.put(plans[i][0],map.get(plans[i][0])+(double)8.5);
+                }
+            }
 
-        String[] answer = answers.get(0).split(" ");
-//        return answer;
-    }
-
-    public static void dfs(int cnt, String present, String answer, String[][] tickets){
-        if(cnt == tickets.length){
-            answers.add(answer);
-            return ;
+            int arriveTime = 0; // 도착시간에 대한 변수
+            if(plans[i][2].contains("P")) { // 월요일 오후에 도착하면
+                arriveTime = Integer.parseInt(plans[i][2].substring(0, plans[i][2].indexOf("P")));
+                if( arriveTime < 6 && arriveTime > 1){ // 월요일은 1시 출근이므로 1시에서 넘은 만큼 휴가 시간 사용
+                    map.put(plans[i][0],map.get(plans[i][0])+(double)arriveTime-1);
+                }
+                else if(arriveTime >= 6){ // 만약 퇴근 시간을 지나서 왔다면 월요일 근무시간인 5시간 제외
+                    map.put(plans[i][0],map.get(plans[i][0])+(double)arriveTime-5);
+                }
+            }
+            // 월요일 오전에 도착하면 휴가 시간을 제외하지 않으니까 else문은 필요 없다.
         }
 
-        for(int i=0;i< tickets.length;i++){
-            if(!visited[i] && tickets[i][0].equals(present)){
-                visited[i] = true;
-
-                dfs(cnt + 1, tickets[i][1], answer + " " + tickets[i][1], tickets);
-                visited[i] = false;
+        String answer = "";
+        ArrayList<Double> list = new ArrayList<>();
+        for(String str : map.keySet()){
+            if(map.get(str) <= time) { // 휴가 시간 내의 휴가지를 모두 선택
+                list.add(map.get(str));
             }
         }
+        Collections.sort(list,Collections.reverseOrder()); // 정렬
+        for(String str : map.keySet()){
+            if(map.get(str) == list.get(0)){ // 휴가 시간을 가장 잘 활용하는 여행지 선택(올해의 마지막 여행이므로)
+                answer = str;
+            }
+        }
+//        return answer;
 
-        return;
+
     }
 }

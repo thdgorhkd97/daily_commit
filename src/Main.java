42e8d252f09f95b00bee78fa146ba5bea818845e
Name <thdgorhkd97@naver.com>
2022-01-07 23:51:33 +0900

    0107

--- src/Main.java
+++ src/Main.java
@@ -5,90 +5,68 @@ import java.util.*;
 
 public class Main {
 
-    // baekjoon 17298 오큰수
-    // stack을 활용하여 시간초과에 걸리지 않는 풀이법에 대해서 이해하느라 시간이 오래 걸림 ㅠㅠ
-    // 일단 원리자체는 수열의 인덱스를 특정 기준(현재 원소가 이전의 원소보다 작을 때까지)
-    // 을 만족하면 index를 stack에 추가하는 것이다.
-    // 그러다가 현재 원소가 top 원소를 인덱스로 하는 수열의 원소보다 크게 되면
-    // stack의 원소를 pop하면서 해당 인덱스에 해당하는 원소를 현재 원소로 바꾼다.
-    // https://st-lab.tistory.com/196 이 사이트에서 설명 가져왔습니다(설명 어렵네요..)
+//    정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+//              X가 3으로 나누어 떨어지면, 3으로 나눈다.
+//              X가 2로 나누어 떨어지면, 2로 나눈다.
+//              1을 뺀다.
+//    정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+
+//              X = 10 인 경우
+//            1
+//            1
+//            2         오답  정답
+//            5     :    0    3
+//            6     :    1    2
+//            7     :    0    3
+//            8     :    1    3
+//            9     :    2    2
+//            10    :    1    3
+
+    // baekjoon 1로 만들기
+    // 먼저 내가 잘못 생각한 부분은 빨리 1로 만들어야 하기 때문에 최대한 3으로 나누거나 2로 나누는 경우를 먼저 체크했다는 것이다.
+    // 예시에서도 나오지만 10의 경우 2로 먼저 나눠서 5로 가는 거보다 1을 먼저 빼고 9를 3으로 나누는 경우가 더 최소의 경우다.
+    // 따라서 3가지의 경우를 모두 체크할 수 있어야 하기 때문에 동적계획법을 활용한다.
+    // 이런 부분에서 내가 가장 먼저 생각한 부분은 2의 배수라면 dp[i/2]와 dp[i-1] 중 더 작은 값에서 1을 더해야 한다는 것인데
+    // 로직은 맞는 것 같은데 오답이 생겼다...
+    // 그래서 답과 비교해보니 답은 내 생각과는 다르게 dp[i] = dp[i-1] + 1을 if문 밖으로 빼서 계산하고 Math.min을 확인했다.
+    // 내 경우와 다른게 뭘까 싶어서 10을 넣어서 확인했는데 5에서부터 정답과 달랐다....
+    // 생각해보니까 5의 경우 if에 걸리지 않으니까 if에 해당해야만 초기화를 하는 나의 코드에서는 5에 대한 경우에 0을 삽입하는 걸 확인할 수 있었다.
+    // 그렇기 때문에 dp[i] = dp[i-1]+1 을 if문 밖으로 빼서 2나 3의 배수가 아닐 경우 1을 더한다는 문제의 조건을 챙겼어야 한다.
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        int X = Integer.parseInt(br.readLine());
 
-        int[] N = new int[T];
+        int[] dp = new int[X+1];
 
-        String str = br.readLine();
+        dp[0] = 0;
+        dp[1] = 0;
 
-        StringTokenizer stk = new StringTokenizer(str," ");
+        // 내가 생각한 코드
+//        for(int i=2;i<=X;i++){
+//            if(i % 2 == 0){
+//                dp[i] = Math.min(dp[i-1]+1,dp[i/2]+1);
+//            }
+//            if(i % 3 == 0){
+//                dp[i] = Math.min(dp[i-1]+1,dp[i/3]+1);
+//            }
+//        }
 
-        int idx = 0;
-        while(stk.hasMoreTokens()){
-            N[idx++] = Integer.parseInt(stk.nextToken());
+        // 정답 코드
+        for (int i = 2; i <= X; i++){
+            dp[i] = dp[i-1] + 1;
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
         }
 
-        int[] NGE = new int[T];
-        Stack<Integer> stack = new Stack<>();
-
-        for(int i=0;i<T;i++){
-            /*
-             * 스택이 비어있지 않으면서
-             * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
-             * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
-             * 해당 인덱스의 값을 현재 원소로 바꿔준다.
-             */
-            while(!stack.isEmpty() && N[stack.peek()] < N[i]){
-                N[stack.pop()] = N[i];
-            }
-
-            stack.push(i);
+        for(int i=2;i<=X;i++){
+            System.out.println(dp[i]);
         }
-
-
-        /*
-         * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
-         * -1로 초기화한다.
-         */
-        while(!stack.isEmpty()){
-            N[stack.pop()] = -1;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < T; i++) {
-            sb.append(N[i]).append(' ');
-        }
-
-        System.out.println(sb);
-
-
-
-        /* stack에 분류된 문제인데 그냥 for문으로 확인했을 때 시간초과가 발생한다.
-        stack을 사용하지 않아서 발생한 문제인가...
-
-        for(int i=0;i<T-1;i++){
-            boolean isBig = false;
-            for(int j=i+1;j<T;j++){
-                if(N[i] < N[j]){
-                    isBig = true;
-                    NGE[i] = N[j];
-                    break;
-                }
-            }
-
-            if(!isBig){
-                NGE[i] = -1;
-            }
-        }
-
-        NGE[T-1] = -1;
-
-        for(int i=0;i<T;i++){
-            System.out.print(NGE[i] + " ");
-        }*/
 
     }
+
+
 
 }

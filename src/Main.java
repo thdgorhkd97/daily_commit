1f789bfb4ec3bb6f443d7af52e29d24705df37d5
Name <thdgorhkd97@naver.com>
2022-01-13 23:55:08 +0900

    0113

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,25 @@ import java.util.*;
 
 public class Main {
 
-    // baekjoon 18258 - 큐 2
-//    push X: 정수 X를 큐에 넣는 연산이다.
-//    pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-//    size: 큐에 들어있는 정수의 개수를 출력한다.
-//    empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-//    front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-//    back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+    // baekjoon 2164 - 카드 2
 
-    // 큐를 구현하는 알고리즘인데 대부분 큐에 구현된 함수를 이용하면 문제가 해결되지만
-    // 큐의 가장 마지막 원소를 구현하는 건 따로 구현을 해야 했다.
-    // 나는 따로 static 변수를 선언하여 offer 함수를 수행할 때 마지막 수를 따로 변수에 적고
-    // 가장 뒤에 있는 정수는 가장 마지막에 들어간 정수이기 때문에 back을 수행할 때는
-    // static 변수를 출력하도록 구현햇다.
-    // 구현은 했지만 좀 더 찾아보니 큐를 LinkedList로 구현해서
-    // q.peekLast()로 마지막 원소를 출력하는 것도 가능하다고 한다.
+//    N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
+//
+//    이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
+//
+//    예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
+//
+//    N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
 
-    // java에서 큐에 대한 추가 정보!!
-    // 큐의 앞 부분인 front는 삭제 연산만 수행
-    // 큐의 뒷 부분 rear는 삽입 연산만 수행
-    // 주로 컴퓨터 버퍼에서 사용하며 대기열을 만들어 순차적으로 처리할 때 사용
-    // 데이터 삽입 : add, offer 중간이 아닌 맨 뒤로만 삽입 가능
-    // 데이터 삭제 : poll 맨 앞의 값 삭제, remove(Object) Object에 해당하는 값 삭제
-    //             poll -> 대기열이 비어있으면 null 반환
-    //             remove -> 대기열이 비어있으면 NoSuchElement 에러 반환
-    // 데이터 초기화 : clear
-    // 데이터 출력 : peek 맨 처름 넣은 값 확인 (모두 확인하려면 Iterator 사용)
-
-
-    static int late = 0;
+    // 처음에는 맨 앞의 원소를 제거하고 list.get(0)로 맨 앞원소를 저장한 다음
+    // 맨 앞 원소를 제거하고 저장해둔 원소를 큐에 offer했다.
+    // 하나 남을 때까지 반복해야 하므로 큐의 사이즈가 1이 되면 break 하였다.
+    // 근데 이렇게 하면 시간초과가 발생했다.
+    // 생각해보니 get으로 맨앞 원소를 찾아서 따로 저장한 다음 다시 저장하는 것이 아니라
+    // 맨 앞 원소를 제거하면서 offer 하는 것이 가능하다고 생각해서 코드를 수정했다.
+    // 틀렸습니다가 나왔다.
+    // 그 후 while(true) 후에 if로 큐의 사이즈를 확인하는 과정을
+    // while문의 조건문에 집어넣었더니 성공적으로 처리되었다.
 
     public static void main(String[] args) throws IOException {
 
@@ -41,45 +31,54 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
+//        LinkedList<Integer> list = new LinkedList<>();
+//
+//        for(int i=1;i<=N;i++){
+//            list.add(i);
+//        }
+//
+//        while(true){
+//            list.remove(0);
+//
+//            int move = list.get(0);
+//            list.remove(0);
+//            list.add(move);
+//
+//            if(list.size() == 1) {
+//                System.out.println(list.get(0));
+//                return ;
+//            }
+//        }     -> 시간초과 발생 코드
+
         Queue<Integer> que = new LinkedList<>();
 
-        for(int i=0;i<N;i++){
-            String str = br.readLine();
-
-            check(str,que);
+        for(int i=1;i<=N;i++){
+            que.offer(i);
         }
 
-    }
+        while(que.size() > 1){
 
-    public static void check(String str, Queue<Integer> q){
-        switch(str){
-            case "pop" :
-                if(q.isEmpty()) System.out.println("-1");
-                else System.out.println(q.poll());
-                return ;
-            case "size" :
-                System.out.println(q.size());
-                return ;
-            case "empty" :
-                if(q.isEmpty()) System.out.println("1");
-                else System.out.println("0");
-                return ;
-            case "front" :
-                if(q.isEmpty()) System.out.println("-1");
-                else System.out.println(q.peek());
-                return ;
-            case "back" :
-                if(q.isEmpty()) System.out.println("-1");
-                else System.out.println(late);
-                return ;
+            que.poll();
+
+            que.offer(que.poll());
+
         }
 
-        if(str.contains("push")){
-            int X = Integer.parseInt(str.substring(5));
-            late = X;
-            q.offer(X);
-            return ;
-        }
+        // 틀렸습니다 가 발생한 코드다.
+//        while(true){
+//
+//            que.poll();
+//
+//            que.offer(que.poll());
+//
+//            if(que.size() == 1){
+//                break;
+//            }
+//        }
+
+        System.out.println(que.poll());
+
+
 
     }
 }

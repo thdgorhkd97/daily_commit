bfa8cccb33af268a0c709db8167144257c6a7e4a
Name <thdgorhkd97@naver.com>
2022-01-14 23:44:17 +0900

    0114

--- src/Main.java
+++ src/Main.java
@@ -5,78 +5,76 @@ import java.util.*;
 
 public class Main {
 
-    // baekjoon 2164 - 카드 2
+    // programmers 신고 결과 받기
+    // hashmap을 활용해서 특정 조건에 맞는 key값에 해당하는 경우를 다 더하고
+    // int형 배열로 반환하는 문제
+    // hashmap에서 조금 문제가 생겼다. 처음에는 getOrDefault를 사용하면 될 줄 알았는데
+    // 자꾸 nullpointerexception이 발생해서 map.containskey()를 활용해보려 했다.
+    // 근데 분명 keyset으로 확인했을 때는 해당 키가 존재하는데 String으로 비교를 하니
+    // map.containskey()가 true가 반환되지 않았다...
+    // 그래서 조금 돌아가는 방법이기는 하지만 keyset을 통해 확인하는 과정에서
+    // if(id_list[i].equals(keyset)) 이렇게 아예 keyset과 비교하려고 했는데
+    // 이 경우 역시도 똑같은 문자열인데도 true로 들어가지 않는 문제가 발생했다...
+    // 문제 자체는 어렵지 않은 것 같은데 hash를 이용해서 key값이 일치하는지를 확인하고
+    // 그에 따라 value값을 저장하는 데 있어서 뭔가가 잘 안되는 것 같다 ㅠㅠ
 
-//    N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
-//
-//    이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
-//
-//    예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다.
-//
-//    N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
-
-    // 처음에는 맨 앞의 원소를 제거하고 list.get(0)로 맨 앞원소를 저장한 다음
-    // 맨 앞 원소를 제거하고 저장해둔 원소를 큐에 offer했다.
-    // 하나 남을 때까지 반복해야 하므로 큐의 사이즈가 1이 되면 break 하였다.
-    // 근데 이렇게 하면 시간초과가 발생했다.
-    // 생각해보니 get으로 맨앞 원소를 찾아서 따로 저장한 다음 다시 저장하는 것이 아니라
-    // 맨 앞 원소를 제거하면서 offer 하는 것이 가능하다고 생각해서 코드를 수정했다.
-    // 틀렸습니다가 나왔다.
-    // 그 후 while(true) 후에 if로 큐의 사이즈를 확인하는 과정을
-    // while문의 조건문에 집어넣었더니 성공적으로 처리되었다.
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
 
-        int N = Integer.parseInt(br.readLine());
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
 
-//        LinkedList<Integer> list = new LinkedList<>();
-//
-//        for(int i=1;i<=N;i++){
-//            list.add(i);
-//        }
-//
-//        while(true){
-//            list.remove(0);
-//
-//            int move = list.get(0);
-//            list.remove(0);
-//            list.add(move);
-//
-//            if(list.size() == 1) {
-//                System.out.println(list.get(0));
-//                return ;
-//            }
-//        }     -> 시간초과 발생 코드
+        int k = 2;
 
-        Queue<Integer> que = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        int[] answer = new int[id_list.length];
 
-        for(int i=1;i<=N;i++){
-            que.offer(i);
+        Set<String> set = new HashSet<>();
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String str : report){
+            if(set.add(str)){
+                list.add(str);
+            }
         }
 
-        while(que.size() > 1){
+        for(int i=0;i<list.size();i++){
+            String str = list.get(i);
 
-            que.poll();
+            int idx = str.indexOf(" ");
 
-            que.offer(que.poll());
+            String ing = str.substring(0,idx);
+            String pp = str.substring(idx);
 
+            map.put(pp, map.getOrDefault(pp,0)+1);
         }
 
-        // 틀렸습니다 가 발생한 코드다.
-//        while(true){
-//
-//            que.poll();
-//
-//            que.offer(que.poll());
-//
-//            if(que.size() == 1){
-//                break;
-//            }
-//        }
+        LinkedList<String> key = new LinkedList<>(map.keySet());
 
-        System.out.println(que.poll());
+        for(String keyset : key){
+            System.out.println(keyset + " " + map.get(keyset));
+
+            for(int i=0;i<id_list.length;i++){
+                System.out.println(id_list[i] + " 비교 " + keyset);
+                if(id_list[i].equals(keyset)){
+                    answer[i] = map.get(keyset);
+                    break;
+                }
+            }
+
+            System.out.println("-----------------------------------");
+        }
+
+        for(int i=0;i< id_list.length;i++){
+            System.out.println(answer[i]);
+        }
+
+
+
+
+
 
 
 

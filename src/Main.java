e88705c8aba49ff12c3a558f4c07d0e3f01e8af8
Name <thdgorhkd97@naver.com>
2022-01-16 23:00:03 +0900

    0116

--- src/Main.java
+++ src/Main.java
@@ -5,108 +5,76 @@ import java.util.*;
 
 public class Main {
 
-    // B사 코딩테스트 마지막 문제
-    // 삼각형 모양으로 구성되는 길이가 차이나는 문자열의 탑을
-    // 시계방향이나 반시계방향으로 돌렸을 때 나오는 문자열의 조합을 보낸다.
-    // 처음에는 어떻게 접근할 지 몰랐는데 여러가지 케이스를 두고 돌렸을 경우와
-    // 비교를 해보니 규칙을 찾을 수 있었다.
-    // 시계방향의 경우 오른쪽 아래에서부터 ㄴ자를 좌우반전시킨 모습의 모양을 가지는
-    // 인덱스의 조합을 돌리는 형태로 구현하면 가능하다.
-    // 규칙을 찾은 뒤부터는 index와 for문을 활용하는 문제였다.
-    // 다만 규칙을 찾는 과정이 쉽지 않았는데 단순히 하나만 보는 것이 아니라
-    // 줄을 추가하거나 길이를 줄이는 등 모든 경우에 적용 가능한 케이스를 찾기 위해
-    // 여러 케이스를 확인한 게 주요했다.
+    // programmers kakao blind test - 신고 결과 받기
+    // 2일전인가 3일전에 해봤던 문제인데 완벽히 해결하지 못해서 다시 시도해 보았다.
+    // 처음에는 문제에서 구하라는 부분을 잘못 이해해서 문제를 해결하던 도중에 로직을 끝내는 실수를 했었고
+    // substring을 하는 부분에서 startIdx는 바로 그 idx부터인데 신고당한 사람의 아이디를 잘못 자르는 실수를 했었다.
+    // 이번엔 조금 길수도 있지만 차근차근 문제에서 구하라는 그대로 따라가면서 문제를 해결해보았다.
+    // 다만 list나 set, 배열을 생각보다 많이 선언했기 때문에 메모리 측면에서는 조금 마음에 들지 않는다.
+    // String[] report를 잘라서 신고한 사람과 신고당한 사람을 나누는 과정을 2번 반복했는데 이러지 말고
+    // 아예 신고한 사람과 신고당한 사람에 대한 map을 다시 만드는 건 어땠을 까 싶다.
+
 
     public static void main(String[] args) throws IOException {
 
-        String[] grid = {"A","MAN","DRINK","WATER11"};
-        boolean clockwise = false;
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
 
-        String[] answer = new String[grid.length];
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
 
-        Character[][] map = new Character[grid.length][];
-        for(int i=0;i<map.length;i++){
-            map[i] = new Character[grid[i].length()];
-        }
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length();j++){
-                map[i][j] = grid[i].charAt(j);
+        int k = 2;
+
+        Map<String,Integer> map = new HashMap<>();
+
+        Set<String> set = new HashSet<>();
+        ArrayList<String> list = new ArrayList<>();
+
+        for(String str : report){
+            if(set.add(str)){ // 중복된 신고는 1회만 인정한다.
+                list.add(str);
             }
         }
 
-        for(int i=0;i<map.length;i++){
-            for(int j=0;j<map[i].length;j++){
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
+        for(String str : list){
+
+            int idx = str.indexOf(" "); // 공백을 기준으로 신고한 사람과 신고당한사람의 이름이 나뉘어진다.
+
+            String ing = str.substring(0,idx); // 신고한 사람
+            String pp = str.substring(idx+1); // 신고당한 사람
+
+            map.put(pp, map.getOrDefault(pp,0)+1); // 신고당한 사람의 횟수를 1더한다.
         }
-        System.out.println();
 
-        if(clockwise){
-            int pos = 0;
-            int idx = 0;
-            while(pos <= grid[grid.length-1].length()-1){
-                StringBuffer sb = new StringBuffer();
+        int[] answer = new int[id_list.length];
 
-                int row = grid.length-1;
-                int column = pos;
+        ArrayList<String> stopUser = new ArrayList<>(); // 정지당한 유저의 리스트
 
-                while(row>=0 && column >= 0){
-                    sb.append(map[row][column]);
-                    if(column-1 >=0){
-                        sb.append(map[row][column-1]);
-                        column -= 1;
-                    }
-                    else{
-                        break;
-                    }
-                    row -= 1;
-                    column -= 1;
-                }
-
-                answer[idx] = sb.toString();
-                System.out.println(answer[idx]);
-                idx += 1;
-                pos += 2;
+        for(int i=0;i< id_list.length;i++){
+            if(map.containsKey(id_list[i]) && map.get(id_list[i])>=k){ // 신고당한 적있고 그 횟수가 k번 이상이라면
+                stopUser.add(id_list[i]); // 정지당한 유저에 추가한다
             }
         }
 
-        else{
-            int idx = 0;
-            int len = grid.length-1;
+        Map<String, Integer> result = new HashMap<>();
+        for(String str : list){
+            int idx = str.indexOf(" ");
 
-            while(len >= 0) {
-                int pos = grid[len].length() - 1;
-                StringBuffer sb = new StringBuffer();
+            String ing = str.substring(0,idx); // 신고한 사람
+            String pp = str.substring(idx+1); // 신고당한 사람
 
-                int row = len;
-                int column = pos;
-
-                while(row <= grid.length-1){
-                    sb.append(map[row][column]);
-
-                    if(row + 1 > grid.length-1){
-
-                        answer[idx] = sb.toString();
-                        idx++;
-                        break;
-                    }
-                    else{
-                        sb.append(map[row+1][column+1]);
-
-                    }
-                    row += 1;
-                }
-
-                len--;
+            if(stopUser.contains(pp)){ // 신고당한 사람이 이미 정지된 유저라면
+                result.put(ing,result.getOrDefault(ing,0)+1); // 정지된 유저를 신고한 사람에 대해 1을 더한다.
             }
         }
 
+        for(int i=0;i<answer.length;i++){
+            if(result.containsKey(id_list[i])){ // 정지된 유저를 신고한 적 있다면
+                answer[i] = result.get(id_list[i]); // 정답 배열에서 1을 더한다.
+            }
+            else{
+                answer[i]=0;
+            }
 
-
-
-
-
+        }
 
 
 

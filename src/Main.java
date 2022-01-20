85d6a2e8c6f303152bc2371a3865c46869be6db4
Name <thdgorhkd97@naver.com>
2022-01-20 23:28:26 +0900

    0120

--- src/Main.java
+++ src/Main.java
@@ -5,62 +5,83 @@ import java.util.*;
 
 public class Main {
 
-    // programmers 줄 서는 방법
-    // 어제 대충 자릿수를 구해서 하면 되지 않을가 싶었는데 모든 경우에 통용되는 로직을
-    // 모르겠어서 결국 로직을 알아보고서야 해결했다 ㅠㅠ 이해하는데 너무 오래 걸렸다...
+    // java 1920 수 찾기
+    // N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+    // 배열에 있는지 확인하기 위해서 for문으로 하나하나 확인하다 보면 상당히 오래 걸린다.
+    // 이분탐색이 빠른 건 알고 있는데 얼마나 빠른지는 몰랐다.
+    // 이분탐색 : O(logN) 인 반면 for문으로 확인할 땐 O(N)이 나온다.
+    // 이분탐색은 반드시!! 정렬된 배열에서 사용 가능하다!!
+    // 이분탐색에서 min은 탐색 범위의 왼쪽 끝 인덱스를 의미하고 max는 가장 끝 인덱스를 의미한다.
+    // while문의 종료 조건은 min < max 이다(인덱스를 역전할 때까지)
+    // while문 내에서 mid값(중간값)을 구하고 배열내의 mid 인덱스 값과 value를 비교한다.
+    // 만약 arr[mid] > value 라면 arr 배열 내에서 더 앞쪽에 있다는 것이므로
+    // max = mid - 1로 해서 min과 max 의 범위를 value가 있는 arr 배열의 앞 부분으로 수정한다.
+    // 이런 식으로 value 값의 범위를 맞춰가면서 arr 과 비교한다.
 
+    // *** Arrays.binarySearch 메소드와의 비교 ***
+    // 위 방식과 거의 유사하지만 차이점은 음수값을 기존 리스트에서 어느 값 사이에 있는지 알 수 있도록
+    // 반환 한다는 차이가 있다고 한다.
+    // ex [2,4,7,8] 에서 3을 찾고자 하면 음수를 반환하는데 3은 2와 4사이에 있어야 하므로 -2를 반환
+    // 자바에서는 음수 값을 -(low+1)로 반환하기 때문이다.
 
     public static void main(String[] args) throws IOException {
 
-        int n = 3;
-        long k = 5;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<Integer> list = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
 
-        int[] answer = new int[n];
+        int[] arr = new int[N];
 
-        long factorial = 1;
-        for(int i=1;i<=n;i++){
-            factorial *= i; // n!을 구함 -> 가능한 모든 경우의 수
-            list.add(i);
-        }
-        k = k-1; // 배열의 idx는 0부터 시작이므로 k=5라면 전체 경우의 수 중 4번째 걸 구해야 한다.
+        String str = br.readLine();
+        StringTokenizer stk = new StringTokenizer(str," ");
 
-        int idx= 0;
-        while(n > 0){ // n--를 해가면서 n번째 까지의 idx를 추가해야 한다. ex) n=3이면 3번째 idx까지 지정
-            factorial /= n; // n번째 자리수를 결정
-
-            answer[idx++] = list.get((int) (k/factorial));
-
-            list.remove((int) (k / factorial));
-            k %= factorial;
-
-            n--;
-        }
-        // 처음에 factorial = 2가 되고 (2번째 자리수를 결정한다는 것)
-        // answer[idx = 0] = list.get((int) k=5/factorial=2)
-        //                 = list.get((int) 2)
-        //                 = list.get(2) = 3
-        // 그 후 list.remove로 3을 제외한다. 이미 줄 세웠으므로
-        // k = 5 % factorial = 2 -> k=1로 변경
-        // n = 3에서 2로 변경
-
-        // 2번째 while 문
-        // factorial = 1이 되고
-        // answer[idx = 1] = list.get((int) k=1/factorial=1)
-        //                 = list.get(1) = 1
-        // 그 후 list.remove로 1을 제외한다.
-        // k = 1 % 1 = 0이 된다.
-
-        // 이런 식으로 n번째 자리를 결정하기 위해서 k/factorial의 list idx를 결정한다.
-
-
-        for(int i=0;i<answer.length;i++){
-            System.out.print(answer[i]+" ");
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
+        Arrays.sort(arr);
 
+        for(int i=0;i<N;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
+        int M = Integer.parseInt(br.readLine());
+
+        str = br.readLine();
+        stk = new StringTokenizer(str," ");
+        for(int i=0;i<M;i++){
+            check(arr,Integer.parseInt(stk.nextToken()));
+        }
 
     }
+
+    static void check(int[] arr, int num){
+
+        int min = 0;
+        int max = arr.length-1;
+
+        if(num < arr[0] || num > arr[max]){
+            System.out.println("0");
+            return ;
+        }
+
+        while(min <= max){
+            int mid = (min+max) / 2;
+
+            if(arr[mid] > num){
+                max = mid - 1;
+            }
+            else if(arr[mid] < num){
+                min = mid + 1;
+            }
+            else{
+                System.out.println("1");
+                return ;
+            }
+        }
+
+        System.out.println("0");
+    }
+
 }

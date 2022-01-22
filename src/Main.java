749dcbbc207129c5c809a47b006a8ae3f74594c4
Name <thdgorhkd97@naver.com>
2022-01-22 23:57:13 +0900

    0122

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,16 @@ import java.util.*;
 
 public class Main {
 
-    // java 10816 - 숫자 카드 2
-    // 이분탐색으로 분류된 문제라서 이분탐색으로 풀어봤는데 결국 처음 생각한대로
-    // hashmap을 활용해서 마무리 하였다
-    // 우선 arr배열중에서 특정수가 얼마나 있는지 체크하는 문제였기 때문에 arr배열에 있는 수를
-    // hashmap에 집어넣고 몇개 있는지 찾고자 하는 수가 key로 존재한다면 그 value값을
-    // 존재하지 않는다면 0을 출력하도록 하여 해결하는 데는 전혀 문제가 없었다.
-    // 근데 이 문제는 이분탐색 문제로 분류되어 있었기 때문에 이분탐색으로 해결해보려고 노력했다.
-    // 먼저 이분탐색을 통해서 특정 수가 arr배열에 있는지 없는지를 확인하는 것은 그리 어렵지 않다.
-    // 근데 있다 없다가 아니라 몇 개 있는지를 알아내야 했기 때문에 내가 선택한 방식은
-    // mid인덱스를 기준으로 앞쪽과 뒤쪽으로 같은 원소가 있는지를 체크해서 idx의 범위를 구하는 방식이었다
-    // 만약 arr[mid]가 num과 같다면 num은 arr에 존재하는 것이기 때문에 arr[mid-1], arr[mid-2]...이런 식으로
-    // arr[mid]를 기준으로 앞과 뒤를 보면서 arr배열에서 num과 같은 원소를 가지는 인덱스의 범위를 구하는 것이다.
-    // 이렇게 나는 그 원소가 arr에 있는지 확인하고 있다면 인덱스의 범위를 찾는 방식으로 했는데
-    // 조금 찾아보니 처음부터 원소의 upper bound와 lower bound를 찾는 방식이 있었다.
-    // 찾고자 하는 수의 이상과 초과를 가지는 원소의 인덱스를 찾는 방식이다.
-    // 만약 arr 배열에 원소가 있다면 나와 같은 방식으로 arr배열의 인덱스 범위가 나오고
-    // arr 배열에 없다면 이상과 초과를 뜻하는 인덱스가 같은 인덱스를 가르키게 되기 때문에
-    // 존재하지 않는다는 것을 쉽게 알 수 있는 방법이 있다.
+    // java baekjoon 1655 가운데를 말해요
+    // 문제에서 요구하는 건 쉽다. 정렬된 값들 중 중간값을 찾아서 리턴하면 된다.
+    // 근데 N의 범위가 100,000이기 때문에 반드시 시간초과가 발생할 것이므로
+    // 우선순위 큐를 활용하기로 했다 (우선순위 큐에 분류된 문제지만 왜 우선순위 큐를 활용해야 할까 생각했다)
+    // 최대 힙과 최소 힙의 크기가 같은 경우에는 반드시 최대 힙으로 푸시한다
+    // 최대 힙과 최소 힙의 크기가 같다면 중간 값중 작은 수를 말해야 하기 때문이다.
+    // 그럼 그냥 푸시된 최대 힙의 peek()를 리턴하면 된다.
+    // 근데 크기가 다를 때 최대 힙에 수를 푸시했을 때 만약 최대 힙의 peek가 최소힙의 peek와
+    // 크기 비교했을 때 최대 힙의 peek가 더 크면 최소 힙과 위치를 바꿔줘야 한다.
+    // 그래야지 홀수개 일때 최대 힙의 peek값이 중간값이 되기 때문이다.
 
     public static void main(String[] args) throws IOException {
 
@@ -30,93 +22,33 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        PriorityQueue<Integer> maxQue = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minQue = new PriorityQueue<>();
 
-        String str = br.readLine();
+        for(int i=0;i<N;i++){
+            int num = Integer.parseInt(br.readLine());
 
-        StringTokenizer stk = new StringTokenizer(str, " ");
+            if(maxQue.size() == minQue.size()){ // 크기가 같으면 최대힙에 넣는다
+                maxQue.add(num);
 
-        HashMap<Integer,Integer> map = new HashMap<>();
+                if(!minQue.isEmpty() && maxQue.peek() > minQue.peek()){
+                    minQue.add(maxQue.poll());
+                    maxQue.add(minQue.poll());
+                }
+            }
+            else{
+                minQue.add(num);
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(stk.nextToken());
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+                if(maxQue.peek() > minQue.peek()){
+                    minQue.add(maxQue.poll());
+                    maxQue.add(minQue.poll());
+                }
+            }
+
+            System.out.println(maxQue.peek());
         }
 
-        Arrays.sort(arr);
-
-        int M = Integer.parseInt(br.readLine());
-
-        str = br.readLine();
-        stk = new StringTokenizer(str, " ");
-
-        int[] answer = new int[M];
-
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<M;i++){
-            int num = Integer.parseInt(stk.nextToken());
-
-            sb.append(map.getOrDefault(num,0)).append(' ');
-
-        }
-
-        System.out.println(sb.toString());
 
 
-
-//        int[] answer = new int[M];
-//
-//        for(int i=0;i<M;i++){
-//            int num = Integer.parseInt(stk.nextToken());
-//            answer[i] = check(arr,num);
-//        }
-//
-//
-//        for(int i=0;i<M;i++){
-//            System.out.print(answer[i] + " ");
-//        }
-//
-//
-//    }
-//
-//    static int check(int[] arr, int num){
-//
-//        int min = 0;
-//        int max = arr.length-1;
-//        int cnt = 0;
-//        while(min < max){
-//            int mid = (min+max) / 2;
-//
-//            if(arr[mid] > num){
-//                max = mid - 1;
-//            }
-//            else if(arr[mid] < num){
-//                min = mid + 1;
-//            }
-//            else{ // arr에 num이 존재합니다
-//                int left = 0;
-//                int right = arr.length-1;
-//
-//                for(int i=mid;i>=0 ;i--){
-//                    if(arr[i] != num){
-//                        left = i+1;
-//                        break;
-//                    }
-//                }
-//                for(int i=mid+1;i<arr.length;i++){
-//                    if(arr[i] != num){
-//                        right = i-1;
-//                        break;
-//                    }
-//                }
-//                cnt = right - left + 1;
-//                break;
-//            }
-//        }
-//
-//        return cnt;
-//
-//    }
-//}
     }
 }

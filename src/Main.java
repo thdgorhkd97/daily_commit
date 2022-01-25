8d336e08ee3810b5544e3e1282f59d0e180a59ea
Name <thdgorhkd97@naver.com>
2022-01-25 22:48:41 +0900

    0125

--- src/Main.java
+++ src/Main.java
@@ -5,112 +5,57 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 2798 - 블랙잭
-    // 3장을 골라서 가장 M에 가까운 조합을 고르는 문제
-    // 3중 for문을 활용하였고 문제 조건에 의해서 M을 넘지 않는 조건과 가장 가까운 조건을 추가하여
-    // 최소의 차이를 가지는 조합을 구하면 되는 문제
+    // java 1436 영화감독 숌숌
 
-    // java baekjoon 7568 - 덩치
-    // 각 사람의 키와 몸무게를 Body class로 저장하여 반복문을 통해 비교하였다.
-    // man[i] = new Body(0,0); 생성한 클래스 객체를 new 키워드를 통해서
-    // 실제 객체를 생성해주지 않아서 NullPointerException이 발생했었다.
+//    종말의 숫자란 어떤 수에 6이 적어도 3개이상 연속으로 들어가는 수를 말한다.
+//    제일 작은 종말의 숫자는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, .... 과 같다.
+//
+//    따라서, 숌은 첫 번째 영화의 제목은 세상의 종말 666, 두 번째 영화의 제목은 세상의 종말 1666 이렇게 이름을 지을 것이다.
+//    일반화해서 생각하면, N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 숫자) 와 같다.
+//
+//    숌이 만든 N번째 영화의 제목에 들어간 숫자를 출력하는 프로그램을 작성하시오.
 
-    // 따로 어떤 자료구조나 알고리즘이 필요한 문제가 아니라 문제의 조건에 맞춰서
-    // 모든 조건을 확인하는 반복문을 활용한 문제였기 때문에 2문제를 해결했다.
+    // 666이 들어가는지를 문자열로 바꾸고 contains로 확인한다.
+    // 그 666이 들어가는 숫자가 몇 번째 숫자인지 확인하고 그 숫자의 순서가 알고자 하는 순서와 같다면
+    // 그 숫자를 출력한다. (근데 이 방법은 소위 말해 노가다 방법이기 때문에 조금 효율적인 방법이 뭐가 있을까 찾아보았다)
 
+    // 인터넷의 가장 일반적인 방법으로는 자릿수를 나눠서 계산하는 방식이다
+    // 예를 들어 맨 앞자리가 몇 인지를 확인하고 순서를 계산한다. 다만 이때 맨 앞자리가 6이라면
+    // 666으로 시작하는 경우가 있기 때문에 0~9까지를 카운트한다.
+    // 이런 방식인건데 이 방법도 개발자가 가능한 케이스를 모두 계산해서 앞자리의 숫자에 따라
+    // 가능한 케이스를 모두 계산해서 프로그래밍해야 하기 때문에 더 나은 방법이 확실한가는 솔직히 잘 모르겠다..
 
-    public static class Body{
-        int height;
-        int weight;
-
-        public Body(int height, int weight){
-            this.height = height;
-            this.weight = weight;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        Body[] man = new Body[N];
-
-        for(int i=0;i<N;i++) {
-            String str = br.readLine();
-
-            StringTokenizer stk = new StringTokenizer(str," ");
-
-            man[i] = new Body(0,0);
-
-            int kg = Integer.parseInt(stk.nextToken());
-            int cm = Integer.parseInt(stk.nextToken());
-
-            man[i].height = cm;
-            man[i].weight = kg;
-
-        }
-
-        int[] grade = new int[N];
-
-        for(int i=0;i<N;i++){
-            int cnt = 0;
-            for(int j=0;j<N;j++){
-                if(man[i].height < man[j].height && man[i].weight < man[j].weight){
-                    cnt++;
-                }
-            }
-
-            grade[i] = cnt+1;
-        }
-
-        for(int i=0;i<N-1;i++){
-            System.out.print(grade[i] + " ");
-        }
-        System.out.println(grade[N-1]);
-
-
-
-
         /*
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String N_M = br.readLine();
-
-        StringTokenizer stk = new StringTokenizer(N_M," ");
-
-        int N = Integer.parseInt(stk.nextToken());
-        int M = Integer.parseInt(stk.nextToken());
-
-        String card = br.readLine();
-
-        stk = new StringTokenizer(card," ");
-
-        int[] cards = new int[N];
-
-        for(int i=0;i<N;i++){
-            cards[i] = Integer.parseInt(stk.nextToken());
-        }
-
-        Arrays.sort(cards); // card 목록을 오름차순으로 정렬
-
-        int blackJack = Integer.MAX_VALUE;
-
-        // 카드의 개수(N) >=3 이기 때문에 예외조건 필요 X
-        for(int i=0;i<cards.length;i++){
-            for(int j=i+1;j<cards.length;j++){
-                for(int k=j+1;k<cards.length;k++){
-
-                    int cardSum = cards[i] + cards[j] + cards[k];
-                    if(cardSum  <= M) // M을 넘지 않는 3장의 조합
-                        blackJack = Math.min(blackJack,M-cardSum); // M과 가까울수록 작으므로
+        while(true){
+            if(String.valueOf(num).contains("666")){
+                if(N == idx){ // 이 조건을 if로 확인하지 말고 while문의 조건으로 빼자
+                    break;
                 }
+                idx++;
             }
+            num++;
+        }*/
+
+        int num = 666;
+        int idx = 1;
+        while(idx != N){
+
+            num++;
+            if(String.valueOf(num).contains("666")){
+
+                idx++;
+            }
+
         }
 
-        System.out.println(M-blackJack); // blackJack이 M과의 최소 차이이므로
-         */
+        System.out.println(num);
+
     }
 
 }

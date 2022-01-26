4f14aa3f3e563a38ea04848d90481009f87e7161
Name <thdgorhkd97@naver.com>
2022-01-26 22:45:38 +0900

    0126

--- src/Main.java
+++ src/Main.java
@@ -5,57 +5,79 @@ import java.util.*;
 
 public class Main {
 
-    // java 1436 영화감독 숌숌
+    // java 가장 큰 수
+    // 문자열 기준 [ (기준값).compareTo(비교대상) ] -> 기준값에 비교대상이 포함되어 있으면 문자열 길이 차이 리턴
+    //                                         -> 맨 앞에서 다르다면 아스키값으로 처리
+    //                                         -> 비교 불가능한 지점에서 아스키값으로 비교 처리
 
-//    종말의 숫자란 어떤 수에 6이 적어도 3개이상 연속으로 들어가는 수를 말한다.
-//    제일 작은 종말의 숫자는 666이고, 그 다음으로 큰 수는 1666, 2666, 3666, .... 과 같다.
-//
-//    따라서, 숌은 첫 번째 영화의 제목은 세상의 종말 666, 두 번째 영화의 제목은 세상의 종말 1666 이렇게 이름을 지을 것이다.
-//    일반화해서 생각하면, N번째 영화의 제목은 세상의 종말 (N번째로 작은 종말의 숫자) 와 같다.
-//
-//    숌이 만든 N번째 영화의 제목에 들어간 숫자를 출력하는 프로그램을 작성하시오.
+    // java 124 나라의 숫자
+    // 1,2,4 라는 숫자로만 해결이 돼야하기 때문에 3의 나머지로 해결하는 3진법과 같은
+    // 방법을 떠올렷다. 근데 3으로 나누어 떨어지는 경우에 문제가 생겼다.
+    // 3으로 나누어 떨어지는 경우에 124나라의 숫자에 맞춰 숫자를 넣고 로직이 마무리 되어야 하는데
+    // 3으로 나누어 떨어지면 나머지가 1이 남아서 숫자를 하나 새로 넣는 문제가 있었다.
+    // 해당 경우를 어떻게 처리해야 할까 고민해보면서 여러 조건을 추가했었는데 그냥
+    // if문을 하나 추가해서 만약 3으로 나누어 떨어지는 경우라면 1을 빼기로 했다.
+    // 그렇게 처리함으로써 3의 배수인 수가 한번 더 수가 들어가는 경우를 조심했다.
 
-    // 666이 들어가는지를 문자열로 바꾸고 contains로 확인한다.
-    // 그 666이 들어가는 숫자가 몇 번째 숫자인지 확인하고 그 숫자의 순서가 알고자 하는 순서와 같다면
-    // 그 숫자를 출력한다. (근데 이 방법은 소위 말해 노가다 방법이기 때문에 조금 효율적인 방법이 뭐가 있을까 찾아보았다)
-
-    // 인터넷의 가장 일반적인 방법으로는 자릿수를 나눠서 계산하는 방식이다
-    // 예를 들어 맨 앞자리가 몇 인지를 확인하고 순서를 계산한다. 다만 이때 맨 앞자리가 6이라면
-    // 666으로 시작하는 경우가 있기 때문에 0~9까지를 카운트한다.
-    // 이런 방식인건데 이 방법도 개발자가 가능한 케이스를 모두 계산해서 앞자리의 숫자에 따라
-    // 가능한 케이스를 모두 계산해서 프로그래밍해야 하기 때문에 더 나은 방법이 확실한가는 솔직히 잘 모르겠다..
 
    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int n = 10;
 
-        int N = Integer.parseInt(br.readLine());
+       int remain = 0;
 
-        /*
-        while(true){
-            if(String.valueOf(num).contains("666")){
-                if(N == idx){ // 이 조건을 if로 확인하지 말고 while문의 조건으로 빼자
-                    break;
-                }
-                idx++;
-            }
-            num++;
-        }*/
+       ArrayList<Integer> list = new ArrayList<>();
+       while(n != 0){
+           remain = n % 3;
+           switch (remain){
+               case 0 : list.add(4); break;
+               case 1 : list.add(1); break;
+               case 2 : list.add(2); break;
+           }
 
-        int num = 666;
-        int idx = 1;
-        while(idx != N){
+           if(n % 3 == 0){ // 3의 배수라면
+              n--;
+           }
+           n /= 3;
+       }
 
-            num++;
-            if(String.valueOf(num).contains("666")){
+       StringBuffer sb = new StringBuffer();
+       for(int i=list.size()-1;i>=0;i--){
+           sb.append(String.valueOf(list.get(i)));
+       }
 
-                idx++;
-            }
+       System.out.println(sb.toString());
 
-        }
+       /* java - 가장 큰 수 (programmers level 2)
+       int[] numbers = {2,10,6};
 
-        System.out.println(num);
+       String[] result = new String[numbers.length];
 
+       int idx = 0;
+       for(Integer num : numbers){
+           result[idx++] = String.valueOf(num);
+       }
+
+       Arrays.sort(result, new Comparator<String>() {
+           @Override
+           public int compare(String o1, String o2) {
+               // 문자열을 합친 것이기 때문에 그대로 합쳐진 문자열이 리턴된다.
+               // ex) 6과 10을 비교한다면 610.compareTo(106)이렇게 된다.
+               // 이 상황에서 610이 더 크므로 양수를 반환하기 때문에 순서를 바꾸지 않는다.
+               // 반면 10,6이라서 반대로 되어 있다면 음수를 반환하기 때문에 6이 앞으로 위치를 바꾸게 된다.
+               return (o2+o1).compareTo(o1+o2);
+           }
+       });
+
+//       Arrays.sort(result,((o1, o2) -> (o2+o1).compareTo(o1+o2)));
+
+
+       StringBuffer sb = new StringBuffer();
+       for(int i=0;i<result.length;i++){
+           sb.append(result[i]);
+       }
+
+       System.out.println("정답 : "+sb.toString());
+        */
     }
-
 }

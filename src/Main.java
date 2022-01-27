b6995806bc40e9c0d67993140a598f650ed3aabc
Name <thdgorhkd97@naver.com>
2022-01-27 23:05:32 +0900

    0127

--- src/Main.java
+++ src/Main.java
@@ -5,79 +5,99 @@ import java.util.*;
 
 public class Main {
 
-    // java 가장 큰 수
-    // 문자열 기준 [ (기준값).compareTo(비교대상) ] -> 기준값에 비교대상이 포함되어 있으면 문자열 길이 차이 리턴
-    //                                         -> 맨 앞에서 다르다면 아스키값으로 처리
-    //                                         -> 비교 불가능한 지점에서 아스키값으로 비교 처리
+    // java baekjoon 2108 - 통계학
+    // N개의 숫자가 들어왔을 때
+//    산술평균 : N개의 수들의 합을 N으로 나눈 값
+//    중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+//    최빈값 : N개의 수들 중 가장 많이 나타나는 값
+//    범위 : N개의 수들 중 최댓값과 최솟값의 차이
+    // 을 구하라
 
-    // java 124 나라의 숫자
-    // 1,2,4 라는 숫자로만 해결이 돼야하기 때문에 3의 나머지로 해결하는 3진법과 같은
-    // 방법을 떠올렷다. 근데 3으로 나누어 떨어지는 경우에 문제가 생겼다.
-    // 3으로 나누어 떨어지는 경우에 124나라의 숫자에 맞춰 숫자를 넣고 로직이 마무리 되어야 하는데
-    // 3으로 나누어 떨어지면 나머지가 1이 남아서 숫자를 하나 새로 넣는 문제가 있었다.
-    // 해당 경우를 어떻게 처리해야 할까 고민해보면서 여러 조건을 추가했었는데 그냥
-    // if문을 하나 추가해서 만약 3으로 나누어 떨어지는 경우라면 1을 빼기로 했다.
-    // 그렇게 처리함으로써 3의 배수인 수가 한번 더 수가 들어가는 경우를 조심했다.
+    // 다른것들은 다 쉬운거였는데 생각보다 최빈값을 구하는 과정에서 시간이 걸렸다.
+    // 좋은 로직으로 빠르게 해결할 수 있는 방법이 없을까 싶어서 처음에는 map을 활용해볼까 하다
+    // map에 넣고나서 정렬을 한 후에 가장 큰 값이 하나인지 둘인지 확인하고
+    // 그 값을 풀어야 하는 상황인건데 별로 좋지 않다고 생각했다.
+    // 그게 더 빠를순 있겠지만 로직에 대한 확신이 안 들어서 조금 돌아가더라도 확실하게 처리하려 했다.
+    // 우선 입력되는 정수의 절댓값이 4000을 넘지 않는다는 문제 조건은 -4000 ~ 4000이라는 소리기 때문에
+    // 각 정수가 몇번 입력되는지 확인하기 위한 many[] 배열에 추가한다.
+    // 단 음수를 확인할 수 없기에 4000을 더해서 넣었다. 0 -> mamy[4000]++ 이런 식으로
+    // 그 후에 가장 많은 최빈값을 확인하고 최빈값과 같다면 list에 추가한다.
+    // 최빈값이 하나라면 list의 사이즈가 1이기 때문에 그대로 출력하고
+    // 최빈값이 여러개라서 list에 여러개가 들어간다면 list를 정렬한 후에 두번째값을 출력한다.
 
 
    public static void main(String[] args) throws IOException {
 
-       int n = 10;
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       int remain = 0;
+       int N = Integer.parseInt(br.readLine());
 
+       int[] num = new int[N];
+
+       for(int i=0;i<N;i++){
+           num[i] = Integer.parseInt(br.readLine());
+       }
+
+       Arrays.sort(num);
+
+       // 산술평균 : N개의 수들의 합을 N으로 나눈 값
+       int sanSool = 0;
+       int sum = 0;
+       for(int i=0;i<N;i++){
+           sum += num[i];
+       }
+       sanSool = (int)Math.round((double)sum / N);
+
+       // 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+       int joongAng = 0;
+       joongAng = num[N/2];
+
+       // 최빈값 : N개의 수들 중 가장 많이 나타나는 값(여러 개 일 경우 2번째로 작은 값)
+       int most = 0;
+       int[] many = new int[8001]; // -4000 ~ 4000
        ArrayList<Integer> list = new ArrayList<>();
-       while(n != 0){
-           remain = n % 3;
-           switch (remain){
-               case 0 : list.add(4); break;
-               case 1 : list.add(1); break;
-               case 2 : list.add(2); break;
+
+       for(int i=0;i<num.length;i++){
+           many[num[i] + 4000]++;
+       }
+
+       int max = 0;
+       for(int i=0;i<many.length;i++){
+           if(max < many[i]){
+               max = many[i];
            }
+       }
 
-           if(n % 3 == 0){ // 3의 배수라면
-              n--;
+       for(int i=0;i<many.length;i++){
+           if(many[i] == max){
+               list.add(i - 4000);
            }
-           n /= 3;
        }
 
-       StringBuffer sb = new StringBuffer();
-       for(int i=list.size()-1;i>=0;i--){
-           sb.append(String.valueOf(list.get(i)));
+       if(list.size() == 1) most = list.get(0);
+       else{
+           Collections.sort(list);
+           most = list.get(1);
        }
 
-       System.out.println(sb.toString());
-
-       /* java - 가장 큰 수 (programmers level 2)
-       int[] numbers = {2,10,6};
-
-       String[] result = new String[numbers.length];
-
-       int idx = 0;
-       for(Integer num : numbers){
-           result[idx++] = String.valueOf(num);
-       }
-
-       Arrays.sort(result, new Comparator<String>() {
-           @Override
-           public int compare(String o1, String o2) {
-               // 문자열을 합친 것이기 때문에 그대로 합쳐진 문자열이 리턴된다.
-               // ex) 6과 10을 비교한다면 610.compareTo(106)이렇게 된다.
-               // 이 상황에서 610이 더 크므로 양수를 반환하기 때문에 순서를 바꾸지 않는다.
-               // 반면 10,6이라서 반대로 되어 있다면 음수를 반환하기 때문에 6이 앞으로 위치를 바꾸게 된다.
-               return (o2+o1).compareTo(o1+o2);
-           }
-       });
-
-//       Arrays.sort(result,((o1, o2) -> (o2+o1).compareTo(o1+o2)));
+       // 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+       int length = num[num.length-1] - num[0];
 
 
-       StringBuffer sb = new StringBuffer();
-       for(int i=0;i<result.length;i++){
-           sb.append(result[i]);
-       }
+       System.out.println(sanSool);
+       System.out.println(joongAng);
+       System.out.println(most);
+       System.out.println(length);
 
-       System.out.println("정답 : "+sb.toString());
-        */
+
+
+
+
+
+
+
+
+
+
     }
 }

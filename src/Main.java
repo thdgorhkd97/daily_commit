e85804106099395cfaea15547d2121a71f7e7e76
Name <thdgorhkd97@naver.com>
2022-02-01 22:31:39 +0900

    0201

--- src/Main.java
+++ src/Main.java
@@ -6,82 +6,61 @@ import java.util.*;
 
 public class Main {
 
-    // java 1929 소수 구하기
-    // 아래의 주석친 부분은 2부터 N-1까지의 수 중에서 N을 나눠지게 하는 약수가 있다면
-    // N이 소수가 아니라는 것을 활용한 정말 기본적인 소수 판별 프로그램이다.
-    // 해당 방법은 시간초과가 발생하게 되고 다른 방법을 찾아야 했다
-    // 에라토스테네스의 체라는 방법을 활용해보기로 했다.
-    // 먼저 개념을 살펴보면 " k=2부터 루트N 이하까지 반복하여 자연수들 중 k를 제외한
-    // k의 배수들을 제외시킨다 " 라는 개념을 적용시킨다.
-    // 루트N 까지 확인하는 것은 소수를 판별함에 있어서 해당 수가 소수라면 반드시 둘중 하나는
-    // 루트N보다 작거나 같기 때문에 소수인지 아닌지 확인할 때 루트 N까지 확인하는 것이다.
-    // 따라서 확인하고자 하는 수(end)보다 하나 크게 boolean 배열을 잡고서
-    // 소수인 수들의 배수를 해당 범위에서 제거해 나간다.
-    // 에라토스테네스의 체를 이용해 소수를 판별하는 알고리즘이 반복문을 통해 확인하는 O(N)혹은
-    // O(루트N) 의 시간이 걸리는 알고리즘보다 훨씬 빠르다.
+    // java 1011 - fly me to the alpha centauri
+
+    // 거리 1 -> 1
+    // 거리 2 -> 1 1
+    // 거리 3 -> 1 1 1
+    // 거리 4 -> 1 2 1
+    // 거리 5 -> 1 2 1 1
+    // 거리 6 -> 1 2 2 1
+    // 거리 7 -> 1 1 2 2 1
+    // 거리 8 -> 1 2 2 2 1
+    // 거리 9 -> 1 2 3 2 1
+    // 거리 10 -> 1 1 2 3 2 1
+
+    // 문제조건을 보고 한참 고민해보다가 뭔가 규칙은 있는 것 같은데 규칙성을 찾기 힘들어서
+    // 결국 풀이를 참고하였다 ㅠㅠ
+    // 위의 조건을 보면 최대 이동거리는 거리의 루트값이하이다. 그래서 거리가 4가 되어야 2만킄 이동한 게 나오고
+    // 거리가 9가 되고 나서야 3만큼 이동한 것이다.
+    // 그래서 4,9,16 ... 이렇게 제곱값이 나올때마다 이동의 최대거리가 변하게 된다.
+    // 그리고 그 순간은 중간 이기 때문에 max * 2 - 1이 된다.
+
+    // 이렇게 규칙성이 보이더라도 이를 코드로 바꾸기 위해서 관통하는 규칙을 찾아야 하는데
+    // 이런 부분이 아직도 많이 부족하다고 느꼈다.
+    // 직관적인 코드 작성 능력이 아니라 규칙성을 찾아내는 통찰력을 길러야 할 것 같다.
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer stk = new StringTokenizer(str, " ");
+        for(int i=0;i<N;i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        int start = Integer.parseInt(stk.nextToken());
-        int end = Integer.parseInt(stk.nextToken());
+            int start = Integer.parseInt(stk.nextToken());
+            int end = Integer.parseInt(stk.nextToken());
 
-        boolean[] isPrime = new boolean[end+1];
+            int distance = end - start;
+            int cnt = 0;
 
-        isPrime[0] = true;
-        isPrime[1] = true;
+            int max = (int)Math.sqrt(distance);
 
-        for(int i=2;i<=Math.sqrt(end);i++){
-            if(isPrime[i]){
-                continue;
+            if(max == Math.sqrt(distance)){
+                cnt = max * 2 - 1;
             }
-
-            for(int j=i*i; j< isPrime.length; j=j+i){
-                isPrime[j] = true;
+            else if(distance <= max * max + max){
+                cnt = max * 2;
             }
+            else cnt = max * 2 + 1;
+
+            System.out.println(cnt);
         }
 
-        for(int i=start;i<=end;i++){
-            if(!isPrime[i]){
-                System.out.println(i);
-            }
-        }
+
 
 
     }
 }
-
-        /*ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i=start;i<=end;i++){
-            isPrimary(i,list);
-        }
-
-        for(int i=0;i<list.size();i++){
-            System.out.println(list.get(i));
-        }
-
-    }
-
-    public static void isPrimary(int num,ArrayList<Integer> list){
-        boolean flag = true;
-
-        for(int i=2;i<num;i++){
-            if(num % i == 0){
-                flag = false;
-                break;
-            }
-        }
-
-        if(flag){
-            list.add(num);
-        }
-
-    }
-}*/
 

7138a791b1d33b61d8e6b7dbc29b65f829a775bf
Name <thdgorhkd97@naver.com>
2022-02-11 21:42:16 +0900

    0211

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,20 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 11054 가장 긴 바이토닉 부분수열
+    // java 1149 RGB 거리
 
-    // 수열 S가 어떤 수 Sk를 기준으로 S1 < S2 < ... Sk-1 < Sk > Sk+1 > ... SN-1 > SN을 만족한다면,
-    // 그 수열을 바이토닉 수열이라고 한다.
+//    RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
+//    집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때,
+//    아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
+//    1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+//    N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+//    i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
 
-    // LIS를 양쪽으로 계산해야 한다고 생각했다.(산 모양으로 증가하는 수열을 구해야 하기 때문에)
-    // 그래서 LIS를 왼쪽에서 증가하는 수열 & 오른쪽에서 부터 증가하는 수열을 구해서
-    // 2개의 LIS를 더한다.
-    // 다만 오른쪽에서부터 증가하는 수열을 구하는 과정에서 인덱스가 헷갈려서 몇 번 수정을 거쳤고
-    // 왼쪽에서의 증가수열과 오른쪽에서의 증가수열이 하나가 겹치는 부분이 있기 때문에
-    // 최종적으로 더해진 수열에서 가장 큰 값 - 1을 해서 답을 도출한다.
+    // 동적 계획법으로 아래로 내려가면서 더해서 최소값이 나오도록 하는 로직이 필요하다
+    // 근데 규칙에서 그 이전의 색깔을 포함하지 않아야 하기 때문에 dp[i][0]을 구할때는
+    // dp[i-1][1], dp[i-1][2] 에 home[i][0] 를 더해서 최소값을 구한다.
+    // 즉 아래로 내려가면서 같은 색이 아닌 다른 색에서 내려오는 거에서
+    // 최소값을 확보할 수 있도록 한것이다.
 
     public static void main(String[] args) throws IOException {
 
@@ -24,59 +27,38 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N+1];
-        int[] bitonic = new int[N+1];
-        int[] bitonic_left = new int[N+1];
-        int[] bitonic_right = new int[N+1];
+        int[][] home = new int[N][3];
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            StringTokenizer stk = new StringTokenizer(str," ");
 
-        for(int i=1;i<=N;i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
-        }
-
-        for(int i=1;i<=N;i++){
-            bitonic_left[i] = 1;
-            for(int j=1;j<i;j++){
-                if(arr[i] > arr[j] && bitonic_left[i] <= bitonic_left[j]){
-                    bitonic_left[i] = bitonic_left[j] + 1;
-                }
+            for(int j=0;j<3;j++){
+                home[i][j] = Integer.parseInt(stk.nextToken());
             }
+
         }
 
-        for(int i=N;i>=1;i--){
-            bitonic_right[i] = 1;
-            for(int j=N;j>i;j--){
-                if(arr[i] > arr[j] && bitonic_right[i] <= bitonic_right[j]){
-                    bitonic_right[i] = bitonic_right[j] + 1;
-                }
-            }
+        int[][] dp = new int[N][3];
+
+        dp[0][0] = home[0][0];
+        dp[0][1] = home[0][1];
+        dp[0][2] = home[0][2];
+
+        for(int i=1;i<N;i++){
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + home[i][0];
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + home[i][1];
+            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1]) + home[i][2];
         }
 
-        for(int i=1;i<=N;i++){
-            bitonic[i] = bitonic_left[i] + bitonic_right[i];
-        }
-
-//        System.out.println("왼쪽에서부터");
-//        for(int i=1;i<=N;i++){
-//            System.out.print(bitonic_left[i] + " ");
+//        for(int i=0;i<N;i++){
+//            for(int j=0;j<3;j++){
+//                System.out.print(dp[i][j] + " ");
+//            }
+//            System.out.println();
 //        }
-//        System.out.println();
-//
-//        System.out.println("오른쪽에서부터");
-//        for(int i=1;i<=N;i++){
-//            System.out.print(bitonic_right[i] + " ");
-//        }
-        int max = 0;
-        for(int i=1;i<=N;i++){
-            max = Math.max(bitonic[i],max);
-        }
 
-        System.out.println(max-1);
-
-
-
-
+        System.out.println(Math.min(Math.min(dp[N-1][0],dp[N-1][1]),dp[N-1][2]));
 
     }
 }

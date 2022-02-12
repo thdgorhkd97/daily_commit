d54974d84937393ffe79d71b92bcf24da5d4c93a
Name <thdgorhkd97@naver.com>
2022-02-12 16:19:20 +0900

    0212

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,21 @@ import java.util.*;
 
 public class Main {
 
-    // java 1149 RGB 거리
+    // java 10844 baekjoon 쉬운 계단 수
 
-//    RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
-//    집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때,
-//    아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
-//    1번 집의 색은 2번 집의 색과 같지 않아야 한다.
-//    N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
-//    i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+    // 45656이란 수를 보자.
+    // 이 수는 인접한 모든 자리의 차이가 1이다. 이런 수를 계단 수라고 한다.
+    // N이 주어질 때, 길이가 N인 계단 수가 총 몇 개 있는지 구해보자. 0으로 시작하는 수는 계단수가 아니다.
 
-    // 동적 계획법으로 아래로 내려가면서 더해서 최소값이 나오도록 하는 로직이 필요하다
-    // 근데 규칙에서 그 이전의 색깔을 포함하지 않아야 하기 때문에 dp[i][0]을 구할때는
-    // dp[i-1][1], dp[i-1][2] 에 home[i][0] 를 더해서 최소값을 구한다.
-    // 즉 아래로 내려가면서 같은 색이 아닌 다른 색에서 내려오는 거에서
-    // 최소값을 확보할 수 있도록 한것이다.
+    // 처음에는 dfs로 접근해서 하나 차이나는 경우를 더해가면서 자릿수가 N이 될때까지 반복하는 식으로
+    // 접근했었는데 잘 풀리지 않았다... 그래서 결국 고민하다가 어떻게 접근해야 하는지 확인을 하고 말았다
+    // dp로 접근해서 동적계획법으로 풀었어야 하는데 길이와 0,9일때를 신경써야 했다.
+    // 2차원배열인 dp 의 의미는 dp[길이][마지막 자리수의 계단 수] 이렇게 된다.
+    // 맨 마지막 수가 0이거나 9라면 위아래가 아니라 1과 8만 올수있기때문에 이 경우를 제외하고는
+    // dp[i][j] = dp[i-1][j-1] + dp[i+1][j+1] 로 처리되어야 한다.
+    // 동적 계획법 문제인데 이걸 처리하는 게 생각보다 어려웠다.
+    // 동적 계획법을 많이 해봐서 조금 한다고 생각했는데 여러 분야에 응용이 가능하기 때문에
+    // 역시 끝까지 해봐야 알 것 같다 ㅠㅠ
 
     public static void main(String[] args) throws IOException {
 
@@ -27,38 +28,32 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] home = new int[N][3];
+        long[][] dp = new long[N+1][10];
 
-        for(int i=0;i<N;i++){
-            String str = br.readLine();
-            StringTokenizer stk = new StringTokenizer(str," ");
+        for(int i=1; i<= 9; i++){
+            dp[1][i] = 1; // 길이가 1이면 무조건 1이 된다(초기화)
+        }
 
-            for(int j=0;j<3;j++){
-                home[i][j] = Integer.parseInt(stk.nextToken());
+        for(int i=2; i<= N;i++){
+            for(int j=0;j<=9;j++){
+                if(j== 0){
+                    dp[i][j] = dp[i-1][j+1] % 1000000000;
+                }
+                else if(j==9){
+                    dp[i][j] = dp[i-1][j-1] % 1000000000;
+                }
+                else {
+                    dp[i][j] = ( dp[i-1][j-1] + dp[i-1][j+1] ) % 1000000000;
+                }
             }
-
         }
 
-        int[][] dp = new int[N][3];
-
-        dp[0][0] = home[0][0];
-        dp[0][1] = home[0][1];
-        dp[0][2] = home[0][2];
-
-        for(int i=1;i<N;i++){
-            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + home[i][0];
-            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + home[i][1];
-            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1]) + home[i][2];
+        long answer = 0;
+        for(int i=0;i<10;i++){
+            answer += dp[N][i];
         }
 
-//        for(int i=0;i<N;i++){
-//            for(int j=0;j<3;j++){
-//                System.out.print(dp[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
-        System.out.println(Math.min(Math.min(dp[N-1][0],dp[N-1][1]),dp[N-1][2]));
+        System.out.println(answer % 1000000000);
 
     }
 }

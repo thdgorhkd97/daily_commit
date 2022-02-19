e5aa5bdd66e30da9dae5fa6df95b23a3f02b2cae
ssong2 <thdgorhkd97@naver.com>
2022-02-19 23:55:40 +0900

    0219

--- src/Main.java
+++ src/Main.java
@@ -6,83 +6,105 @@ import java.util.*;
 
 public class Main {
 
-    // java 구명보트 & 튜플
-    //
+    // devcourse 백엔드 코딩테스트 2번문제
+    // push 되는 string의 share / comment 가 일치하게 되면
+    // 인원수에 따라 push 되는 string을 따로 구분해야 하는데 이 부분을 어떻게 처리할까 고민하다가
+    // 시간이 많이 소모 되었다 ㅠㅠㅠ
+    // 그래서 결국 모든 push 되는 string을 저장해 두었다가 push 되는 조건이 일치하게 되면
+    // push되는 string을 변경함으로써 조건을 맞추는 작업을 추가했다.
 
     public static void main(String[] args) throws IOException {
 
-//        String s = "{{2},{2,1},{2,1,3},{2,1,3,4}}";
-//
-//        s = s.substring(2,s.length()-2);
-//
-//        s = s.replace("},{","/");
-//
-//        String[] arr = s.split("/");
-//
-//        System.out.println(s);
-//
-//        Arrays.sort(arr,((o1, o2) -> o1.length() - o2.length()));
-//
-//        // 처음에는 단순히 arr배열의 각 순서의 맨 마지막 원소만을 취하는 방식으로 했었는데
-//        // 문제를 보면 원소 순서가 반드시 맨 마지막 원소가 새로운 원소일 것이라는 조건이 없기 때문에
-//        // 그렇게 하면 안 된다.
-//        // 예를들어 1 / 1 2 / 1 2 3 / 1 2 3 4 이런 식이면 1 2 3 4 라는 정답이 나오지만
-//        // 1 / 2 1 / 2 1 3 / 2 1 3 4 이렇게 정렬이 안 되어 있다면 단순히 맨 뒤 원소를 취하는 건
-//        // 정답이 될 수 없다.
-//
-//        /* 그래서 혹시 정렬을 해서 맨 마지막 원소를 취하면 되지 않을까 생각해서 이렇게도 구현해 봤는데
-//
-//        // 길이가 1인 제일 첫번째 arr[0]가 길이가 2인 두번째 arr[1]을 기준으로 큰 지 작은지 모르기 때문에
-//        // 크기를 비교해서 정렬하는 기준을 잡아야 하는 문제가 또 생긴다
-//        // 예를 들어 arr[0] = 1이면 1 2 이렇게 되어야 하므로 오름차순 정렬
-//        // arr[1] = 2면 2 1 이렇게 되어야 하므로 내림차순 정렬이 되어야 하므로 맨 뒤를 취하는 건 옳지 않은 것 같다.
-//        for(int i=1;i<arr.length;i++){
-//            String[] str = arr[i].split(",");
-//
-//            Arrays.sort(str);
-//
-//            list.add(Integer.parseInt(String.valueOf(str[i].charAt(str[i].length()-1))));
-//        }
-//        */
-//
-//        ArrayList<Integer> list = new ArrayList<>();
-//        list.add(Integer.parseInt(arr[0]));
-//
-//        for(int i=1;i<arr.length;i++){
-//            String[] str = arr[i].split(",");
-//
-//            for(int j=0;j<str.length;j++){
-//                if(!list.contains(Integer.parseInt(str[j]))){
-//                    list.add(Integer.parseInt(str[j]));
-//                    break;
-//                }
-//            }
-//        }
-//
-//        int[] answer = new int[list.size()];
-//
-//        for(int i=0;i<list.size();i++){
-//            answer[i] = list.get(i);
-//        }
+        String[] records = {
+                "john share",
+                "mary comment",
+                "jay share",
+                "check notification",
+                "check notification",
+                "sally comment",
+                "james share",
+                "check notification",
+                "lee share",
+                "laura share",
+                "will share",
+                "check notification",
+                "alice comment",
+                "check notification"};
 
-        int[] people = {70,80,50};
-        int limit = 100;
-        int answer = 0;
+        Stack<String> alarm = new Stack<>();
+        Stack<String> savings = new Stack<>();
 
-        Arrays.sort(people); // 50 60 70 80
+        for(int i=0;i< records.length;i++){
+            String name = records[i].substring(0,records[i].indexOf(" "));
+            String category = records[i].substring(records[i].indexOf(" ") + 1);
 
-        int l = 0, r = people.length-1;
-
-        while(l <= r){
-            if(people[l] + people[r] <= limit){
-                l++;
+            if(alarm.isEmpty()){ // 스택이 비어있으면
+                alarm.push(records[i]);
             }
-            r--;
-            answer++;
+            else if(records[i].equals("check notification")){ // 보관함으로 이동
+                savings.push(alarm.pop());
+            }
+            else{
+                String prev = alarm.peek();
+                String prevName = prev.substring(0,prev.indexOf(" ")); // 이전 이름
+                String prevCategory = prev.substring(prev.indexOf(" ") + 1); // 이전 comment ? share ?
 
+                String together = "";
+                if(prevCategory.equals(category)){
+                    together = prevName + "&&" + name;
+                    alarm.pop();
+                    alarm.push(together+" "+prevCategory);
+                }
+                else{
+                    alarm.push(records[i]);
+                }
+            }
         }
 
-        System.out.println(answer);
+
+        String[] answer = new String[savings.size()];
+        int idx = 0;
+        for(String str : savings){
+
+            String people = str.substring(0,str.indexOf(" ")); // 같은 카테고리를 가지는 사람을 && 으로 구분한 문자열
+            String category = str.substring(str.indexOf(" ") + 1); // 어떤 카테고리?
+            String[] person = people.split("&&"); // 같은 카테고리를 가지는 사람 수
+
+            StringBuffer save = new StringBuffer();
+
+            if(person.length == 1){ // 1명 일때
+                save.append(person[0]);
+            }
+            else if(person.length == 2){ // 2명 일때
+                String appned = person[0] + " and " + person[1];
+                save.append(appned);
+            }
+            else{ // 3명 이상이면 맨 앞사람 + n 명
+                String append = person[0] + " and " + String.valueOf(person.length-1) + " others";
+                save.append(append);
+            }
+
+            if(category.equals("share")){
+                save.append(" shared your post");
+            }
+            else if(category.equals("comment")){
+                save.append(" commented on your post");
+            }
+
+
+            answer[idx++] = save.toString();
+        }
+
+
+        /*
+        while(!savings.isEmpty()){
+        System.out.println(savings.pop());
+        }
+        */
+
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
 
 
 

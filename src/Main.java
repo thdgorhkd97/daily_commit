73ee5aac964a5c97d96b85d985fde9d40176f2ac
ssong2 <thdgorhkd97@naver.com>
2022-02-28 23:07:13 +0900

    0228

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,14 @@ import java.util.*;
 public class Main {
 
     // java 18870 좌표 압축
-    // 문제의 예시를 통해서 이해한 바로는 중복되는 값을 제외하고 해당 인덱스의 값보다
-    // 더 작은 원소의 개수를 구해서 다시 만들면 되는 것이라고 이해했다.
-    // 그래서 중복되지 않게 원소들을 넣고 (set을 활용) 정렬한 후(Collections.sort)
-    // 원소가 같으면 그때의 인덱스를 넣는다(정렬되어 있기 때문에 인덱스 순서는 자기 자신보다
-    // 작은 원소들의 개수를 의미한다)
-    // 이렇게 해서 예시에 해당하는 정답은 잘 나오는데 돌려보면 시간초과가 발생한다.
-    // 아무래도 2중 for문을 돌면서 break를 쓴다해도 배열의 크기가 커지면 시간초과가 발생하는
-    // 문제가 생기는 것 같다. 2중 for문이 아니라면 hash를 써야하나싶다
-
+    // 문제를 잘못 이해한 부분이 있어서 해결하는 시간이 오래 걸렸다.
+    // 나는 처음에 해당 인덱스보다 작은 원소의 개수를 더해서 새로운 인덱스값을 만드는 줄 이해했는데
+    // 알고보니 개수가 아니라 등수를 구해서 넣는 것이었다.(예시로 나와있던 값이 우연히 개수와 등수가
+    // 일치하는 경우라서 헷갈렸다.)
+    // 개수를 구하는 경우는 아래쪽에 주석처리 된 부분이다.
+    // 해당 원소와 개수를 넣어서 키 값을 비교해가며 더 작은 값이라면 더한다.
+    // 등수를 구하는 건 오히려 쉬웠는데 map에 해당 원소와 개수를 넣는 게 아니라 원소와 rank를 넣는다.
+    // rank는 0으로 초기화하고 키값이 달라지면 등수가 증가해야 하기 때문에 rank++로 등수를 바꾼다.
 
     public static void main(String[] args) throws IOException {
 
@@ -31,33 +30,46 @@ public class Main {
             arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int[] answer = new int[N];
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        ArrayList<Integer> list = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
+        int[] clone = arr.clone();
+        Arrays.sort(clone);
 
-        for(Integer num : arr){
-            if(set.add(num)) list.add(num);
-        }
-
-        Collections.sort(list);
-
-        for(int i=0;i<arr.length;i++){
-            int num = arr[i];
-            for(int j=0;j<list.size();j++){
-                if(list.get(j) == num){
-                    answer[i] = j;
-                    break;
-                }
+        int ranking = 0;
+        for(int num : clone){
+//            System.out.print(num + " ");
+            if(!map.containsKey(num)){
+                map.put(num,ranking++);
             }
         }
 
+        int[] answer = new int[N];
+
+        for(int i=0;i<N;i++){
+            answer[i] = map.get(arr[i]);
+        }
+
         StringBuffer sb = new StringBuffer();
-        for(int i=0;i<answer.length;i++){
+
+        for(int i=0;i<N;i++){
             sb.append(answer[i]).append(' ');
         }
 
         System.out.println(sb.toString());
+
+//        for(int i=0;i<N;i++){
+//
+//            int cnt = 0;
+//            for(Integer keyset : key){
+//                if(keyset < arr[i]){
+//                    cnt += map.get(keyset);
+//                }
+//            }
+//
+//            answer[i] = cnt;
+//        }
+
+
 
     }
 }

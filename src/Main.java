cd09c2fc244d0a0a5776adcf85de0d58ab744fd3
ssong2 <thdgorhkd97@naver.com>
2022-03-03 23:38:09 +0900

    0303

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,11 @@ public class Main {
 
     // java 9095 1,2,3 더하기
 
-    // 1 = 1
-    // 2 = 1,1 / 2 = 2
-    // 3 = 1,1,1 / 1, 2 / 2,1 / 3 = 4
-    // 4 = 1,1,1,1 / 1,1,2 / 1,2,1 / 2,1,1 / 2,2 / 1,3 / 3,1 = 7
-
-    // 5 = 1+1+1+1+1 / 1+1+1+2 / 1+1+2+1 / 1+2+1+1 / 2+1+1+1 / 1+1+3 / 1+3+1 / 3+1+1
-    //     1+2+2 / 2+1+2 / 2+2+1 / 2+3 / 3+2 = 13
-
-    // 6 = 1+1+1+1+1+1 / 2+1+1+1+1 (2가 한 개일때 5) / 2+2+1+1 / 2+1+2+1 /
-    //     2+1+1+2 / 1+1+2+2 / 1+2+2+1 / 1+2+1+2 / 1+1+1+3 (3 한개면 4)
-    //     1+2+3 / 1+3+2 / 2+3+1 / 2+1+3 / 3+2+1 / 3+1+2 / 3+3 / 2+2+2
-    //     -> 24
-
-    // 정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
-
-    // 1 ~ 6까지의 수에 대해 경우의 수를 모두 구했더니 규칙을 찾을 수 있었다.
-    // 7까지 구하려 했는데 7의 경우 너무 복잡해서 구하기 어려웠다.
-    // 다행히 규칙이 바로 보여서 dp를 활용해 해결할 수 있었지만 브루트 포스(완전탐색)으로
-    // 분류된 문제이니 만큼 완전탐색을 통해서 해결하는 연습도 필요할 것 같다.
+    // dp가 아닌 완전 탐색 방법으로 해결하는 경우
+    // 재귀를 통해서 특정 수가 되면 답에 +1 하고 특정 수를 넘어서면 해당 케이스를 종료한다.
+    // 근데 이게 재귀를 통해서 할 때 재귀의 종료 조건을 어떻게 걸어야 하나 고민을 많이 했다.
+    // 더해나가다가 타겟 넘버를 넘어서면 종료하면 되는 것이었고
+    // 1,2,3을 차례로 계속 더해나가면서 재귀를 통해서 구현하면 되는 건데.. 재귀는 해도해도 참..
 
     public static void main(String[] args) throws IOException {
 
@@ -34,24 +20,40 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
 
-        int[] number = new int[11];
-        number[1] = 1;
-        number[2] = 2;
-        number[3] = 4;
-
-        for(int i=4;i<11;i++){
-            number[i] = number[i-1] + number[i-2] + number[i-3];
-        }
-
         for(int i=0;i<T;i++){
-            int num = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-            System.out.println(number[num]);
+            System.out.println(OneTwoThree(0,n));
+
         }
-
-
-
-
 
     }
+
+    private static int OneTwoThree(int number, int n){
+        if(number > n){
+            return 0;
+        }
+
+        if(number == n){
+            return 1;
+        }
+
+        int cnt = 0;
+        for(int i=1;i<=3;i++){
+            cnt += OneTwoThree(number + i,n);
+        }
+
+        return cnt;
+    }
 }
+
+
+//    중복제거할 때 distinct 를 사용하는데
+//    select distinct count(name) 이렇게 썼는데 괄호 안에 넣어 주어야 한다.
+//    SELECT count(distinct name) from animal_ins;
+
+//    group by 순서를 from 앞에 둬서 실패했었다.
+//    문법은 얼추 아는 데 순서를 헷갈리거나 위치가 확실하지 않은 경우가 꽤 있다.
+//    select animal_type, count(*)
+//    from animal_ins group by animal_type order by animal_type;
+//    < select -> from -> where -> group by -> having -> order by >

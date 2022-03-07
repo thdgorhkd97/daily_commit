ff708ad7ec9903d8e48c0a8cb853c45d64721dd4
ssong2 <thdgorhkd97@naver.com>
2022-03-07 23:59:18 +0900

    0307

--- src/Main.java
+++ src/Main.java
@@ -8,74 +8,72 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // java programmers level 2 - k진수에서 소수 구하기
+    // java programmers level 2 - N개의 최소공배수
 
-    // 0P0처럼 소수 양쪽에 0이 있는 경우
-    // P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
-    // 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
-    // P처럼 소수 양쪽에 아무것도 없는 경우
-    // 단, P는 각 자릿수에 0을 포함하지 않는 소수입니다.
+    // arr의 모든 원소의 공통된 최소공배수를 구하는 문제
+    // 처음에 생각했던 방식은 lcd_long 방식이었다.
+    // 가장 큰 숫자를 기준으로 그 숫자에 +1씩 해가면서 다른 모든 수가 그 수의 약수인지 확인하는
+    // 식으로 진행하였다. 공통된 배수는 반드시 가장 큰 숫자보다도 커야 하기 때문에 for문의
+    // 첫 시작점의 수를 설정하는 것으로 시간을 줄이려고 했다
 
-    // 437674을 3진수로 바꾸면 211020101011입니다. 여기서 찾을 수 있는 조건에 맞는 소수는 왼쪽부터 순서대로 211, 2, 11이 있으며, 총 3개입니다.
-    // (211, 2, 11을 k진법으로 보았을 때가 아닌, 10진법으로 보았을 때 소수여야 한다는 점에 주의합니다.)
-    // 211은 P0 형태에서 찾을 수 있으며, 2는 0P0에서, 11은 0P에서 찾을 수 있습니다.
+    // 근데 시간초과가 우려되어 다른 생각을 해봤다.
+    // 공통 공배수라는 건 단순히 가장 큰 숫자보다 더 크다는 것 뿐 아니라 가장 큰 숫자보다
+    // n배씩 해가는 배수이기 때문에 1,2,3,4... 가장 큰 숫자의 배수를 다른 원소들로 나누면서
+    // 공배수인지 확인하면 되는 것이다.
 
-    // 우선 n을 k진수로 변경하고 나서 0과0 사이에 있거나 0뒤, 0앞 등으로 어떻게
-    // 잘라서 소수인지를 확인하려고 하는데 어떻게 3가지 기준으로 수를 잘라서 나눠야 하는지
-    // 한참을 고민했는데 생각해보니까 어차피 0으로 잘라야 하는 건데 굳이 0의 위치에 따라서
-    // 나누지 않아도 될 것 같아서 0을 기준으로 자르고 나오는 모든 수를 확인하면 될 것 같았다.
-    // 16가지 중 2가지 경우에 런타임 에러가 뜨는데.. 길이가 특이 케이스가 있는건가. 해결해야 한다.
+    // 실제로 시간상 이득이 있는건지 확인하기 위해서 시간을 재보았는데
+    // 내가 임의로 정한 이름(lcd_short vs lcd_long) 의 시간 비교는 arr의 크기를 2000으로
+    // 설정했을 때 lcd_short 는 2 lcd_long은 17이라는 숫자가 나왔다.
+    // 물론 원소가 임의로 마구 들어가 있어서 정확한 비교는 안 될 수있지만 같은 상황에서 시간이
+    // 줄어드는 것은 확실한 것 가탇.
 
     public static void main(String[] args) throws IOException {
 
-        int n = 1;
-        int k = 10;
+        int[] arr = new int[2000];
 
-        String number = k_jinsoo(n,k);
-
-        String[] num = number.split("0");
-
-//        System.out.println("num");
-//        for(int i=0;i<num.length;i++){
-//            System.out.println(num[i]);
-//        }
-
-        int answer = 0;
-        for(int i=0;i<num.length;i++){
-            if(isPrime(num[i])) answer++;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = 2*i + 2;
         }
+        Arrays.sort(arr);
+
+        long beforeTime = System.currentTimeMillis();
+        int answer = lcd_short(arr,arr[arr.length-1]);
+
+        long afterTime = System.currentTimeMillis();
+        System.out.println(" 시간 : " + (afterTime- beforeTime)/1000);
 
         System.out.println(answer);
     }
 
-    public static boolean isPrime(String str){
-        if(str.equals("1") || str.equals("")) return false;
-        else if(str.equals("2")) return true;
-        int number = Integer.parseInt(str);
+    public static int lcd_short(int[] arr,int max){
 
-        for(int i=2;i<number;i++){
-            if(number % i == 0){
-                return false;
+
+        for(int i=1; ;i++){
+            int num = max * i;
+            boolean flag = true;
+            for(int j=0;j<arr.length;j++){
+                if(num % arr[j] != 0){
+                    flag = false;
+                    break;
+                }
             }
+            if(flag) return num;
         }
-
-        return true;
     }
 
-    public static String k_jinsoo(int n,int k){
-        StringBuffer sb = new StringBuffer();
 
-        while(n > 0){
-            sb.append(n % k);
-            n = n/k;
+    public static int lcd_long(int[] arr,int max){
+
+        for(int i=max; ;i++){
+            boolean flag = true;
+            for(int j=0;j<arr.length;j++){
+                if( i % arr[j] != 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) return i;
         }
-
-        StringBuffer str = new StringBuffer();
-        for(int i=sb.toString().length()-1;i>=0;i--){
-            str.append(sb.toString().charAt(i));
-        }
-
-
-        return str.toString();
     }
+
 }

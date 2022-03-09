8c647cabfe97144f6134e7045eef49c90eff17cf
ssong2 <thdgorhkd97@naver.com>
2022-03-09 23:30:43 +0900

    0309

--- src/Main.java
+++ src/Main.java
@@ -8,78 +8,66 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    // 피보나치 수
-    // 재귀로 구현하려고 했는데 재귀로 구현하게 되면 n자체가 너무 크고 1234567의 나머지가
-    // 저장되어야 하다보니 시간초과가 발생하게 된다.
-    // 동적 계획법으로 해결했다.
+    // N과 M 만들기
+    // dfs를 활용한 n개의 수 중 r 개를 뽑는 문제다.
+    // dfs를 통해 depth를 두어 몇 번째로 뽑는 건지 visited 배열을 통해 해당 원소를
+    // 이미 뽑았는지 아닌지를 확인하는 것으로 구할 수 있는데
+    // 방문여부를 확인하는 visited 배열을 사용하지 않으면 중복을 허용하는 중복 수열을
+    // 구하는 것도 가능하다.
 
-    // programmers level 2 - 기능개발
-    // 우선 각 기능별로 종료시킬 수 있는 모든 일자를 구한 뒤에 한번에 배포할 수 있는 기능의 수를 구한다,
-    // 예를 들어 7 3 9 이렇게 있으면 두번째 기능은 3일만에 완료되었으므로
-    // 첫번째 기능이 배포될때 두번째 기능까지 배포 가능하므로 2 1 이렇게 정답이 된다.
-    // 기능이 완료되는 일자를 비교해가면서 한 번에 배포 가능한 기능을 list에 넣고
-    // 마지막으로 남은 기능의 수를 넣으면 완성
-
-
-
+    static StringBuffer sb = new StringBuffer();
 
     public static void main(String[] args) throws IOException {
 
-        int[] progresses = {95,90,99,99,80,99};
-        int[] speeds = {1,1,1,1,1,1};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int length = progresses.length;
-        int[] successDays = new int[length];
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        for(int i=0;i<length;i++){ // 완료 일자 구하는 for문
-            int day = 0;
-            while(progresses[i] < 100){
-                progresses[i] += speeds[i];
-                day++;
-            }
-            successDays[i] = day;
+        int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
+
+        int[] arr = new int[N];
+        int[] result = new int[N];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = i+1;
         }
 
-        int cnt = 1;
-        int num = successDays[0];
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=1;i< successDays.length;i++){
-            if(successDays[i] <= num){ // 이미 구현되어 있는 기능이 있는지 확인
-                cnt++;
+        boolean[] visited = new boolean[N];
+
+        int depth = 0;
+
+        dfs(arr,result,depth,M,visited);
+
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int[] arr,int[] result, int depth,int M, boolean[] visited){
+        if(depth == M){
+            for(int i=0;i<M;i++){
+                 sb.append(result[i]).append(' ');
             }
-            else{
-                list.add(cnt);
-                cnt = 1;
-                num = successDays[i];
-            }
+            sb.append('\n');
+
+            return ;
         }
 
-        list.add(cnt);
-
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
+        for(int i=0;i<arr.length;i++){
+            result[depth] = arr[i];
+            dfs(arr,result,depth+1,M,visited);
         }
 
-        /* 피보나치 수
-        int n = 5;
-
-        int[] fibo = new int[n+1];
-
-        fibo[0] = 0;
-        fibo[1] = 1;
-        fibo[2] = 1;
-
-        if(n > 2){
-            for(int i=3;i<=n;i++){
-                fibo[i] = fibo[i-1] % 1234567 + fibo[i-2] % 1234567;
-                System.out.println(i + " 번째 수는 " + fibo[i]);
+        /* 중복을 허용하지 않는 순열 코드
+        // 해당 코드에서 visited라는 방문여부를 확인하는 boolean 배열을 제외하면
+        // 해당 원소를 뽑았는지 확인하지 않기 때문에 중복을 허용하는 순열을 뽑는다.
+        for(int i=0;i<arr.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                result[depth] = arr[i];
+                dfs(arr,result,depth+1,M,visited);
+                visited[i] = false;
             }
         }
-
-        int answer = fibo[n] % 1234567;
-
          */
-
     }
 }

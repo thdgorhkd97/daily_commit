187be3de42aca2276453a770df3db168ce432af7
ssong2 <thdgorhkd97@naver.com>
2022-03-22 23:53:28 +0900

    0322

--- src/Main.java
+++ src/Main.java
@@ -6,203 +6,130 @@ import java.util.*;
 
 public class Main {
 
-    // java 프린터
+    // java 11723 집합
 
-    // 큐와 arryalist를 함께 사용하면서 중요도가 가장 높다면 출력 순서인 answer를 +1하고
-    // 중요도가 더 높은 문서가 있으면 큐와 arraylist에서 맨 앞 원소를 제외하고
-    // 맨 뒤에 다시 넣는다
-    // 이때 int[] 배열을 넣어서 순서를 알고자 하는 원소에 대해서는 int[2]에서
-    // 두 번째 원소가 1이면 알고자하는 순서이고 0이면 알고자하는 순서가 아니다.
+    // 비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
+    // add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
+    // remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
+    // check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
+    // toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
+    // all: S를 {1, 2, ..., 20} 으로 바꾼다.
+    // empty: S를 공집합으로 바꾼다.
 
-    // java 소용돌이 1
-    // n = 3이면
-    // 1 2 3
-    // 8 9 4
-    // 7 6 5 이렇게 오른쪽으로 아래로 왼쪽으로 위로 반복하며 채워나가는 소용돌이
+    // 처음에는 arraylist로 해서 각 연산마다 탐색함으로써 처리했다.
+    // 근데 arraylist에서 탐색이 O(n)이다 보니까 연산의 수가 많아지면 시간초과가 나는 것 같았다
+    // 어떻게 시간을 줄일 수 있을까 고민하면서 로직을 바꿔나갔는데도 계속 못했다 ㅠㅠ
+    // 그래서 아이디어를 얻고자 조금 찾아봤는데 굳이 arraylist로 복잡하게 하지 않아도 되는 것이
+    // 추가되거나 삭제되는 숫자가 1~20이라는 제한이 문제에서 있기 때문에 boolean[] 타입으로
+    // 선언해서 만약 20이 들어있다면 [20]을 true로 빠지면 [20]을 false로 바꿈으로써
+    // boolean[20]이면 1byte * 20으로 처리가 가능했다.
 
-    // java 소용돌이 2
-    // n = 3이면
-    // 5 4 3
-    // 6 1 2
-    // 7 8 9 중앙에서 시작해서 오른쪽 위쪽 왼쪽 아래쪽 반복하여 채워나가는 소용돌이
-    // 시작지점이 중앙부터기 때문에 어떻게 접근해야 할지 더 고민스러웠다.
-    // 처음에는 중앙에서 네모칸으로 한칸씩 늘려가면서 채워지는 규칙을 보고 네모칸으로
-    // 채워나가야 하나 고민했는데 그렇게 하려면 범위를 설정해야 한다는 게 부담스럽게 느껴졌다
-    // 그래서 처음부터 채워나가던 소용돌이1 처럼 오히려 map[n-1][n-1] 부터 채워나가면서
-    // 역소용돌이로 1까지 빼나가면서 채워나갔다.
 
     public static void main(String[] args) throws IOException {
 
-        int n = 5;
-        int num = 1;
-        int max = n * n;
-        int[][] map = new int[n][n];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                map[i][j] = 0;
+        int M = Integer.parseInt(br.readLine());
+
+        boolean[] bitmask = new boolean[21];
+        String operator = "";
+        int num = 0;
+
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<M;i++){
+            String str = br.readLine();
+
+            if(str.equals("all")){
+                for(int j=1;j<=20;j++) bitmask[j] = true;
             }
-        }
-        int x = n-1;
-        int y = n-1;
-        while(num <= max){
-            for(int i=y;i>=0;i--){
-                if(map[x][i] != 0) break;
-                map[x][i] = max--;
-                y = i;
-            }
-            x--;
-            for(int i=x;i>=0;i--){
-                if(map[i][y] != 0) break;
-                map[i][y] = max--;
-                x = i;
-            }
-            y++;
-            for(int i=y;i<n;i++){
-                if(map[x][i] != 0) break;
-                map[x][i] = max--;
-                y = i;
-            }
-            x++;
-            for(int i=x;i<n;i++){
-                if(map[i][y] != 0) break;
-                map[i][y] = max--;
-                x = i;
-            }
-            y--;
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(map[i][j] +  " ");
-            }
-            System.out.println();
-        }
-        /* 소용돌이 1
-        int n = 5;
-
-        int[][] map = new int[n][n];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++) {
-            map[i][j] = 0;    }
-        }
-
-        int num = 1;
-        int max = n * n;
-
-        int x = 0;
-        int y = 0;
-
-        while(num <= max){
-            for(int i=y;i<n;i++){
-                if(map[x][i] != 0) break;
-                map[x][i] = num++;
-                y = i;
-            }
-
-            x++;
-
-            for(int i=x;i<n;i++){
-                if(map[i][y] != 0) break;
-                map[i][y] = num++;
-                x = i ;
-            }
-
-            y--;
-
-            for(int i=y;i>=0;i--){
-                if(map[x][i] != 0) break;
-                map[x][i] = num++;
-                y=i;
-            }
-
-            x--;
-
-            for(int i=x;i>=0;i--){
-                if(map[i][y] != 0) break;
-                map[i][y] = num++;
-                x=i;
-            }
-            y++;
-
-        }
-
-
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-         */ // 소용돌이 1
-
-        /*
-        int[] priorities = {2,1,3,2};
-        int location = 2;
-        int answer = 0;
-
-        Queue<int[]> que = new LinkedList<>();
-        ArrayList<int[]> list = new ArrayList<>();
-
-        for(int i=0;i<priorities.length;i++){
-
-            int[] print = new int[2];
-            print[0] = priorities[i];
-            if(i==location){
-                print[1] = 1;
+            else if(str.equals("empty")){
+                for(int j=1;j<=20;j++) bitmask[j] = false;
             }
             else{
-                print[1] = 0;
-            }
+                String[] map = str.split(" ");
+                operator = map[0];
+                num = Integer.parseInt(map[1]);
 
-            que.add(print);
-            list.add(print);
+                switch (operator){
+                    case "add" :
+                        bitmask[num] = true;
+                        break;
+                    case "remove" :
+                        bitmask[num] = false;
+                        break;
+                    case "check" :
+                        if(bitmask[num]) sb.append("1").append("\n");
+                        else sb.append("0").append("\n");
+                        break;
+                    case "toggle" :
+                        if(bitmask[num]) bitmask[num] = false;
+                        else bitmask[num] = true;
+                        break;
+                }
+
+
+            }
         }
 
-        while(true) {
-            int critical = list.get(0)[0];
-            boolean flag = true;
-            int[] print = new int[2];
+        System.out.println(sb.toString());
 
-            for (int i = 0; i < list.size(); i++) {
-                if (critical < list.get(i)[0]) {
-                    flag = false;
-                    break;
+        /*
+        ArrayList<Integer> list = new ArrayList<>();
+        String operator = "";
+        int num = 0;
+
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for(int i=0;i<M;i++){
+            String str = br.readLine();
+
+            if(str.equals("all")){
+                list.clear();
+                for(int j=1;j<=20;j++){
+                    list.add(j);
                 }
             }
-
-            if (!flag) { // 중요도 더 높은게 있습니다
-                print = que.poll();
-                list.remove(0);
-
-                que.add(print);
-                list.add(print);
-            } else {
-                print = que.poll();
-                list.remove(0);
-                answer++;
-                if (1 == print[1]) {
-                    break;
-                }
+            else if(str.equals("empty")){
+                if(!list.isEmpty())list.clear();
             }
+            else{
+                String[] map = str.split(" ");
+                operator = map[0];
+                num = Integer.parseInt(map[1]);
 
+                switch (operator){
+                    case "add" :
+                        if(!list.contains(num)){
+                            list.add(num);
+                        }
+                        break;
+                    case "remove" :
+                        if(list.contains(num)){
+                            list.remove(new Integer(num));
+                        }
+                        break;
+                    case "check" :
+                        if(list.contains(num)){
+                            answer.add(1);
+                        }
+                        else answer.add(0);
+                        break;
+                    case "toggle" :
+                        if(list.contains(num)){
+                            list.remove(new Integer(num));
+                        }
+                        else{
+                            list.add(num);
+                        }
+                        break;
+                }
+
+            }
         }
 
-        System.out.println(answer);
+        for(int i=0;i<answer.size();i++){
+            System.out.println(answer.get(i));
+        }
          */
-
-    }
-    private static boolean Allfill(int[][] map){
-        for(int i=0;i<map.length;i++){
-            for(int j=0;j<map.length;j++){
-                if(map[i][j] != 0){
-                    return false;
-                }
-            }
-        }
-
-        return true;
     }
 }

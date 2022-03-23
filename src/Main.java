42a98f7a0184bde2bf5cc3f910ad0b4bc9d14b12
ssong2 <thdgorhkd97@naver.com>
2022-03-23 23:55:55 +0900

    0323

--- src/Main.java
+++ src/Main.java
@@ -6,130 +6,72 @@ import java.util.*;
 
 public class Main {
 
-    // java 11723 집합
+    // java 14391 종이 조각
 
-    // 비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
-    // add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
-    // remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
-    // check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
-    // toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
-    // all: S를 {1, 2, ..., 20} 으로 바꾼다.
-    // empty: S를 공집합으로 바꾼다.
+//    영선이는 숫자가 쓰여 있는 직사각형 종이를 가지고 있다. 종이는 1×1 크기의 정사각형 칸으로 나누어져 있고, 숫자는 각 칸에 하나씩 쓰여 있다. 행은 위에서부터 아래까지 번호가 매겨져 있고, 열은 왼쪽부터 오른쪽까지 번호가 매겨져 있다.
+//
+//    영선이는 직사각형을 겹치지 않는 조각으로 자르려고 한다. 각 조각은 크기가 세로나 가로 크기가 1인 직사각형 모양이다. 길이가 N인 조각은 N자리 수로 나타낼 수 있다. 가로 조각은 왼쪽부터 오른쪽까지 수를 이어 붙인 것이고, 세로 조각은 위에서부터 아래까지 수를 이어붙인 것이다.
+//
+//    아래 그림은 4×4 크기의 종이를 자른 한 가지 방법이다.
+//    종이를 적절히 잘라서 조각의 합을 최대로 하는 프로그램을 작성하시오
 
-    // 처음에는 arraylist로 해서 각 연산마다 탐색함으로써 처리했다.
-    // 근데 arraylist에서 탐색이 O(n)이다 보니까 연산의 수가 많아지면 시간초과가 나는 것 같았다
-    // 어떻게 시간을 줄일 수 있을까 고민하면서 로직을 바꿔나갔는데도 계속 못했다 ㅠㅠ
-    // 그래서 아이디어를 얻고자 조금 찾아봤는데 굳이 arraylist로 복잡하게 하지 않아도 되는 것이
-    // 추가되거나 삭제되는 숫자가 1~20이라는 제한이 문제에서 있기 때문에 boolean[] 타입으로
-    // 선언해서 만약 20이 들어있다면 [20]을 true로 빠지면 [20]을 false로 바꿈으로써
-    // boolean[20]이면 1byte * 20으로 처리가 가능했다.
-
+    // 잘랐을 때 가장 큰 값을 찾아야 하기 때문에 자릿수가 가장 큰 게 가장 크다고 생각했다.
+    // 그래서 가로로 쫙 잘라서 더하것과 세로로 쫙 잘라서 더한 것 중 max 값을 리턴했는데
+    // 해당 케이스를 벗어나는 케이스가 있는 것 같다.
+    // 근데 예외 케이스가 뭐가 될까 싶다... 도저히 못 찾겠어서 공부해야겠다..
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int M = Integer.parseInt(br.readLine());
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        boolean[] bitmask = new boolean[21];
-        String operator = "";
-        int num = 0;
+        int N = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
 
-        StringBuffer sb = new StringBuffer();
-        for(int i=0;i<M;i++){
+        int[][] map = new int[N][M];
+
+        for(int i=0;i<N;i++){
             String str = br.readLine();
-
-            if(str.equals("all")){
-                for(int j=1;j<=20;j++) bitmask[j] = true;
-            }
-            else if(str.equals("empty")){
-                for(int j=1;j<=20;j++) bitmask[j] = false;
-            }
-            else{
-                String[] map = str.split(" ");
-                operator = map[0];
-                num = Integer.parseInt(map[1]);
-
-                switch (operator){
-                    case "add" :
-                        bitmask[num] = true;
-                        break;
-                    case "remove" :
-                        bitmask[num] = false;
-                        break;
-                    case "check" :
-                        if(bitmask[num]) sb.append("1").append("\n");
-                        else sb.append("0").append("\n");
-                        break;
-                    case "toggle" :
-                        if(bitmask[num]) bitmask[num] = false;
-                        else bitmask[num] = true;
-                        break;
-                }
-
-
+            for(int j=0;j<M;j++){
+                map[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
             }
         }
 
-        System.out.println(sb.toString());
+//        for(int i=0;i<N;i++){
+//            for(int j=0;j<M;j++){
+//                System.out.print(map[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
-        /*
-        ArrayList<Integer> list = new ArrayList<>();
-        String operator = "";
-        int num = 0;
 
-        ArrayList<Integer> answer = new ArrayList<>();
+        int answer = 0;
+        answer = Math.max(Seroro(map,N,M),Garoro(map,N,M));
+        System.out.println(answer);
 
+    }
+
+    private static int Seroro(int[][] map,int N,int M){
+        int sum = 0;
         for(int i=0;i<M;i++){
-            String str = br.readLine();
-
-            if(str.equals("all")){
-                list.clear();
-                for(int j=1;j<=20;j++){
-                    list.add(j);
-                }
+            StringBuffer sb = new StringBuffer();
+            for(int j=0;j<N;j++){
+                sb.append(map[j][i]);
             }
-            else if(str.equals("empty")){
-                if(!list.isEmpty())list.clear();
-            }
-            else{
-                String[] map = str.split(" ");
-                operator = map[0];
-                num = Integer.parseInt(map[1]);
-
-                switch (operator){
-                    case "add" :
-                        if(!list.contains(num)){
-                            list.add(num);
-                        }
-                        break;
-                    case "remove" :
-                        if(list.contains(num)){
-                            list.remove(new Integer(num));
-                        }
-                        break;
-                    case "check" :
-                        if(list.contains(num)){
-                            answer.add(1);
-                        }
-                        else answer.add(0);
-                        break;
-                    case "toggle" :
-                        if(list.contains(num)){
-                            list.remove(new Integer(num));
-                        }
-                        else{
-                            list.add(num);
-                        }
-                        break;
-                }
-
-            }
+            sum += Integer.parseInt(sb.toString());
         }
-
-        for(int i=0;i<answer.size();i++){
-            System.out.println(answer.get(i));
+        return sum;
+    }
+    private static int Garoro(int[][] map,int N,int M){
+        int sum = 0;
+        for(int i=0;i<N;i++){
+            StringBuffer sb = new StringBuffer();
+            for(int j=0;j<M;j++){
+                sb.append(map[i][j]);
+            }
+            sum += Integer.parseInt(sb.toString());
         }
-         */
+        return sum;
     }
 }

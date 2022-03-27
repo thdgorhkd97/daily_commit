b0c63e147c0564c25b4beea6f97fc758322faab8
ssong2 <thdgorhkd97@naver.com>
2022-03-27 23:50:39 +0900

    0327

--- src/Main.java
+++ src/Main.java
@@ -6,73 +6,99 @@ import java.util.*;
 
 public class Main {
 
-    // L사 코딩테스트 1번 문제
-    // 1번문제면 가장 쉬운 문제였을 텐데 예상보다 시간을 많이 썼던 문제라서 다시 한 번 체크해보려 한다.
-    // 문제 자체는 심플했다. 주어진 로그 문자열에 대해서 조건에 맞는 입력인지 파악해서 알맞지 않은 로그의 수를 반환한다.
-    // 조건은 여러가지가 있다.
-    // 1. 특정 이름을 포함하지 않는경우
-    // 2. 공백으로 시작하는 경우
-    // 3. 로그의 길이가 100을 넘는 경우
-    // 4. 특정 이름에 대한 내용에 공백이 있는 경우(indexOf를 활용한 위치를 기준으로 substring으로 잘라서 구별했다)
-    // 5. 특수문자나 숫자를 포함하는 경우
-    // 이렇게 5가지 조건을 봐야 하는데 4번에서 substring으로 자르는 건 그리 어렵지 않았는데 5번에서 처음엔 정규표현식을
-    // 잘못 표현해서 특수문자나 숫자를 포함하지 않을 때를 체크한 것 같다.
+    // java 3차 압축
+    // 1. 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
+    // 2. 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
+    // 3. w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
+    // 4. 입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
+    // 5. 단계 2로 돌아간다.
+
+    // 문제를 잘못 이해하고 접근한 것이 문제가 되는 것 같다.
+    // 처음에 이해한 건 첫번째 예시에 집중해서 그 다음 글자까지를 묶은 2개의 길이만을
+    // 사전에 포함되었는지 확인하는 식으로 했는데 그렇게 되는 것이 아니라
+    // 그 뒤쪽으로 사전에 포함된다면 가장 긴 문자열을 찾아야 하는 문제였다
+    // 그리고 그 과정에서 현재 시작 지점과 그 뒤쪽으로 사전에 포함되는 문자열을 찾는
+    // 인덱스를 찾는 과정에서 변수가 헷갈려서 시간이 조금 걸렸다
+    // 나머지에 대해 내일 구현해 봐야 겠다.
+    // 문제 이해하다가 시간이 많이 흘러서 ;;
 
     public static void main(String[] args) throws IOException {
 
+        String msg = "KAKAO";
 
-        String[] logs = {"team_name : MyTeam application_name : YourApp error_level : info messag : IndexOutOfRange", "no such file or directory", "team_name : recommend application_name : recommend error_level : info message : RecommendSucces11", "team_name : recommend application_name : recommend error_level : info message : Success!", "   team_name : db application_name : dbtest error_level : info message : test", "team_name     : db application_name : dbtest error_level : info message : test", "team_name : TeamTest application_name : TestApplication error_level : info message : ThereIsNoError"};
+        HashMap<String,Integer> map = new HashMap<String,Integer>();
+        int num = 1;
+        map.put("A",num++);
+        map.put("B",num++);
+        map.put("C",num++);
+        map.put("D",num++);
+        map.put("E",num++);
+        map.put("F",num++);
+        map.put("G",num++);
+        map.put("H",num++);
+        map.put("I",num++);
+        map.put("J",num++);
+        map.put("K",num++);
+        map.put("L",num++);
+        map.put("M",num++);
+        map.put("N",num++);
+        map.put("O",num++);
+        map.put("P",num++);
+        map.put("Q",num++);
+        map.put("R",num++);
+        map.put("S",num++);
+        map.put("T",num++);
+        map.put("U",num++);
+        map.put("V",num++);
+        map.put("W",num++);
+        map.put("X",num++);
+        map.put("Y",num++);
+        map.put("Z",num++);
 
-        int answer = 0;
+        int idx = 0;
+        for(int i=0;i<msg.length();i++){
+            StringBuffer sb = new StringBuffer();
 
-        String teamName = "";
-        String applicationName = "";
-        String errorLevel = "";
-        String Message = "";
-
-        for(String str : logs){
-            System.out.print(str + " ");
-
-            if(!(str.contains("team_name") && str.contains("application_name") && str.contains("error_level") && str.contains("message")) || (str.charAt(0)==' ') || (str.length() >= 100)){
-                System.out.println(" 4가지 중 하나가 없다");
-                answer++;
-            }
-
-            else {
-                //System.out.println(str +  " "+str.indexOf("team_name") + " " + str.indexOf("application_name") + " " +str.indexOf("error_level") + " "+str.indexOf("message") + " ");
-
-                teamName = str.substring(12,str.indexOf("application_name")-1);
-                applicationName = str.substring(str.indexOf("application_name")+19,str.indexOf("error_level")-1);
-                errorLevel = str.substring(str.indexOf("error_level")+14,str.indexOf("message")-1);
-                Message = str.substring(str.indexOf("message")+10);
-
-                // System.out.println("teamName = " + teamName + " applicationName = " + applicationName + " errorLevel = " + errorLevel + " Message = " + Message);
-
-                if(teamName.contains(" ") || applicationName.contains(" ") || errorLevel.contains(" ") || Message.contains(" ")){
-                    answer++;
-                    System.out.println(" 자른거에 공백을 포함합니다");
-
-                }else{
-                    teamName = teamName.replaceAll("[^a-zA-Z]","&");
-                    applicationName = applicationName.replaceAll("[^a-zA-Z]","&");
-                    errorLevel = errorLevel.replaceAll("[^a-zA-Z]","&");
-                    Message = Message.replaceAll("[^a-zA-Z]","&");
+            sb.append(msg.charAt(i));
 
 
-                    if(teamName.contains("&") || applicationName.contains("&") || errorLevel.contains("&") || Message.contains("&")){
-                        answer++;
-                        System.out.println(" 특수문자나 숫자가 포함되네요 ");
-
-                    }
-                }
-
-            }
         }
 
-        System.out.println(answer);
+        /*
+        int idx = 0;
+        int pos = 0;
+        while(idx <= msg.length()-1){
+            StringBuffer sb = new StringBuffer();
 
+            for(int i=pos; ;){
+                sb.append(msg.charAt(i));
 
+                String str = sb.toString();
+                System.out.println(str);
 
+                if(map.containsKey(str)){
+                    list.add(map.get(str));
+                }
+                else{
+                    map.put(str,num++);
+                    break;
+                }
+
+                i++;
+                pos = i;
+
+            }
+            idx += pos;
+
+        }
+
+         */
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
 
     }
 }

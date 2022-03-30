c79a3bce9550dd8321af2fe365e35777e37e9c7c
ssong2 <thdgorhkd97@naver.com>
2022-03-30 23:55:04 +0900

    0330

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,10 @@ import java.util.*;
 
 public class Main {
 
-    // java 카드 구매하기
-    // 1 2 3 ... N 까지의 카드팩이 있고 N개의 카드를 구매할 때의 최대값을 구하라
-    // 처음에는 N개를 만들 수 있는 N까지의 수를 이용한 경우의 수를 구해서 해당 경우의 수에 해당하는
-    // 금액 중 최소를 구하고자 하였다. 근데 해당 방법으로 하면 너무나 복잡해져서 결국 방법을 찾아보았다
-    // dp 방식을 활용하되 경우의 수를 구하는 것이 아니라 어차피 경우의 수에 대한 금액을 구하는 것이므로
-    // 바로 금액을 구하는 dp 방식을 활용하면 된다.
-    // dp의 방식은 카드 x개가 들어있는 카드팩을 구매하게 되면 그 이후로는 카드 N-x개를 구입한다.
-    // dp[i] = dp[i-n] + money[n] 으로 구해야 한다.
+    // JAVA - BOJ 거리
+    // B O J가 반복되기 때문에 B -> O, O->J, J->B 해당 구문을 반복하면서
+    // 이전 인덱스의 위치 차이를 제곱해서 더해나가면 될 줄 알았는데..
+    // 풀리지 않았다..
 
     public static void main(String[] args) throws IOException {
 
@@ -21,27 +17,130 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] money = new int[N+1];
-        int[] dp = new int[N+1];
+        char[] ch = new char[N+1];
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-
-        for(int i=1;i<=money.length-1;i++){
-            money[i] = Integer.parseInt(stk.nextToken());
+        String str = br.readLine();
+        for(int i=0;i<=N-1;i++){
+            ch[i+1] = str.charAt(i);
         }
 
+        int[] dp = new int[N+1];
+        Arrays.fill(dp,-1);
         dp[0] = 0;
+        dp[1] = 1;
 
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=i;j++){
-                dp[i] = Math.max(dp[i],dp[i-j] + money[j]);
-//                System.out.println(i + " 번째 " + dp[i]);
+        int pos = 0;
+        int idx = 0;
+        String[] next = {"B","O","J"};
+        int min = 0;
+        int lastB = 0;
+        int lastO = 0;
+        int lastJ = 0;
+
+        while(pos < N){
+
+            switch (next[idx]){
+                case "B" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'O'){
+                            lastO = i;
+                            pos = i;
+                            min += (i-lastB) * (i-lastB);
+                            dp[i] = min;
+                        }
+                        else if(str.charAt(i) == 'J') break;
+                    }
+                    break;
+                case "O" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'J'){
+                            lastJ = i;
+                            pos = i;
+
+                            min += (i-lastO) * (i-lastO);
+                            dp[i] = min;
+                        }
+                        else if(str.charAt(i) == 'B') break;
+                    }
+                    break;
+                case "J" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'B'){
+                            lastB = i;
+                            pos = i;
+
+                            min += (i-lastJ) * (i-lastJ);
+                            dp[i] = min;
+                        }
+                        else if(str.charAt(i) == 'O') break;
+                    }
+                    break;
             }
+            idx++;
+            idx = idx % 3;
+
+            System.out.println(pos);
         }
 
         System.out.println(dp[N]);
+        /*
+        int lastB = 0;
+        int lastO = 0;
+        int lastJ = 0;
+        String[] next = {"B","O","J"};
+        int idx = 0;
+        int min = 0;
+        int pos = 0;
 
+        int a = 0;
+        while(pos != N-1){
 
+            boolean flag = true;
+            switch (next[idx]){
+                case "B" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'O'){
+                            lastO = i;
+                            pos = i;
+                            flag = false;
+                        }
+                        if(str.charAt(i) =='J') break;
+                    }
+                    min += Math.pow(lastO-lastB,2);
+                    break;
+
+                case "O" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'J'){
+                            lastJ = i;
+                            pos = i;
+                            flag = false;
+                        }
+                        if(str.charAt(i) =='B') break;
+                    }
+
+                    min += Math.pow(lastJ-lastO,2);
+                    break;
+                case "J" :
+                    for(int i=pos;i<str.length();i++){
+                        if(str.charAt(i) == 'B'){
+                            lastB = i;
+                            pos = i;
+                            flag = false;
+                        }
+                        if(str.charAt(i) =='O') break;
+                    }
+                    min += Math.pow(lastB-lastJ,2);
+                    break;
+            }
+            idx++;
+            idx = idx % 3;
+
+            System.out.println(pos);
+
+        }
+
+        System.out.println(min);*/
 
 
 

2db192dce7e6170aec827bd2c5f7572f5d6a9df1
ssong2 <thdgorhkd97@naver.com>
2022-04-01 23:41:54 +0900

    0401

--- src/Main.java
+++ src/Main.java
@@ -6,83 +6,106 @@ import java.util.*;
 
 public class Main {
 
-    // JAVA - BOJ 거리
+    // java bfs로 최단 거리 찾기 & 도형이 이중 배열로 주어질 때 회전 시키기
+    // 여태껏 문제를 풀면서 헷갈렸던 개념을 다시 잡으려고 어떤 문제를 푼게 아니라
+    // 해당 개념을 체크하기 위해서 필요 부분만 확인했다
+    // bfs로 거리를 찾는 건 좌표에서 상하좌우를 확인하면서 좌표 내에 위치한다면
+    // 이전의 거리 값 + 1로 하면서 체크해 나간다
 
-    // 어제부터 고민해보다가 결국 방법을 조금 알아보게 되었다 ㅠㅠ
-    // 처음부터 2중 for문은 고려하지 않았는데 N이 1000이하라는 점 시간 제한이 엄격하지 않다는 점에서
-    // 2중 for문을 사용하는 것이 괜찮다는 걸 보았고 min으로 dp를 채워나갔다
-    // 근데 애초에 dp 배열을 Integer.MAX_VALUE로 채워서 dp를 채워나가다가
-    // dp[N-1]이 Integer.MAX 면 -1 해당 인덱스에 거쳐가지 않았다는 것이므로 -1을 반환하면 되겠다
-    // 싶었는데 전체를 출력해보니까 일단 dp가 초기화된 인덱스 이후로 이상한 값이 들어갔다..
-    // 분명 dp[i]에 진입하지 않는데 dp[i]가 그대로 Integer.MAX가 아니라 절댓값이 크게 차이나지 않는
-    // 음수값이 들어갔다.. 아마도 int범위를 넘어가서 뭔가 문제가 생겼나 싶은데
-    // 그 과정에서 어떤 문제인지 까지는 도저히 모르겠다 ㅠㅠ
-    // 그래서 MAX_VALUE가 아닌 비슷한 큰수를 넣음으로써 해결했다.
+    // 도형을 회전 시키는 건 잘 몰라서 조금 알아보았다
+    // 2중 for문으로 주어지는 상황이라면 2중 for문을 돌리면서
+    // 90도씩 돌린다고 생각하면 된다.
+    //        for (int i = 0; i < key.length; i++) {
+    //            for (int j = 0; j < key.length; j++) {
+    //                result[i][j] = key[key.length - 1 - j][i];
+    //            }
+    //        }
+    // 새로운 배열의 맨 윗줄을 해당 배열의 왼쪽줄부터 채워나가는 식으로 하는 것이다.
 
+    static class P {
+        int x;
+        int y;
+        int cost;
+        P(int x,int y,int cost){
+            this.x = x;
+            this.cost = cost;
+            this.y = y;
+        }
+    }
+
+    static int idx = 0;
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
+        int[][] maps = {
+                {1,0,1,1,1},
+                {1,0,1,0,1},
+                {1,0,1,1,1},
+                {1,1,1,0,1},
+                {0,0,0,0,1}
+        };
 
-        int N = Integer.parseInt(br.readLine());
+        boolean[][] check = new boolean[maps.length][maps.length];
 
-        char[] ch = new char[N];
+        // 좌 상 우 하
+        int[] dx = {0,-1,0,1};
+        int[] dy = {-1,0,1,0};
 
-        String str = br.readLine();
+        check[0][0] = true;
+        Queue<P> que = new LinkedList<>();
+        que.add(new P(0,0,0));
 
-        ch = str.toCharArray();
+        while(!que.isEmpty()){
+            P point = que.poll();
 
-        int[] dp = new int[N];
-        Arrays.fill(dp,2100000000);
-        dp[0] = 0;
+            int curX = point.x;
+            int curY = point.y;
 
+            for(int i=0;i<dx.length;i++){
+                int nowX = curX + dx[i];
+                int nowY = curY + dy[i];
 
-        for(int i=0;i<N-1;i++){
-            int pos = i;
-            char c = ch[pos];
-
-            switch(c){
-                case 'B' :
-                    for(int j=i+1;j<N;j++){
-                        int nextIdx = j;
-                        char next = ch[j];
-                        if(next == 'O'){
-                            dp[nextIdx] = Math.min(dp[nextIdx], dp[pos] + (nextIdx-pos) * (nextIdx-pos));
-                        }
-                    }
-                    break;
-                case 'O' :
-                    for(int j=i+1;j<str.length();j++){
-                        int nextIdx = j;
-                        char next = ch[j];
-                        if(next == 'J'){
-                            dp[nextIdx] = Math.min(dp[nextIdx], dp[pos] + (nextIdx-pos) * (nextIdx-pos));
-                        }
-                    }
-                    break;
-                case 'J' :
-                    for(int j=i+1;j<str.length();j++){
-                        int nextIdx = j;
-                        char next = ch[j];
-                        if(next == 'B'){
-                            dp[nextIdx] = Math.min(dp[nextIdx], dp[pos] + (nextIdx-pos) * (nextIdx-pos));
-                        }
-                    }
-                    break;
+                if(nowX < 0 || nowY < 0 || nowX > maps.length-1 || nowY > maps.length-1) continue;
+                if(!check[nowX][nowY] && maps[nowX][nowY] == 1){
+                    que.add(new P(nowX,nowY,point.cost+1));
+                    check[nowX][nowY] = true;
+                }
             }
         }
+         */
 
-        if(dp[N-1] != 2100000000){
-            System.out.println(dp[N-1]);
-        }
-        else System.out.println("-1");
+        int[][] key = {
+                {0,0,0},
+                {1,0,0},
+                {0,1,1}};
 
-
-
-
+        rotate90(key);
 
 
 
 
     }
+
+    public static void rotate90(int[][] key){
+        idx++;
+        int[][] result = new int[key.length][key.length];
+        for (int i = 0; i < key.length; i++) {
+            for (int j = 0; j < key.length; j++) {
+                result[i][j] = key[key.length - 1 - j][i];
+            }
+        }
+
+        for(int i=0;i<result.length;i++){
+            for(int j=0;j<result.length;j++){
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        if(idx == 4) return ;
+        rotate90(result);
+    }
+
 }

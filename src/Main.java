a0bd97acc93545471f7a8dc18d6a9192d650fe14
ssong2 <thdgorhkd97@naver.com>
2022-04-03 23:56:17 +0900

    0403

--- src/Main.java
+++ src/Main.java
@@ -6,138 +6,75 @@ import java.util.*;
 
 public class Main {
 
-    // 코딩테스트 중 1번 문제
-    // ?로 들어오는 부분은 a,b,c 중 하나로 바뀐다고 할 때 a,b,c 모두가 연결되는 게
-    // 몇 가지 경우인지 확인하는 문제이다.
+    // java 불량 사용자
 
-    // 그래서 나는 ?의 개수를 파악하고 해당 개수만큼으로 combination을 구해서 ?를 a,b,c로 채울 수 있는
-    // 모든 경우의 수를 구한 다음에 a,b,c에 대해서 bfs로 연결된 a,b,c를 체크하고 모든 부분을 방문했는지 확인하여 마무리한다
+    // 주어진 불량 사용자 목록에 일치하는 아이디 목록의 수를 구하는 문제이다.
 
-    // 위의 방식대로 했다. 문제 해결은 완료했는데 시간적으로 줄일 수 있는 부분이 있을 것 같다는 생각이 든다.
-    // a,b,c에 대해서 모든 부분이 연결되어 있다면 a,b,c 에 대해 한번씩만 bfs를 돌면서 visited[][] 를 true로 바꾸면 될 것이라 생각했는데
-    // 천천히 생각을 다시 해보니 가장 마지막에 false로 남아있는걸 확인하는 것이 아니라 중간중간에 이미 bfs를 돌린 문자에 대해서
-    // visited[][] 가 false라면 다른 문자를 돌지 않았어도 다로 break로 빠져서 끝낼 수 있었을 것 같다.
+    // user_id에서 구해지는 banned_id의 수와 일치하는 아이디 목록만큼을 조합으로 구한 다음에
+    // 해당 목록이 banned_id에 일치하는 지를 확인해보려 했다.
+    // 근데 banned_id를 보면서 거기에 해당하는 문자까지 확인하려다 보니까 또 다시
+    // 다중 for문에 대한 인덱스 개념이 많이 어렵게 느껴졌다 ㅠㅠ
+    // 이중 for문을 돌면서 string을 비교하는데 거기서 또 이중 for문을 돌면서 문자 하나하나
+    // 비교해야 하는 부분이 많이 힘들었다.
 
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
-        String[] grid = { "cc?"};
+        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
 
-        int letter = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length();j++){
-                if(grid[i].charAt(j) == '?'){
-                    letter ++;
-                }
-            }
-        }
+        String[] banned_id = {"fr*d*", "abc1**"};
 
-        char[] ch = {'a','b','c'};
-
-        char[] result = new char[letter];
         int depth = 0;
+        String[] result = new String[banned_id.length];
+        int startIdx = 0;
 
-        combination(ch,result,depth,grid);
+        combination(depth,startIdx,result,user_id,banned_id);
 
         System.out.println(answer);
-
     }
 
-    private static void combination(char[] ch, char[] result, int depth,String[] grid){
+    private static void combination(int depth,int start,String[] result,String[] user_id,String[] banned_id){
         if(depth == result.length){
 
-            if(allChain(result,grid)){
+            if(isPossible(result,user_id,banned_id)){
                 answer++;
             }
+
             return ;
         }
-
-        for(int i=0;i<ch.length;i++){
-
-            result[depth] = ch[i];
-            combination(ch,result,depth+1,grid);
+        for(int i=start;i< user_id.length;i++){
+            result[depth] = user_id[i];
+            combination(depth+1,i+1,result,user_id,banned_id);
         }
     }
 
-    private static boolean allChain(char[] result,String[] grid){
-        int idx = 0;
-        char[][] copy = new char[grid.length][grid[0].length()];
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[i].length();j++){
-                if(grid[i].charAt(j) == '?'){
-                    copy[i][j] = (result[idx++]);
-                }
-                else copy[i][j] = (grid[i].charAt(j));
-            }
-        }
+    private static boolean isPossible(String[] result,String[] userId,String[] ban){
+        boolean[] check = new boolean[result.length];
 
-        boolean[] ABC = new boolean[3]; // 0 -> A, 1->B, 2->C
-        boolean[][] visited = new boolean[copy.length][copy[0].length];
-        for(int i=0;i<copy.length;i++){
-            for(int j=0;j<copy[0].length;j++){
-                if(copy[i][j] == 'a' && !ABC[0]){
-                    ABC[0] = true; // a에 대한 색칠 완료
-                    bfs(copy,visited,i,j);
+        for(int i=0;i< ban.length;i++){
+            for(int j=0;j< result.length;j++){
+
+                String str1 = ban[i];
+                String str2 = result[j];
+                boolean flag = true;
+                for(int m=0;m<str1.length();m++){
+                    if(ban[i].charAt(m) != '*' && ban[i].charAt(m) != str2.charAt(m)){
+                        flag = false;
+                        break;
+                    }
                 }
-                else if(copy[i][j] == 'b' && !ABC[1]){
-                    ABC[1] = true;
-                    bfs(copy,visited,i,j);
-                }
-                else if(copy[i][j] == 'c' && !ABC[2]){
-                    ABC[2] = true;
-                    bfs(copy,visited,i,j);
+
+                if(flag){
+                    check[i] = true;
                 }
             }
         }
 
-// for(int i=0;i<copy.length;i++) {
-// for (int j = 0; j < copy[0].length; j++) {
-// System.out.print(copy[i][j] + " ");
-// }
-// System.out.println();
-// }
-//
-// for(int i=0;i< visited.length;i++){
-// for(int j=0;j<visited[0].length;j++){
-// System.out.print(visited[i][j] + " ");
-// }
-// System.out.println();
-// }
-//
-// System.out.println("--------------------------------------");
-
-        for(int i=0;i< visited.length;i++){
-            for(int j=0;j<visited[0].length;j++){
-                if(!visited[i][j]) return false;
-            }
+        for(int i=0;i< check.length;i++){
+            if(!check[i]) return false;
         }
-
         return true;
-    }
-    private static void bfs(char[][] copy,boolean[][] visited,int a,int b){
-        Queue<Character> que = new LinkedList<>();
-
-        visited[a][b] = true;
-        que.add(copy[a][b]);
-
-        while(!que.isEmpty()){
-            char ch = que.poll();
-
-            if(a-1 >= 0 && copy[a-1][b] == ch && !visited[a-1][b]){
-                bfs(copy,visited,a-1,b);
-            }
-            if(a+1 < copy.length && copy[a+1][b] == ch && !visited[a+1][b]){
-                bfs(copy,visited,a+1,b);
-            }
-            if(b-1 >= 0 && copy[a][b-1] == ch && !visited[a][b-1]){
-                bfs(copy,visited,a,b-1);
-            }
-            if(b+1 < copy[0].length && copy[a][b+1] == ch && !visited[a][b+1]){
-                bfs(copy,visited,a,b+1);
-            }
-        }
-
     }
 
 }

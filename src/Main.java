1d450f6e8c6e856a97aa55b9609ef9b24a9e82a2
ssong2 <thdgorhkd97@naver.com>
2022-04-04 22:38:48 +0900

    0404

--- src/Main.java
+++ src/Main.java
@@ -6,75 +6,72 @@ import java.util.*;
 
 public class Main {
 
-    // java 불량 사용자
+    // 내일 면접을 앞두고 있어서 플로이드 와샬 알고리즘을 정리해보려 한다.
+    // 플로이드 와샬도 그래프에서 정점에서 정점까지의 최단 경로를 구하는 알고리즘인데
+    // 최단 경로를 구하는 다익스트라와의 차이점은 "모든" 정점에서의 최단경로를 구한다는 것
+    // 플로이드 와샬의 핵심은 '거쳐가는 정점'을 기준으로 최단 거리를 구한다는 것이다.
+    // 그리고 다익스트라는 음의 가중치가 있을 때 불가능하지만
+    // 플로이드 와샬은 사이클이 없다면 음수 가중치에 대한 처리가 가능하다.
 
-    // 주어진 불량 사용자 목록에 일치하는 아이디 목록의 수를 구하는 문제이다.
+    // kruskal -> 최소 신장 트리(MST)를 찾는 알고리즘이다.
+    // 가중치를 낮은 순에서 높은 순으로 오름차순 정렬한다.
+    // 가중치가 가장 낮은 간선부터 선택하면서 양쪽 정점을 union한다.(이때 두 정점이 같은 집합이면
+    // 사이클이 있는 것이므로 포함시키지 않는다)
 
-    // user_id에서 구해지는 banned_id의 수와 일치하는 아이디 목록만큼을 조합으로 구한 다음에
-    // 해당 목록이 banned_id에 일치하는 지를 확인해보려 했다.
-    // 근데 banned_id를 보면서 거기에 해당하는 문자까지 확인하려다 보니까 또 다시
-    // 다중 for문에 대한 인덱스 개념이 많이 어렵게 느껴졌다 ㅠㅠ
-    // 이중 for문을 돌면서 string을 비교하는데 거기서 또 이중 for문을 돌면서 문자 하나하나
-    // 비교해야 하는 부분이 많이 힘들었다.
 
-    static int answer = 0;
+    // (java 불량 사용자) 해결 필요
+
+    static int[][] graph;
+    static int[][] dist;
 
     public static void main(String[] args) throws IOException {
 
-        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+        graph = new int[5][5];
+        int N = 5;
+        dist = new int[N][N];
 
-        String[] banned_id = {"fr*d*", "abc1**"};
 
-        int depth = 0;
-        String[] result = new String[banned_id.length];
-        int startIdx = 0;
-
-        combination(depth,startIdx,result,user_id,banned_id);
-
-        System.out.println(answer);
-    }
-
-    private static void combination(int depth,int start,String[] result,String[] user_id,String[] banned_id){
-        if(depth == result.length){
-
-            if(isPossible(result,user_id,banned_id)){
-                answer++;
-            }
-
-            return ;
-        }
-        for(int i=start;i< user_id.length;i++){
-            result[depth] = user_id[i];
-            combination(depth+1,i+1,result,user_id,banned_id);
-        }
-    }
-
-    private static boolean isPossible(String[] result,String[] userId,String[] ban){
-        boolean[] check = new boolean[result.length];
-
-        for(int i=0;i< ban.length;i++){
-            for(int j=0;j< result.length;j++){
-
-                String str1 = ban[i];
-                String str2 = result[j];
-                boolean flag = true;
-                for(int m=0;m<str1.length();m++){
-                    if(ban[i].charAt(m) != '*' && ban[i].charAt(m) != str2.charAt(m)){
-                        flag = false;
-                        break;
-                    }
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(i==j){
+                    dist[i][j] = 0;
+                    continue;
                 }
+                dist[i][j] = 100000;
+            }
+        }
 
-                if(flag){
-                    check[i] = true;
+        addLine(0,1,5);
+        addLine(0,2,7);
+        addLine(0,4,1);
+        addLine(1,2,3);
+        addLine(1,3,6);
+        addLine(0,3,2);
+        addLine(2,3,10);
+        addLine(3,4,4);
+
+        for(int k=0;k<N;k++){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    dist[i][j] = Math.min(dist[i][j],dist[i][k] + dist[k][j]);
                 }
             }
         }
 
-        for(int i=0;i< check.length;i++){
-            if(!check[i]) return false;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
         }
-        return true;
+
+
     }
+    private static void addLine(int a,int b, int weight){
+        dist[a][b] = Math.min(dist[a][b],weight);
+        dist[b][a] = Math.min(dist[b][a],weight);
+    }
+
+
 
 }

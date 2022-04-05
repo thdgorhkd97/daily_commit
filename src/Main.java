6124b385ddf8b0f70ee3982e5add10168a069cfa
ssong2 <thdgorhkd97@naver.com>
2022-04-05 23:12:19 +0900

    0405

--- src/Main.java
+++ src/Main.java
@@ -6,72 +6,71 @@ import java.util.*;
 
 public class Main {
 
-    // 내일 면접을 앞두고 있어서 플로이드 와샬 알고리즘을 정리해보려 한다.
-    // 플로이드 와샬도 그래프에서 정점에서 정점까지의 최단 경로를 구하는 알고리즘인데
-    // 최단 경로를 구하는 다익스트라와의 차이점은 "모든" 정점에서의 최단경로를 구한다는 것
-    // 플로이드 와샬의 핵심은 '거쳐가는 정점'을 기준으로 최단 거리를 구한다는 것이다.
-    // 그리고 다익스트라는 음의 가중치가 있을 때 불가능하지만
-    // 플로이드 와샬은 사이클이 없다면 음수 가중치에 대한 처리가 가능하다.
+    // java 불량 사용자
 
-    // kruskal -> 최소 신장 트리(MST)를 찾는 알고리즘이다.
-    // 가중치를 낮은 순에서 높은 순으로 오름차순 정렬한다.
-    // 가중치가 가장 낮은 간선부터 선택하면서 양쪽 정점을 union한다.(이때 두 정점이 같은 집합이면
-    // 사이클이 있는 것이므로 포함시키지 않는다)
+    // 풀이를 찾아보고 로직을 이해하면서 직접 주석을 달아보았다.
+    // 풀이 과정은 나랑 비슷한 것 같은데 matches라는 메소드를 활용해서 백트래킹으로 조합을 구했는데
+    // matches라는 메소드에 대해서 좀 더 알아보자.
 
+    // str.matches("(.*)code(.*)"); -> code 앞뒤로 다른 문자가 와도 code가 있으면
+    //                                 true를 리턴( .contains와 같은 기능)
+    // str.matches("code"); -> code를 제외한 문자가 포함되면 false를 리턴
 
-    // (java 불량 사용자) 해결 필요
+    // contains와 matches의 차이점
+    // contains()는 인자로 전달된 문자열이 존재하는지 확인
+    // matches()는 정규표현식을 인자로 받고 동일한 패턴이면 true 리턴
 
-    static int[][] graph;
-    static int[][] dist;
+    // str.matches("java") -> 정확히 일치해야 한다.(정규표현식이 아니면)
+    // 대소문자도 구별한다. 정확히 일치해야만 true를 리턴하기 때문에
+    // 특정 문자열을 검색할 때 보다는 정규식 표현에 주로 활용한다.
+
+    static HashSet<String> set;
+    static String[] regex;
+    static String[] user;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
 
-        graph = new int[5][5];
-        int N = 5;
-        dist = new int[N][N];
+        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
+        String[] banned_id = {"fr*d*", "abc1**"};
 
+        set = new HashSet<>();
+        user = user_id;
 
-        for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                if(i==j){
-                    dist[i][j] = 0;
-                    continue;
-                }
-                dist[i][j] = 100000;
-            }
+        regex = new String[banned_id.length];
+        for(int i = 0; i < banned_id.length; i++) {
+            regex[i] = banned_id[i].replace("*", ".");
+            // *을 .으로 변경한다.( mathches()를 활용하기위함 ?)
         }
 
-        addLine(0,1,5);
-        addLine(0,2,7);
-        addLine(0,4,1);
-        addLine(1,2,3);
-        addLine(1,3,6);
-        addLine(0,3,2);
-        addLine(2,3,10);
-        addLine(3,4,4);
+        visited = new boolean[user.length];
+        backtracking(0, ""); // 내가 했던 것처럼 전체적인 조합에 대해서 구하는 메서드
 
-        for(int k=0;k<N;k++){
-            for(int i=0;i<N;i++){
-                for(int j=0;j<N;j++){
-                    dist[i][j] = Math.min(dist[i][j],dist[i][k] + dist[k][j]);
-                }
-            }
-        }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                System.out.print(dist[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(set.size());
 
 
     }
-    private static void addLine(int a,int b, int weight){
-        dist[a][b] = Math.min(dist[a][b],weight);
-        dist[b][a] = Math.min(dist[b][a],weight);
+
+    private static void backtracking(int idx,String result){
+        if(idx == regex.length) { // banned_id와 길이가 같다면
+//            System.out.println(result);
+            String[] str = result.split(" ");
+            Arrays.sort(str); // 정렬
+
+            String newstr = "";
+            for(int i = 0; i < str.length; i++) {
+                newstr += str[i]; // 배열에 있는 걸 하나의 string으로
+            }
+            set.add(newstr); // string이 set에 들어가는지(중복이 아닌지)
+            return;
+        }
+
+        for(int i = 0; i < user.length; i++) {
+            if(!visited[i] && user[i].matches(regex[idx])) {
+                visited[i] = true;
+                backtracking(idx + 1, result + " " + user[i]);
+                visited[i] = false;
+            }
+        }
     }
-
-
-
 }

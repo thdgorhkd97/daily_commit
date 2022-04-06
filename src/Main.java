4caaeabde6bca63a0e83690d070133da39d0ba06
ssong2 <thdgorhkd97@naver.com>
2022-04-06 23:56:34 +0900

    0406

--- src/Main.java
+++ src/Main.java
@@ -6,71 +6,168 @@ import java.util.*;
 
 public class Main {
 
-    // java 불량 사용자
+    // 알고리즘 스터디 공부 내용 bfs & 그래프
 
-    // 풀이를 찾아보고 로직을 이해하면서 직접 주석을 달아보았다.
-    // 풀이 과정은 나랑 비슷한 것 같은데 matches라는 메소드를 활용해서 백트래킹으로 조합을 구했는데
-    // matches라는 메소드에 대해서 좀 더 알아보자.
-
-    // str.matches("(.*)code(.*)"); -> code 앞뒤로 다른 문자가 와도 code가 있으면
-    //                                 true를 리턴( .contains와 같은 기능)
-    // str.matches("code"); -> code를 제외한 문자가 포함되면 false를 리턴
-
-    // contains와 matches의 차이점
-    // contains()는 인자로 전달된 문자열이 존재하는지 확인
-    // matches()는 정규표현식을 인자로 받고 동일한 패턴이면 true 리턴
-
-    // str.matches("java") -> 정확히 일치해야 한다.(정규표현식이 아니면)
-    // 대소문자도 구별한다. 정확히 일치해야만 true를 리턴하기 때문에
-    // 특정 문자열을 검색할 때 보다는 정규식 표현에 주로 활용한다.
-
-    static HashSet<String> set;
-    static String[] regex;
-    static String[] user;
-    static boolean[] visited;
+    static ArrayList<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
-        String[] user_id = {"frodo", "fradi", "crodo", "abc123", "frodoc"};
-        String[] banned_id = {"fr*d*", "abc1**"};
+        int[][] friends = {{1, 2}, {2, 3}, {4, 5}};
+        int[] grade = {3, 0, 5, 4, 0};
 
-        set = new HashSet<>();
-        user = user_id;
+        int n = 5;
+        int[][] map = new int[n+1][n+1];
 
-        regex = new String[banned_id.length];
-        for(int i = 0; i < banned_id.length; i++) {
-            regex[i] = banned_id[i].replace("*", ".");
-            // *을 .으로 변경한다.( mathches()를 활용하기위함 ?)
+        for(int i=0;i<friends.length;i++){
+            map[friends[i][0]][friends[i][1]] = 1;
+            map[friends[i][1]][friends[i][0]] = 1;
         }
 
-        visited = new boolean[user.length];
-        backtracking(0, ""); // 내가 했던 것처럼 전체적인 조합에 대해서 구하는 메서드
+//        for(int i=1;i<=map.length-1;i++){
+//            for(int j=1;j<=map.length-1;j++){
+//                System.out.print(map[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
-        System.out.println(set.size());
+        for(int i=1;i<=n;i++) {
+            boolean[] visited = new boolean[n + 1];
+            whoFriend(i,map,visited);
 
+            if(grade[i-1] == 0){
+                int sum = 0;
+                for(int j=0;j<list.size();j++){
+                    sum = grade[list.get(j)-1];
+//                    System.out.println(list.get(j));
+                }
+                System.out.println(sum);
+                System.out.println();
+                grade[i-1] = sum / (list.size()-1);
+            }
 
+            list.clear();
+
+        }
+
+        for(int i=0;i<grade.length;i++){
+            System.out.println((i+1) + " 번의 평점 : " + grade[i]);
+        }
     }
 
-    private static void backtracking(int idx,String result){
-        if(idx == regex.length) { // banned_id와 길이가 같다면
-//            System.out.println(result);
-            String[] str = result.split(" ");
-            Arrays.sort(str); // 정렬
+    private static void whoFriend(int start,int[][] map,boolean[] visited){
+        Queue<Integer> que = new LinkedList<>();
+        visited[start] = true;
+        que.add(start);
+        while(!que.isEmpty()){
+            int v = que.poll();
+            list.add(v);
 
-            String newstr = "";
-            for(int i = 0; i < str.length; i++) {
-                newstr += str[i]; // 배열에 있는 걸 하나의 string으로
-            }
-            set.add(newstr); // string이 set에 들어가는지(중복이 아닌지)
-            return;
-        }
-
-        for(int i = 0; i < user.length; i++) {
-            if(!visited[i] && user[i].matches(regex[idx])) {
-                visited[i] = true;
-                backtracking(idx + 1, result + " " + user[i]);
-                visited[i] = false;
+            for(int i=1;i<=map.length-1;i++){
+                if(map[v][i] == 1 && !visited[i]){
+                    que.add(i);
+                    visited[i] = true;
+                }
             }
         }
+
     }
 }
+
+        /*
+        int[] priorities = {1,1,9,1,1,1};
+        int location = 0;
+
+        Queue<int[]> que = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<priorities.length;i++){
+            int[] add = new int[2];
+            list.add(priorities[i]);
+            add[0] = priorities[i];
+            if(i== location){
+                add[1] = 1;
+            }
+            else{
+                add[1] = 0;
+            }
+            que.add(add);
+        }
+
+        int answer = 0;
+
+        while(true){
+
+            int[] arr = new int[2];
+            arr = que.poll();
+
+            boolean isMax = true;
+            for(int i=0;i<list.size();i++){
+                if(arr[0] < list.get(i)){
+                    isMax = false;
+                    break;
+                }
+            }
+
+            if(!isMax){
+                que.add(arr);
+            }
+            else{
+                answer++;
+                list.remove(new Integer(arr[0]));
+                if(arr[1] == 1){
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+         */
+
+        /*
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> que = new LinkedList<>();
+        int old = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            String[] operator = str.split(" ");
+            if(!operator[0].equals("push")){
+                switch (operator[0]){
+                    case "pop" :
+                        if(que.isEmpty()) list.add(-1);
+                        else list.add(que.poll());
+
+                        if(que.isEmpty()) old = 0;
+                        break;
+                    case "size" :
+                        list.add(que.size());
+                        break;
+                    case "empty" :
+                        if(que.isEmpty()) list.add(1);
+                        else list.add(0);
+                        break;
+                    case "front" :
+                        if(que.isEmpty()) list.add(-1);
+                        else list.add(que.peek());
+                        break;
+                    case "back" :
+                        if(que.isEmpty()) {
+                            list.add(-1);
+                        }
+                        else list.add(old);
+                        break;
+                }
+            }
+            else{
+                if(old == 0) old = Integer.parseInt(operator[1]);
+                que.offer(Integer.parseInt(operator[1]));
+
+            }
+        }
+         */
+

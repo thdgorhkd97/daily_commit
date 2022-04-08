45a74bc1a1b0ce48b65399d8479caeb796bd7b80
ssong2 <thdgorhkd97@naver.com>
2022-04-08 23:41:57 +0900

    0408

--- src/Main.java
+++ src/Main.java
@@ -6,60 +6,57 @@ import java.util.*;
 
 public class Main {
 
-    // java 줄 서는 방법
-    // 사전 순으로 순열을 표현하는 방법으로 해서 쭉 모든 케이스를 표현한 후에
-    // k번째를 구하려고 했는데 그렇게 하니 몇몇 케이스에서 시간과 메모리 초과가 발생한다
-    // 그래서 다른 방법을 찾아야만했다.
-
-    // n = 4, k = 7인 경우를 생각해보면
-//            1, 2, 3, 4
-//            1, 2, 4, 3
-//            1, 3, 2, 4
-//            1, 3, 4, 2
-//            1, 4, 2, 3
-//            1, 4, 3, 2
-//            2, 1, 3, 4
-//            2, 1, 4, 3
-//            2, 3, 1, 4
-//            2, 3, 4, 1
-//            2, 4, 1, 3
-//            2, 4, 3, 1
-//            3, 1, 2, 4
-//            …
-    // 이렇게 되는 건데 즉 맨 앞자리는 (n-1)! 만큼 반복되므로 맨 앞자리를 유추할 수 있다.
-    // 그 후에는 2를 제외한 [1,3,4] 중에 k = (k-1) % (n-1)!을 통해 구할 수 있다고 한다
+    // java 파괴되지 않은 건물
+    // board 배열을 갱신해 나가면서 skill의 1 ~ 4 까지의 범위를
+    // 수정해가는데 skill을 for문으로 돌리고 skill의 1~4 까지를 for문으로 하는데
+    // 3중 for문이 되어 효율성을 초과하게 된다...
+    // for문을 돌리긴 해야 할 것 같은데 오히려 문제가 간단한 편이라 시간을 어떻게 줄여야 하는지
+    // 잘 모르겠다 (효율성을 체크해야 한다..)
 
     public static void main(String[] args) throws IOException {
 
-        int n = 3;
-        long k = 5;
+        int[][] board = {{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5}};
+        int[][] skill = {{1,0,0,3,4,4},{1,2,0,2,3,2},{2,1,0,3,1,2},{1,0,1,3,3,1}};
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-        long factorial = 1;
-        for(int i=1;i<=n;i++){
-            list.add(i);
-            factorial *= i;
+        int[][] sum = new int[board.length][board[0].length];
+        for(int[] row : skill){
+            int degree = 0;
+            if(row[0] == 1){ // type == 1 공격
+                degree = row[5] * (-1);
+            }
+            else{ // type == 2 힐
+                degree = row[5];
+            }
         }
 
-        long remain = k-1;
-        int[] answer = new int[n];
-
-        int i = 0;
-        while(i < n){
-            factorial = factorial / (n-i); // n-i = 3 2 1 factorial = 2 1 1
-            long value = remain / factorial; // value = 2 4 4
-            answer[i++] = list.get((int) value);
-            list.remove((int)value);
-            remain = remain % factorial;
-
-            System.out.println("factorial = " + factorial + " value = " + value + " remain = " + remain);
+        /* 정확성에 대해서는 맞지만 효율성 X
+        for(int i=0;i<skill.length;i++){
+            if(skill[i][0] == 1){ // type == 1 (공격)
+                for(int j=skill[i][1]; j<=skill[i][3];j++){
+                    for(int k=skill[i][2]; k<=skill[i][4];k++){
+                        board[j][k] -= skill[i][5];
+                    }
+                }
+            }
+            else{ // type == 2 (힐)
+                for(int j=skill[i][1]; j<=skill[i][3];j++){
+                    for(int k=skill[i][2]; k<=skill[i][4];k++){
+                        board[j][k] += skill[i][5];
+                    }
+                }
+            }
         }
 
+        int answer = 0;
 
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] > 0) answer++;
+            }
+        }
 
-
-
-
+        System.out.println(answer);
+         */
     }
+
 }

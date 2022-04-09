27b15772c11abdc92f72457785f79a24e640f424
ssong2 <thdgorhkd97@naver.com>
2022-04-09 23:49:13 +0900

    0409

--- src/Main.java
+++ src/Main.java
@@ -7,56 +7,50 @@ import java.util.*;
 public class Main {
 
     // java 파괴되지 않은 건물
-    // board 배열을 갱신해 나가면서 skill의 1 ~ 4 까지의 범위를
-    // 수정해가는데 skill을 for문으로 돌리고 skill의 1~4 까지를 for문으로 하는데
-    // 3중 for문이 되어 효율성을 초과하게 된다...
-    // for문을 돌리긴 해야 할 것 같은데 오히려 문제가 간단한 편이라 시간을 어떻게 줄여야 하는지
-    // 잘 모르겠다 (효율성을 체크해야 한다..)
+
+    // 누적합을 활용해야 활용성을 통과할 수 있는 문제다
+    // 누적합으로 문제를 해결하는 방법에 대해서 잘 알고 있지 않아서 시간이 상당히 걸렸다
+    // 풀이 방법에 대해서는 이해가 가는 것 같은데 막상 array의 범위를 벗어나게 되는데 이게
+    // 정확한 풀이 방법을 아직 이해하지 못한 것 같다.
+    // 해당 문제를 마무리하고 누적합의 개념에 대해 제대로 파악해야 할 필요가 있을 것 같다..
+
 
     public static void main(String[] args) throws IOException {
 
         int[][] board = {{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5},{5,5,5,5,5}};
         int[][] skill = {{1,0,0,3,4,4},{1,2,0,2,3,2},{2,1,0,3,1,2},{1,0,1,3,3,1}};
 
-        int[][] sum = new int[board.length][board[0].length];
+        int[][] prefix = new int[board.length][board[0].length];
+
         for(int[] row : skill){
+            int r1 = row[1];
+            int c1 = row[2];
+            int r2 = row[3];
+            int c2 = row[4];
             int degree = 0;
-            if(row[0] == 1){ // type == 1 공격
-                degree = row[5] * (-1);
+            if(row[0] == 1){
+                degree = -row[5];
             }
-            else{ // type == 2 힐
+            else{
                 degree = row[5];
             }
-        }
 
-        /* 정확성에 대해서는 맞지만 효율성 X
-        for(int i=0;i<skill.length;i++){
-            if(skill[i][0] == 1){ // type == 1 (공격)
-                for(int j=skill[i][1]; j<=skill[i][3];j++){
-                    for(int k=skill[i][2]; k<=skill[i][4];k++){
-                        board[j][k] -= skill[i][5];
-                    }
-                }
-            }
-            else{ // type == 2 (힐)
-                for(int j=skill[i][1]; j<=skill[i][3];j++){
-                    for(int k=skill[i][2]; k<=skill[i][4];k++){
-                        board[j][k] += skill[i][5];
-                    }
-                }
+            for(int i=r1;i<r2;++i){
+                prefix[i][c1] += degree;
+                prefix[i][c2] += degree;
             }
         }
 
         int answer = 0;
-
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
+                if(j!=0) prefix[i][j] += prefix[i][j-1];
+                board[i][j] += prefix[i][j];
                 if(board[i][j] > 0) answer++;
             }
         }
 
         System.out.println(answer);
-         */
     }
 
 }

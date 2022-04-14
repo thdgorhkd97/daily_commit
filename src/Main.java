b4846862b1e3386b101e6fdcde36b688d3fd85b2
ssong2 <thdgorhkd97@naver.com>
2022-04-14 23:20:07 +0900

    0414

--- src/Main.java
+++ src/Main.java
@@ -5,59 +5,70 @@ import java.util.*;
 
 public class Main {
 
-    // java 징검다리
+    // java 숨바꼭질
+    // bfs를 활용한 문제라고 할 수 있다.
+    // 정해진 위치에서 해당 위치로 -1 +1 *2 의 위치로 움직이는 걸 선택하면서
+    // 최소횟수로 이동할 수 있는 경우를 구하는 것이다.
+    // 처음 위치인 N에서부터 -1 +1 *2를 bfs로 넣고 만약 그 수에 해당하는
+    // boolean 배열인 visited가 false 라면 해당 수에 처음 접근하는 것이므로
+    // 최소라는 소리가 되는 거니까 그 수를 만든 경우에서 +1을 해서 넣는다.
+    // ex) dp[10] = dp[5] + 1( visited[10] = false 라면)
 
-    // 이분탐색으로 분류된 문제이기에 이분탐색으로 해결해 보려고 했는데
-    // 정렬된 rocks[] 배열을 만들어서 이를 기준으로 이분탐색을 해야 할 것 같긴 했는데
-    // 이분탐색의 기준을 어떻게 처리해야 할 지 몰라서 알아보았다.
-    // 역시 이분탐색의 기준이 되는 건 가능한 최소값이었는데 이 최소값을 갱신하는 기준은
-    // 해당 거리를 가질 수 있는 바위간의 거리였다.
-    // 즉 어떤 거리를 기준으로 바위간의 거리가 이보다 작으면 해당 바위를 제거하는 식으로
-    // 진행하면서 어떤 거리가 n개의 바위를 제거해서 만들어 질 수 있는 거리인지를 보는 것이다.
-    // 코드 자체는 이분탐색 그 자체이기에 간단했는데
-    // 이 로직을 이해하는 게 상당히 힘들었다.
-    // 시간이 굉장히 오래걸렸고 level 4에 해당하는 문제답게...
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
 
-        int distance = 25;
-        int[] rocks = {2,14,11,21,17};
-        int n = 2;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Arrays.sort(rocks);
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(stk.nextToken());
+        int K = Integer.parseInt(stk.nextToken());
 
-        int left = 0;
-        int right = distance;
-        int mid = 0;
+        boolean[] visited = new boolean[100001];
+        dp = new int[100001];
 
-        int answer = 0;
-
-        while(left <= right){
-            mid = (left + right) / 2;
-            int prev = 0;
-            int removeCnt = 0;
-
-            for(int i=0;i<rocks.length;i++){
-                if(rocks[i] - prev < mid){ // 바위간의 거리가 mid보다 작으면 바위 제거
-                    removeCnt++; // 제거한 바위 수
-                    if(removeCnt > n) break; // n개까지 제거 가능
-                }
-                else{
-                    prev = rocks[i];
-                }
-            }
-
-            if(removeCnt > n){ // 만약 제거한 바위가 n개 이상이면 바위간의 거리가 너무 길다는 것이므로 수정
-                right = mid - 1;
-            }
-            else {
-                answer = Math.max(answer,mid);// mid이하의 거리를 가진 바위간의 거리가 적절하니까 answer 초기화
-                left = mid + 1;
-            }
-        }
-
+        dp[N] = 0;
+        System.out.println(bfs(N,K,visited));
 
 
     }
+
+    private static int bfs(int N,int K,boolean[] visited){
+
+        Queue<Integer> que = new LinkedList<>();
+        que.add(N);
+        visited[N] = true;
+
+        while(!que.isEmpty()){
+            int v = que.poll();
+
+            int nextMove = 0;
+            for(int i=0;i<3;i++) {
+
+                switch (i){
+                    case 0 : nextMove = v-1; break;
+                    case 1 : nextMove = v+1; break;
+                    case 2 : nextMove = 2*v; break;
+                }
+
+
+
+                if (nextMove >= 0 && nextMove <= 100000) {
+                    if (!visited[nextMove]) {
+                        que.offer(nextMove);
+                        dp[nextMove] = dp[v] + 1;
+                        visited[nextMove] = true;
+//                        System.out.println(nextMove + " 는 " + dp[v]+ " 에서 1번 더 이동");
+                    }
+                }
+                if (nextMove == K) {
+                    return dp[nextMove];
+                }
+            }
+
+        }
+        return 0;
+    }
+
 
 }

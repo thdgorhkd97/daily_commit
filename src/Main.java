92c9ff989009c34a168ff4f3b84117c82362f17a
ssong2 <thdgorhkd97@naver.com>
2022-04-18 23:54:12 +0900

    0418

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,80 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 차이를 최대로
+    // java 1759 암호 만들기
+    // 주어지는 C 개의 알파벳을 이용해서 L개의 알파벳을 고른 후에 만들어지는 문자열 중에서
+    // 모음이 1개 이상이고 자음이 2개 이상인 것을 고르고
+    // 해당 문자열이 사전순으로 나열되어 있어야 하기 때문에 해당 문자열을 Chararray[]로 바꿔서
+    // 문자열 각각이 사전순으로 나열되어 있는지 확인한 다음
+    // 해당 조건을 만족하는 모든 문자열을 마지막으로 사전순 나열한다.
+    // 이렇게 하면 정확성은 맞는게 확실한 것 같은데 메모리 초과가 발생한다.
+    // 일단 모든 문자열을 확인해야 해서 순열로 문자열을 만드는 건 맞는 것 같은데
+    // 각각의 문자열이 사전순으로 배열되어 있는지를 확인하는 과정에서
+    // StringBuffer와 CharArray를 활용하는 게 메모리를 너무 많이 먹는 것 같다 ㅠㅠ
 
-//    N개의 정수로 이루어진 배열 A가 주어진다.
-//    이때, 배열에 들어있는 정수의 순서를 적절히 바꿔서 다음 식의 최댓값을 구하는 프로그램을 작성하시오.
-//    |A[0] - A[1]| + |A[1] - A[2]| + ... + |A[N-2] - A[N-1]|
-
-    // 배열 A를 순열로 순서를 바꿔가면서 가능한 경우의 수를 모두 구한 다음
-    // 최대값을 비교한다.
-    // 처음에 순열로 구해야 하는 것을 조합으로 구했다가 시간이 많이 걸렸다..
-
-
-    static int max = 0;
+    static ArrayList<String> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
         StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-        int[] num = new int[N];
-        for(int i=0;i<N;i++){
-            num[i] = Integer.parseInt(stk.nextToken());
+
+        int L = Integer.parseInt(stk.nextToken());
+        int C = Integer.parseInt(stk.nextToken());
+
+        String[] str = new String[C];
+
+        stk = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<C;i++){
+            str[i] = stk.nextToken();
         }
 
-
-        int[] result = new int[N];
-        boolean[] visited = new boolean[N];
+        String[] result = new String[L];
+        boolean[] visited = new boolean[C];
         int depth = 0;
-        combination(num,result,visited,depth);
+        combination(result,str,L,visited,depth);
 
-        System.out.println(max);
+        Collections.sort(list);
+
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
 
     }
-    private static void combination(int[] num,int[] result,boolean[] visited,int depth){
-        if(depth == result.length){
-            int sum = 0;
-            for(int i=0;i< result.length-1 ;i++){
-                sum += Math.abs(result[i] - result[i+1]);
+
+    private static void combination(String[] result,String[] str,int L,boolean[] visited,int depth){
+        if(depth == L){
+            int mo = 0; // 모음 수
+            int ja = 0; // 자음 수
+            StringBuffer sb = new StringBuffer();
+            for(int i=0;i<result.length;i++){
+                if(result[i].equals("a") ||result[i].equals("e") ||result[i].equals("i") ||result[i].equals("o") ||result[i].equals("u") ){
+                    mo++;
+                }
+                else ja++;
+
+                sb.append(result[i]);
             }
-            max = Math.max(max,sum);
+
+            char[] string = sb.toString().toCharArray();
+            Arrays.sort(string);
+            StringBuffer sb2 = new StringBuffer();
+            for(int i=0;i<string.length;i++){
+                sb2.append(string[i]);
+            }
+
+            if(mo>=1 && ja>=2 && sb.toString().equals(sb2.toString())) {
+                list.add(sb.toString());
+            }
 
             return ;
         }
-
-        for(int i=0;i<num.length;i++){
+        for(int i=0;i<str.length;i++){
             if(!visited[i]){
-                result[depth] = num[i];
+                result[depth] = str[i];
                 visited[i] = true;
-                combination(num,result,visited,depth+1);
+                combination(result,str,L,visited,depth+1);
                 visited[i] = false;
             }
         }

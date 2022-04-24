8c186e08e2deb3a620b661d6cb9750c68b272758
ssong2 <thdgorhkd97@naver.com>
2022-04-24 23:57:37 +0900

    0424

--- src/Main.java
+++ src/Main.java
@@ -8,48 +8,95 @@ import java.util.*;
 
 public class Main {
 
-    // line 코팅테스트 문제
-    // arr과 brr을 일치시키기 위한 과정을 거치면서 앞에서부터
-    // 길이를 맞추고 그 다음 인덱스에 그만큼을 더하거나 빼면서 끝까지 맞춰간다.
-    // 근데 이를 다 맞는지를 확인하는 과정에서 arr과 brr을 끝까지 확인하면서
-    // 모두 맞는지를 확인하는 과정이 오래걸린 것 같다ㅠㅠ
+    // java 10973 이전 순열
+    // combination method에서 result를 출력했을 때는 사전순으로 잘 정렬되서 나오는데
+    // 왜 list.add(result)를 해서 list를 확인하면 제대로 나오지 않는지 모르겠다...
+    // list의 선언도 했고 static이고 int[]을 받는 데 왜...
+
+    static ArrayList<int[]> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
-        int[] arr = {3,7,2,4};
-        int[] brr = {4,5,5,2};
-        int answer = 0;
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
-        int idx = 0;
-        while(!arrEqaulbrr(arr,brr)){
-            for(int i=0;i<arr.length;i++){
-                if(arr[i] != brr[i]){
-                    idx = i;
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        int[] target = new int[N];
+        for(int i=0;i<N;i++){
+            target[i] = Integer.parseInt(stk.nextToken());
+        }
+
+        int[] arr = new int[N];
+        for(int i=1;i<=N;i++){
+            arr[i-1] = i;
+        }
+
+        boolean flag = true;
+        for(int i=0;i< target.length;i++){
+            if(target[i] != i+1){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            System.out.println("-1");
+            return ;
+        }
+
+
+        boolean[] visited = new boolean[N];
+        int depth = 0;
+        int[] result = new int[N];
+        combination(visited,arr,depth,result);
+
+        for(int i=0;i<list.size();i++){
+            for(int j=0;j<list.get(i).length;j++){
+                System.out.print(list.get(i)[j] + " ");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<list.size();i++){
+            flag = true;
+
+            for(int j=0;j<list.get(i).length;j++){
+                if(list.get(i)[j] != target[j]){
+                    flag = false;
                     break;
                 }
             }
 
-            int change = Math.abs(arr[idx] - brr[idx]);
-            if(arr[idx] > brr[idx]){
-                arr[idx] -= change;
-                arr[idx+1] += change;
-            }else{
-                arr[idx] += change;
-                arr[idx+1] -= change;
+            if(flag) {
+                for (int j = 0; j < list.get(i-1).length; j++) {
+                    System.out.print(list.get(i - 1)[j] + " ");
+                }
+                return ;
             }
 
-            answer++;
         }
 
-        System.out.println(answer);
+
 
     }
-    private static boolean arrEqaulbrr(int[] arr, int[] brr){
+    private static void combination(boolean[] visited,int[] arr,int depth,int[] result){
+        if(depth == result.length){
+
+//            for(int i=0;i< result.length;i++){
+//                System.out.print(result[i] + " ");
+//            }
+//            System.out.println();
+            list.add(result);
+            return ;
+        }
+
         for(int i=0;i<arr.length;i++){
-            if(arr[i] != brr[i]){
-                return false;
+            if(!visited[i]){
+                result[depth] = arr[i];
+                visited[i] = true;
+                combination(visited,arr,depth+1,result);
+                visited[i] = false;
             }
         }
-        return true;
     }
 }

f332727dc0db68184c43283059c592b4aa5d529e
ssong2 <thdgorhkd97@naver.com>
2022-04-29 23:41:59 +0900

    0429

--- src/Main.java
+++ src/Main.java
@@ -8,58 +8,46 @@ import java.util.*;
 
 public class Main {
 
-    // java 알고리즘 스터디
-    // java programmers 가장 큰 정사각형 찾기
-
-    // 1로 이루어진 가장 큰 정사각형의 넓이를 찾는 문제다
-    // 처음에는 1인 board[i][j]에서 그 점을 시작으로 오른쪽으로 쭉 1인 부분과
-    // 아래로 쭉 1인 부분을 찾아서 그중 작은 값이 정사각형의 길이가 되니까
-    // 그렇게 구해지는 정사각형의 최대값을 구하려고 했는데
-    // 1인 지점을 찾을 때마다 오른쪽으로 아래로 길이를 구해서 다중 for문을 돌리고 하면
-    // 지나치게 오래 걸린다.
-
-    // 그래서 알아본 방법으로는 정사각형의 오른쪽 아래점을 기준으로 길이가 2인 정사각형을
-    // 계속해서 알아간다고 생각하면서
-    // 그런 정사각형이 어디까지 이어지는지 알아보는식으로 진행하면 된다고 한다.
-
+    // java 1476 날짜 계산
+    // E, S, M 이 정해진 범위가 있기 때문에 마치 시계처럼 가장 작은 단위가
+    // 정해진 범위를 벗어나면 다음 단위가 1씩 올라가는 식으로 구현을 하는데
+    // 특정한 시간이 주어지면 그에 해당하는 E S M을 구하는 게 아니라 반대로
+    // E S M이 주어지고 그에 해당하는 날짜를 구하는 것이기 때문에 1 1 1 로
+    // 세팅한 상태에서 주어진 시간이 될 때까지 하루하루 더해나간다.
 
     public static void main(String[] args) throws IOException {
 
-        int[][] board = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int answer = 1234;
-        for(int i=1;i<board.length;i++){
-            for(int j=1;j<board[i].length;j++){
-                if(board[i][j] == 1&&board[i-1][j-1] > 0 && board[i-1][j] > 0 && board[i][j-1] > 0){
-                    board[i][j] = Math.min(Math.min(board[i-1][j-1],board[i-1][j]),board[i][j-1]);
-                    board[i][j] += 1;
-                }
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+
+        int E = Integer.parseInt(stk.nextToken());
+        int S = Integer.parseInt(stk.nextToken());
+        int M = Integer.parseInt(stk.nextToken());
+
+        int e=1,s=1,m=1;
+        int year = 1;
+        while(true){
+            if(e==E && s==S && m==M) {
+                System.out.println(year);
+                break;
             }
-        }
-
-        int max = 0;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++) {
-                if (max < board[i][j]) {
-                    max = board[i][j];
-                }
+            e+=1;
+            s+=1;
+            m+=1;
+            if(e==16) {
+                e=1;
             }
-        }
-
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                System.out.print(board[i][j] + " ");
+            if(s==29) {
+                s=1;
             }
-            System.out.println();
+            if(m==20) {
+                m=1;
+            }
+
+            year ++;
+
         }
-
-        answer = max * max;
-
-
-
-
-
-
 
     }
 }

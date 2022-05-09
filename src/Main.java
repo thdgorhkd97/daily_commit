250baf3310bc7a9f27a2c8fd62faa9818b728759
송해광 <thdgorhkd97@naver.com>
2022-05-09 15:32:07 +0900

    0509

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
 
-    // java baekjoon 15650
-    // 전체 길이와 구하고자 하는 순열의 크기가 주어지면 중복을 제외한
-    // 구하고자 하는 순열의 조합을 출력한다.
-    // startIndex와 depth를 +1 해가면서 재귀호출하는 식으로 구현할 수 있고
-    // 이제 순열이나 조합에 대해서는 상황에 맞게 활용할 수 있다고 생각한다.
-    // 다만 순열이나 조합은 재귀호출을 통해서 구현하기 때문에 n이 10이상일 때는
-    // 결코 사용하면 안 된다.
+    // java baekjoon 16194 카드 구매하기 2
+    // 처음에는 단순히 구하고자 하는 N을 곱해서 만들 수 있는 것에 대해서만 고려했다.
+    // 즉 N이 10이면 1,2,5,10 만 보면서 몫을 곱해서 최소를 구했는데 틀렸다는 걸 보니
+    // 숨겨진 테스트 케이스에 대해서는 이것저것 합쳐서 답으로 도출되는 경우가 있는 것으로 보인다.
+
+    // 결국 주어진 N을 만들 수 있는 모든 경우의 수에 대해서 고려해야 했다.
+    // dp에 저장된 값에서 dp[i] = dp[i-j] + arr[j] 를 통해 구했다.
+    // ex) dp[5] = dp[3] + arr[2] 이건 5장을 만들 수 있는 경우를 구하려면
+    //     for문을 돌면서 i-j 장을 만들 수 있는 최소의 경우에 j 장의 가격을 더해서
+    //     최소값을 구하는 식으로 for문을 돈다.
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
+
         StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-
-        int N = Integer.parseInt(stk.nextToken());
-        int M = Integer.parseInt(stk.nextToken());
-
-        int[] arr = new int[N];
-
-        for(int i=0;i<N;i++){
-            arr[i] = i+1;
+        for(int i=1;i<=N;i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int r = M;
-        int depth = 0;
-        int[] result = new int[r];
-        int start = 0;
-        combination(arr,result,r,depth,start);
-
-    }
-
-    private static void combination(int[] arr,int[] result,int r, int depth, int start){
-        if(depth == result.length){
-            for(int i=0;i<result.length;i++){
-                System.out.print(result[i] + " ");
+        for(int i=1;i<=N;i++){
+            dp[i] = arr[i];
+            for(int j=1;j<=i;j++){
+                dp[i] = Math.min(dp[i],dp[i-j] + arr[j]);
             }
-            System.out.println();
-            return ;
-        }
-
-        for(int i=start;i<arr.length;i++){
-               result[depth] = arr[i];
-               combination(arr,result,r,depth+1,i+1);
 
         }
+
+        System.out.println(dp[N]);
+
     }
-
 }

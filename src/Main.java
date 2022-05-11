2cbbf880419f9a59020e36065964422e3f5f55ff
송해광 <thdgorhkd97@naver.com>
2022-05-11 23:58:39 +0900

    0511

--- src/Main.java
+++ src/Main.java
@@ -6,56 +6,55 @@ import java.util.*;
 
 public class Main {
 
-    // java 골드바흐의 추측
-    // 에라토스테네스의 체를 구현하는데 생각보다 오래걸렸다 ㅠㅠ
-    // 저 부분을 소수인지 아닌지를 나는 확인을 따로 했는데 그게 시간을 많이 잡는 것 같다
-    // 골드바흐의 추측을 하다 끝났는데 내일 마무리 해야겠다..
+    // java baekjoon 9020 골드바흐의 추측
+    // 정해진 수를 2로 나눈다음 해당 수의 양쪽을 +1/ -1해가면서
+    // 그 두 수가 소수인지를 파악한다.
+    // 양쪽에 +1/-1을 했기 때문에 합은 무조건 맞을 것이고 두 수가 소수인지는
+    // isPrime[] 배열에서 해당하는 인덱스가 true/false 를 확인하면 되기 때문에
+    // 내가 처음에 생각했던 합이 일치하는지를 파악하고 그게 소수인지를 보는 것보다
+    // 훨씬 단순하게 구현가능했다
+
+    public static boolean[] isPrime = new boolean[10001];
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean[] isPrime = new boolean[1000001]; // 소수를 false로 남게 한다
-        EratosTenes(isPrime);
 
-        for(int i=0;i<isPrime.length;i++){
-            if(!isPrime[i]) System.out.println(i);
-        }
+        Erathostenes(); // 에라토스테네스의 체로 isPrime의 범위에 있는 소수를 판별
 
-        while(true){
+        int T = Integer.parseInt(br.readLine());	// 테스트케이스
+
+        while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
+            int first_partition = n / 2;
+            int second_partition = n / 2;
 
-            if(n== 0) break;
+            while (true) {
 
-            goldbache(n,isPrime);
-        }
-
-
-
-
-    }
-
-    private static void EratosTenes(boolean[] isPrime) {
-
-        // 소수는 false
-        // 1은 소수가 아니므로 제외
-        isPrime[0] = isPrime[1] = true;
-
-        for(int i=2; i*i<=isPrime.length-1; i++){
-            // prime[i]가 소수라면
-            if(!isPrime[i]){
-                // prime[j] 소수가 아닌 표시
-                for(int j=i*i; j<=isPrime.length-1; j+=i) isPrime[j] = true;
+                // 두 파티션이 모두 소수일 경우
+                if (!isPrime[first_partition] && !isPrime[second_partition]) {
+                    System.out.println(first_partition + " " + second_partition);
+                    break;
+                }
+                first_partition--;
+                second_partition++;
             }
         }
 
-
     }
 
-    private static void goldbache(int n,boolean[] isPrime) {
+    // 에라토스테네스의 체
+    public static void Erathostenes() {
+        isPrime[0] = isPrime[1] = true;
 
-        // 처음은 for(int i=0;i<n;i++)
-        // 시간이 너무 많이 걸려서 boolean[] isPrime을 봤는데...
-
+        for (int i = 2; i <= Math.sqrt(isPrime.length); i++) {
+            if (isPrime[i])
+                continue;
+            for (int j = i * i; j < isPrime.length; j += i) {
+                isPrime[j] = true;
+            }
+        }
     }
+
 }

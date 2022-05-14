f63f2934f90a545611ad7471a96c9a34acfa8f3c
송해광 <thdgorhkd97@naver.com>
2022-05-14 23:36:16 +0900

    0514

--- src/Main.java
+++ src/Main.java
@@ -12,49 +12,40 @@ public class Main {
     // 10,000 이하의 자연수로 이루어진 길이 N짜리 수열이 주어진다.
     // 이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이를 구하는 프로그램을 작성하시오.
 
-    // len을 1부터 시작해서 arr.length까지 되도록 + 해가면서
-    // 2중 for문을 통해 어디부터 어디까지 더하는 구역을 구해서 더했는데
-    // 로직자체가 문제인지 구현과정이 문제인지 잘 해결되지는 못했다....
+    // for문으로 구현하면 시간초과가 나는 걸 보면 맞는 지 모르겠지만 투포인터로 구현해야 시간에 대한 부분이 해결될 것 같다
+    // 근데 if(total >= s && min > end - start) min = end - start; 이 부분을 안 넣어서 계속 실패했다 ㅠㅠ
+    // 해당부분을 보면 total이 주어진 s보다 커야할 것 && 현재 min보다 최소값으로 갱신가능할 것이라는 조건 인데
+    // 이를 추가했어야 min을 계속해서 갱신가능하며 나는 그냥 투포인터로 합계만 확인하는 과정만 본것 같다..
+
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(stk.nextToken());
-        int S = Integer.parseInt(stk.nextToken());
+        int n = Integer.parseInt(stk.nextToken());
+        int s = Integer.parseInt(stk.nextToken());
 
-        int[] arr = new int[N];
+        int[] arr = new int[n];
 
         stk = new StringTokenizer(br.readLine()," ");
         for(int i=0;i<arr.length;i++){
             arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int len = 1;
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int total = 0;
+        while(start <= n && end <= n) {
+            if(total >= s && min > end - start) min = end - start;
 
-        while(len < arr.length){
-            int left = 0;
-            int right = left + len;
-
-            boolean flag = true;
-            for(int i=0;i<arr.length-len;i++){
-                int sum = 0;
-                for(int j=i;j<right;j++){
-                    sum += arr[j];
-                }
-                if(sum >= S){
-                    flag = false;
-                    break;
-                }
-            }
-
-            if(!flag) break;
-
-            len++;
+            if(total < s) total += arr[end++];
+            else total -= arr[start++];
         }
 
-        System.out.println(len);
+        if(min == Integer.MAX_VALUE) System.out.println("0");
+        else System.out.println(min);
 
     }
 }

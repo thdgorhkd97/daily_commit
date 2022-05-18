29ddb461382198a1d88517f41d2ab7c52701ad66
송해광 <thdgorhkd97@naver.com>
2022-05-18 18:10:35 +0900

    0518

--- src/Main.java
+++ src/Main.java
@@ -8,67 +8,67 @@ import java.util.*;
 
 public class Main {
 
-    // algoexpert easy 1
-    // sequence가 array의 부분집합에 해당하는 걸 묻는 문제다
-    // index를 구해서 오름차순인지 확인하면 되는 문제라고 생각했는데 생각보다 예외조건이 상당히 많았다
-    // 문제에 예외조건이 주어지는 게 아니라 테스트 케이스를 보면서 하나씩 해결해야 하는 게 algoexpert
-    // 의 특징이라고 생각된다.( 생길 수 있는 예외조건을 미리 파악해야 하며 테스트 케이스를 통해 이를 알아내야 하는것)
-    // 1. sequence가 array보다 클 경우 -> size 확인해서 false 리턴
-    // 2. sequence에 array에 없는 원소가 있을 경우 -> flag를 통해 있는지 없는지 확인
-    // 3. sequence나 array에 중복되는 원소가 있을 경우 -> 마지막 확인 조건을 arr[i] >= arr[i+1]로 하여 중복되는 경우를 포함하도록 한다.
-    // 근데 마지막 하나의 테스트 케이스에 대해 이해가 잘 가지 않는다. [1,1,1,1,1]에 [1,1,1]이 부분집합이 되냐 안되냐는 건데
-    // false를 리턴하기를 바라는데 나는 이게 true로 반환되어야 하지 않냐는 생각때문에 이 테스트 케이스를 해결하지 못하겠다..
+    // java baekjoon 2644 촌수계산
+    // 처음에는 tree를 구현해서 depth 차이를 계산해야 하나 싶었는데 생각해보니
+    // 부모 자식 관계만 파악하면 되기 때문에 tree를 구현할 필요까진 없을 것으로 보였다.
+    // 그래서 부모 자식 관계만 넣은 일차원 배열(d)을 만든 다음에
+    // bfs를 통해 첫 지점에서부터 출발해서 2차원 배열 map[][] 의 map[v][i]가 1이면
+    // 연결된 것이기 때문에 거리를 입력한다는 의미에서 d[i] = d[v] + 1을 해준다.
+    // 그러다가 원하는 도착지점에 도달하면 로직을 끝내고 만약 bfs로 타고타고 가더라도
+    // 해당 정점을 거치지 않았다면 연결되어 있지 않다는 것이므로 -1을 리턴한다.
 
     public static void main(String[] args) throws IOException {
 
-        List<Integer> array = new ArrayList<>();
-        List<Integer> sequence = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arrayValue = {1,1,1,1,1};
-        int[] sequenceValue = {1,1,1};
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i=0;i< arrayValue.length;i++){
-            array.add(arrayValue[i]);
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+
+        int one = Integer.parseInt(stk.nextToken());
+        int two = Integer.parseInt(stk.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] map = new int[n+1][n+1];
+
+        for(int i=0;i<m;i++){
+            stk = new StringTokenizer(br.readLine()," ");
+            int x = Integer.parseInt(stk.nextToken());
+            int y = Integer.parseInt(stk.nextToken());
+
+            map[y][x] = 1;
+            map[x][y] = 1;
         }
-        for(int j=0;j< sequenceValue.length;j++){
-            sequence.add(sequenceValue[j]);
+
+        int[] d = new int[n+1];
+
+
+        bfs(one, two,d,map);
+
+        if(d[two] == 0){
+            System.out.println("-1");
         }
+        else System.out.println(d[two]);
+    }
 
+    private static void bfs(int one, int two,int[] d,int[][] map) {
 
-        if(sequence.size() > array.size()) System.out.println("false 1");
+        Queue<Integer> que = new LinkedList<>();
+        que.add(one);
 
+        while(!que.isEmpty()){
+            int v = que.poll();
 
-        int[] arr = new int[sequenceValue.length];
+            if(v == two) break;
 
-        boolean[] check = new boolean[sequenceValue.length];
-
-        for(int i=0;i< sequenceValue.length;i++){
-            int value = sequenceValue[i];
-            boolean flag = false;
-            for(int j=0;j< arrayValue.length;j++){
-                if(value == arrayValue[j] && !check[i]){
-                    arr[i] = j;
-                    check[i] = true;
-                    flag = true;
-                    break;
+            for(int i=1;i<=map.length-1;i++){
+                if(map[v][i] == 1 && d[i] == 0){
+                    d[i] = d[v] + 1;
+                    que.add(i);
                 }
             }
-
-//            if(!flag) return false;
         }
 
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i] + " ");
-        }
-        for(int i=0;i< arr.length-1;i++){
-            if(arr[i] > arr[i+1]){
-//                return false;
-                System.out.println("false3");
-            }
-        }
-
-//        return true;
-
-        System.out.println("true4");
     }
 }

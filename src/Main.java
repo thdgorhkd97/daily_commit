e5a32cd78bd1a381fd11e478d8629881cae02516
송해광 <thdgorhkd97@naver.com>
2022-05-20 23:48:32 +0900

    0520

--- src/Main.java
+++ src/Main.java
@@ -8,61 +8,58 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 16917 양념 반 후라이드 반
-    // 반반치킨을 2배 한 가격과 양념 + 후라이드 가격만을 비교한 게 패착이었다
-    // 양념이나 후라이드를 사더라도 반반이 싸다면 최소비용이 더 적어진다는 것을 고려해야 했다 ㅠㅠ
+    // java baekjoon 16968 차량 번호판 1
+    // 처음에는 길이가 4라는 부분을 보고 2^4 = 16 가지 경우라서 브루트 포스를 생각했다가
+    // 길이가 4 "이하"라는 걸 체크하고서는 완전탐색은 아니라고 생각했다.
+    // 같은 문자나 숫자가 연속으로 오면 안 되는 거니까 문자-문자면 26이 아닌 25를 곱하고
+    // 숫자-숫자면 10이 아닌 9를 곱하면서 string을 끝까지 간다.
+    // 이때 i번째가 숫자인지 문자인지는 flag를 true나 false로 바꿔가면서 한다.
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        String str = br.readLine();
 
-        int A = Integer.parseInt(stk.nextToken()); // 양념치킨 가격
-        int B = Integer.parseInt(stk.nextToken()); // 후라이드치킨 가격
-        int C = Integer.parseInt(stk.nextToken()); // 반반 가격
-        int X = Integer.parseInt(stk.nextToken()); // 양념 치킨 마리수
-        int Y = Integer.parseInt(stk.nextToken()); // 후라이드 치킨 마리수
+        // cccc dddd
+        // cccd dddc
+        // ccdc ddcd
+        // ccdd ddcc
+        // cdcc dcdd
+        // cdcd dcdc
+        // cddc dccd
+        // cddd dccc
 
-        /*
-        int min = Integer.MAX_VALUE;
-
-        int halfChicken = Math.min(X,Y) * 2;
-
-        for(int i = 2; i<=halfChicken; i+=2){
-
-            int chicken = i/2;
-            min = Math.min(min, i*C + A *(X-chicken) + B*(Y-chicken));
-
-            System.out.println(i*C + A *(X-chicken) + B*(Y-chicken));
+        int answer = 1;
+        boolean flag = true;
+        if(str.charAt(0)=='c'){
+            flag = true;
+            answer = 26;
+        }
+        else{ // 처음이 d
+            flag = false;
+            answer = 10;
         }
 
-        min = Math.min(min, X*A + B*Y);
-        System.out.println(X*A + B*Y);
-
-        System.out.println(min);
-         */
-
-        long sum = 0;
-        if(A + B > 2 * C){ // 양념 + 후라이드 > 반반 2마리 (반반을 사는 게 이득)
-            int halfChicken = Math.min(X,Y);
-            if(halfChicken < X){ // 반반에다가 양념치킨을 더해야
-                sum += Y * C*2;
-                sum += Math.min((X-Y) * A, (X-Y) * 2 * C);
+        for(int i=1;i<str.length();i++){
+            if(flag && str.charAt(i) == 'c'){ // c뒤에 c가 또 오는 경우
+                answer *= 25;
+                flag = true;
             }
-            else{
-                sum += X * C * 2;
-                sum += Math.min( (Y-X) * B, (Y-X) * 2 * C);
+            else if(flag && str.charAt(i) == 'd'){ // c뒤에 d가 오는 경우
+                answer *= 10;
+                flag = false;
             }
-
+            else if(!flag && str.charAt(i) == 'c'){ // d뒤에 c가 오는 겨웅
+                answer *= 26;
+                flag = true;
+            }
+            else {
+                flag = false;
+                answer *= 9;
+            }
         }
-        else{
-            sum = X*A + B*Y;
-        }
 
-        System.out.println(sum);
-
-
-
+        System.out.println(answer);
     }
 }

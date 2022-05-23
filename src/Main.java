a949839b40bb0f2c4ada5236708d30cebc656b4c
송해광 <thdgorhkd97@naver.com>
2022-05-23 23:52:56 +0900

    0523

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,13 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 14467 소가 길을 건너간 이유 1
-    // 소의 위치 변화가 발생한 횟수를 구하는 문제.
-    // 2가지 실수를 했다
-    // 1. 소의 위치가 주어지는 경우가 소의 위치가 변할 때 일 줄 알아서 set 자료구조에
-    //    있는지 여부만 확인해서 있을 때마다 answer+1 했는데 소의 위치가 그대로인 경우도 있기에
-    //    해당 방법은 옳은 방법이 아니었다.
-    // 2. 주어지는 N의 크기로 int형 배열을 선언했는데 N의 크기는 위치변화의 개수이고 소는 10마리라고
-    //    나와있기 때문에 11로 선언하여 문제를 해결했다
-    // 추가로 cow배열을 cow[몇번째 소인지][소의 위치] 이렇게 해서 2차원 배열로 선언하려고 했는데
-    // 생각해보니 2차원배열을 1차원배열로 하는방법을 통해서 cow[소의위치] 이렇게만 선언해서
-    // cow의 i번째를 몇번째 소인지 판단하는 식으로 2차원배열이 아닌 1차원배열로 메모리를 고려했다
+    // java baekjoon 소가 길을 건너는 이유 3
+    // 소가 도착한 시간과 길을 건너는 시간이 주어지면 모두가 건너는 시간을 구한다.
+    // 내가 주목한 부분은 소가 출발하는 시간이다.
+    // 소가 도착한 시간이 이전 소가 건넌 시간보다 더 크다면 answer를 아예 다음 소의 도착시간으로
+    // 바꾸고 그 소가 건너는 시간을 answer에 더한다.
+
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -26,33 +22,48 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] cow = new int[11];
-        Arrays.fill(cow,-1);
-
-        int answer = 0;
+        int[][] cow = new int[N+1][N+1];
 
         for(int i=0;i<N;i++){
             StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-            int cowNumber = Integer.parseInt(stk.nextToken());
-            int cowLocation = Integer.parseInt(stk.nextToken());
-
-            /* 소에 대한 위치의 변화가 있을때만 주어지는 줄 알았는데 위치의 변화가 없는
-               경우도 주어진다는 걸 알고 이 방법은 취소
-            if(set.add(cow)){
-                answer++;
-            }
-             */
-
-            if(cow[cowNumber] != -1 && cow[cowNumber] != cowLocation){
-                answer++;
-            }
-
-            cow[cowNumber] = cowLocation;
-
+            int arriveTime = Integer.parseInt(stk.nextToken());
+            int checkTime = Integer.parseInt(stk.nextToken());
+            cow[i][0] = arriveTime;
+            cow[i][1] = checkTime;
         }
 
+        Arrays.sort(cow, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+//        for(int i=0;i<cow.length;i++){
+//            System.out.println(cow[i][0] + " " + cow[i][1]);
+//        }
+
+        timeCalculate(cow);
+
         System.out.println(answer);
+
+
+    }
+    private static void timeCalculate(int[][] cow) {
+
+        int idx = 1;
+
+        answer = cow[0][0] + cow[0][1];
+
+        while(idx < cow.length){
+            if(answer < cow[idx][0]){
+                answer = cow[idx][0];
+            }
+            answer += cow[idx][1];
+
+            idx++;
+        }
 
 
 

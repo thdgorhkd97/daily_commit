837319fb1ec1a12cdeb411341c6009ca9aabe1eb
송해광 <thdgorhkd97@naver.com>
2022-05-29 23:43:21 +0900

    0529

--- src/Main.java
+++ src/Main.java
@@ -1,125 +1,116 @@
 package src; // daily 폴더를 source root로 인식시켰기 때문에
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    // java baekjoon 3085 사탕 게임
-    // 인접한 두칸을 바꿨을 때 행이나 열에 가장 많은 같은 색의 사탕 수를 반환
-    // 구현하는 과정에서 실수를 많이 했는지 시간을 꽤 들였는데 성공하지 못했다...
-    // 내가 생각한 건 가로로 먼저 바꿔가면서 바꾼 map[i][j]를 기준으로 행과 열을
-    // 비교하는 식으로 생각했는데 아예 끝까지 로직을 짜고 세로를 짜려다 보니
-    // i와 j를 헷갈려서 중간에 무너진 것 같다 ㅠㅠ
-    // 내일은 가로로 바꾸는 것과 세로로 바꾸는 걸 먼저 확인하고 바꾼 i,j에 대해서
-    // 개수를 세보는 식으로 해야겠다.
-    // 그리고 코드도 생각보다 중복된 부분이 많아서 좀 더 함수화 해야 할 것 같다.
+    // java programmers 메뉴 리뉴얼
+    // 애초에 문제에 대한 이해가 상당히 오래걸렸다...
+    // course[]의 길이에 대해서 조합 메뉴중 가장 많은 것만 넣어야 했다
+    // 근데 hashmap을 활용해서 어떤 조합 메뉴가 몇 번 반복되었는지 까지는 확인이 가능한데
+    // 이를 기준으로 특정 길이에 대한 메뉴를 고르는 걸 어떻게 해야 할 지 잘 모르겠다...
 
-    static int answer = 0;
-    static int red = 0;
-    static int blue = 0;
-    static int green = 0;
-    static int yellow = 0;
+    static HashMap<String,Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
 
-        int N = Integer.parseInt(br.readLine());
+        int[] course = {2,3,4};
 
-        char[][] map = new char[N][N];
+        Arrays.sort(orders, (o1, o2) -> o2.length()-o1.length());
 
-        for(int i=0;i<N;i++){
-            map[i] = br.readLine().toCharArray();
-        }
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N-1;j++){
-                if(map[i][j] != map[i][j+1]) {
-
-                    char tmp = map[i][j];
-                    map[i][j] = map[i][j+1];
-                    map[i][j+1] = tmp;
-
-                    answer = Math.max(answer,checkGaro(map, i, j));
-
-
-                    tmp = map[i][j];
-                    map[i][j] = map[i][j+1];
-                    map[i][j+1] = tmp;
+        for(int i=0;i< course.length;i++){
+            for(int j=0;j<orders.length;j++){
+                if(course[i] > orders[j].length()){
+                    break;
+                }
+                else{
+                    int depth = 0;
+                    boolean[] visited = new boolean[orders[j].length()];
+                    combination(orders[j].toCharArray(),visited,depth,orders[j].length(),course[i]);
+//                    System.out.println(orders[j] + " 에서 " + course[i] + " 만큼 ");
                 }
             }
         }
 
-        for(int i=0;i<N-1;i++){
-            for(int j=0;j<N;j++){
-                if(map[i][j] != map[i+1][j]) {
+        List<Map.Entry<String, Integer>> list_entries = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
 
-                    char tmp = map[i][j];
-                    map[i][j] = map[i+1][j];
-                    map[i+1][j] = tmp;
+        // 비교함수 Comparator를 사용하여 내림 차순으로 정렬
+        Collections.sort(list_entries, new Comparator<Map.Entry<String, Integer>>() {
+            // compare로 값을 비교
+            public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2)
+            {
+                // 내림 차순으로 정렬
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
 
-                    answer = Math.max(answer,checkSero(map, i, j));
+        int max = 0;
+        for(Map.Entry<String, Integer> entry : list_entries) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+            max = Math.max(max,entry.getValue());
+        }
+        ArrayList<String> list = new ArrayList<>();
 
 
-                    tmp = map[i][j];
-                    map[i][j] = map[i+1][j];
-                    map[i+1][j] = tmp;
 
+
+
+
+
+    }
+
+    private static void combination(char[] toCharArray, boolean[] visited, int start, int length, int r) {
+        if(r == 0){
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < length; i++) {
+                if (visited[i]) {
+                    sb.append(toCharArray[i]);
                 }
             }
+            map.put(sb.toString(),map.getOrDefault(sb.toString(),0)+1);
+            return ;
         }
 
-        System.out.println(answer);
+        for (int i = start; i < length; i++) {
+            visited[i] = true;
+            combination(toCharArray, visited, i + 1, length, r - 1);
+            visited[i] = false;
+        }
     }
 
-    private static int checkSero(char[][] map, int row, int column) {
-        red = 0; blue=0; green=0; yellow = 0;
-
-        for(int j=0;j<map.length;j++){
-            switch (map[row][j]){
-                case 'C' : red++; break;
-                case 'P' : blue++; break;
-                case 'Z' : green++; break;
-                case 'Y' : yellow++; break;
-            }
-        }
-
-        return Math.max(Math.max(red,blue),Math.max(green,yellow));
-    }
-
-    private static int checkGaro(char[][] map, int row, int column){
-
-        red = 0; blue=0; green=0; yellow = 0;
-
-        for(int j=0;j<map.length;j++){
-            switch (map[row][j]){
-                case 'C' : red++; break;
-                case 'P' : blue++; break;
-                case 'Z' : green++; break;
-                case 'Y' : yellow++; break;
-            }
-        }
-
-        int max = Math.max(Math.max(red,blue),Math.max(green,yellow));
-
-        red = 0; blue=0; green=0; yellow = 0;
-
-        for(int i=0;i<map.length;i++){
-            switch (map[i][column]){
-                case 'C' : red++; break;
-                case 'P' : blue++; break;
-                case 'Z' : green++; break;
-                case 'Y' : yellow++; break;
-            }
-        }
-
-        max = Math.max(max,Math.max(Math.max(red,blue),Math.max(green,yellow)));
-        return max;
-
-    }
 }
+
+        /*
+        String[] strings = {"abce", "abcd", "cdx"};
+        int n = 2;
+
+        Arrays.sort(strings);
+        String[] answer = new String[strings.length];
+
+        ArrayList<Character> list = new ArrayList<>();
+        for(int i=0;i<strings.length;i++){
+            list.add(strings[i].charAt(n));
+        }
+
+        Collections.sort(list);
+
+        int idx = 0;
+
+        boolean[] visited = new boolean[strings.length];
+        for(int i=0;i<list.size();i++){
+            for(int j=0;j< strings.length; j++){
+                if(strings[j].charAt(n) == list.get(i) && !visited[j]){
+                    answer[idx++] = strings[j];
+                    visited[j] = true;
+                    break;
+                }
+            }
+        }
+
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
+         */

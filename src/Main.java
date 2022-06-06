0a130f4f6cad25080d1951df1b0686dee81b5570
송해광 <thdgorhkd97@naver.com>
2022-06-06 23:44:49 +0900

    0606

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,68 @@
 package src; // daily 폴더를 source root로 인식시켰기 때문에
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    // java programmers level 2 - 다리를 지나는 트럭
-    // 다리를 큐로 생각하는 개념도 생각했고 무게의 합으로 트럭이 앞으로 나아간다는 걸
-    // 생각했는데 트럭끼리의 무게의 합이 다리의 무게보다 높으면 현재 다리위에
-    // 있는 트럭만 앞으로 보낸다는 걸 생각하지 못했는데
-    // 해당 부분은 큐에 0을 추가하는 식으로 처리한다는 게 진짜 인상깊었다.
-    // 코드를 보고 정말 감탄을 금치 못한 것 같다
+    // java baekjoon 11659 구간 합 구하기 4
+    /*
+    작성자 : 송해광
+    2022-06-06 소요 시간 : 40분
+    문제 설명 : 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+    문제 접근 방법 : 그때그때 인덱스에 해당하는 for문을 돌리면 시간초과가 발생하기 때문에
+                   다른 방법을 활용해야 했고 누적합을 활용하기로 하였다.
+                   배열의 합을 바로바로 구할 수 있는 방법이기 때문이다.
+     */
+
 
     public static void main(String[] args) throws IOException {
 
-        int bridge_length = 2;
-        int weight = 10;
-        int[] truck_weights = {7,4,5,6};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int answer = 0;
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        Queue<Integer> que = new LinkedList<>(); // 다리
-        int sum = 0; // 무게
+        int N = Integer.parseInt(stk.nextToken()); // 수의 개수 ( arr 의 크기 )
+        int M = Integer.parseInt(stk.nextToken()); // 합을 구해야 하는 횟수
 
-        // queue 7 0 4 5 0 6 -> 0 6
-        // sum   7 0 4 9 5 0 6 -> 6
-        // time  1 2 3 4 5 6 -> 6
-
-        for(int truck : truck_weights){
-
-            while(true){
-                if(que.isEmpty()){ // 큐가 비어있다 = 다리가 비어있다
-                    que.add(truck);
-                    sum += truck;
-                    answer++;
-                    break;
-                }
-                else if(que.size() == bridge_length){
-                    sum -= que.poll();
-                }
-                else{
-                    if(sum + truck <= weight){
-                        que.add(truck);
-                        sum += truck;
-                        answer++;
-                        break;
-                    }
-                    else{
-                        que.add(0);
-                        answer++;
-                    }
-                }
-            }
-
+        int[] arr = new int[N]; // 주어지는 수를 담을 배열
+        stk = new StringTokenizer(br.readLine()," ");
+        for(int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(stk.nextToken());
         }
 
-        // 트럭을 올리는 게 기준이기 때문에 건너야함
-//        return answer + bridge_length;
+        accumlateSum(arr); // arr배열의 누적합 계산
+
+        for(int i=0;i<M;i++){
+            stk = new StringTokenizer(br.readLine()," ");
+            int start = Integer.parseInt(stk.nextToken()); // 합을 구해야하는 구간 시작
+            int end = Integer.parseInt(stk.nextToken()); // 합을 구해야하는 구간 끝
+            checkSum(arr,start,end); // arr배열의 start에서 end 까지의 합을 계산하는 메서드
+        }
 
     }
-}
-/*
 
-        int num = 626331;
-        int cnt = 0;
-
-        for(int i=0;i<500;i++){
-            if(num % 2 == 0){
-                num = num/2;
-            }
-            else{
-                num = (num * 3) + 1;
-            }
-
-            cnt ++;
-            if(num == 1){
-                break;
-            }
+    private static void checkSum(int[] arr, int start, int end) {
+        if(start == 1){ // start가 1이라는 건 end번 까지의 누적합을 구하라는 것
+            System.out.println(arr[end-1]); // 배열의 인덱스는 0부터 시작하기 때문에 end-1 번째를 구한다
         }
+        else if( start == arr.length){ // start 가 arr의 크기와 같이 주어지면 end는 반드시 start와 같은 맨 끝이기 때문에
+                                       // 맨 마지막 원소를 가져오면된다.
+            System.out.println(arr[arr.length-1] - arr[arr.length-2]);
+            // arr은 누적합을 저장한 배열로 바뀌었기 때문에 가장 마지막 원소에서 바로 앞 원소를 뺀다
+        }
+        else{
+            System.out.println(arr[end-1] - arr[start-2]); // 배열의 중간에서 중간까지를 구하는 것
+        }
+    }
 
-        System.out.println(cnt);
- */
+    private static void accumlateSum(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            arr[i] += arr[i-1];
+        }
+    } // 누적합 계산 함수
+
+
+}

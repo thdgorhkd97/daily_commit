c633c3c0ee3a6b7640e5253350de27acc1dd92d2
송해광 <thdgorhkd97@naver.com>
2022-06-07 23:49:01 +0900

    0607

--- src/Main.java
+++ src/Main.java
@@ -7,62 +7,47 @@ import java.util.*;
 
 public class Main {
 
-    // java baekjoon 11659 구간 합 구하기 4
-    /*
-    작성자 : 송해광
-    2022-06-06 소요 시간 : 40분
-    문제 설명 : 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
-    문제 접근 방법 : 그때그때 인덱스에 해당하는 for문을 돌리면 시간초과가 발생하기 때문에
-                   다른 방법을 활용해야 했고 누적합을 활용하기로 하였다.
-                   배열의 합을 바로바로 구할 수 있는 방법이기 때문이다.
-     */
-
+    // JAVA baekjoon 2309 일곱 난쟁이
+    // 작성자 : 송해광 ( 2022 - 06 - 07 )
+    // 문제 설명 : 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
+    //           아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
+    // 문제 접근 : 처음에는 7명의 키를 더해서 100이 되는 걸 찾아야 하나 싶었는데 그렇게 하려면
+    //           더 힘들 것 같아서 2명을 빼서 합이 100이 되는 걸 찾으려 했다.
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-
-        int N = Integer.parseInt(stk.nextToken()); // 수의 개수 ( arr 의 크기 )
-        int M = Integer.parseInt(stk.nextToken()); // 합을 구해야 하는 횟수
-
-        int[] arr = new int[N]; // 주어지는 수를 담을 배열
-        stk = new StringTokenizer(br.readLine()," ");
-        for(int i=0;i<N;i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
+        int yodle = 9; // 9명의 난쟁이 수
+        int[] height = new int[yodle]; // 9명의 난쟁이의 키를 저장하는 배열
+        int sum = 0 ; // 9명의 난쟁이의 키의 합
+        for(int i=0;i<yodle;i++){
+            height[i] = Integer.parseInt(br.readLine());
+            sum += height[i]; // 키를 더해나간다.
         }
 
-        accumlateSum(arr); // arr배열의 누적합 계산
+        Arrays.sort(height); // 난쟁이들의 키를 오름차순으로 정렬
 
-        for(int i=0;i<M;i++){
-            stk = new StringTokenizer(br.readLine()," ");
-            int start = Integer.parseInt(stk.nextToken()); // 합을 구해야하는 구간 시작
-            int end = Integer.parseInt(stk.nextToken()); // 합을 구해야하는 구간 끝
-            checkSum(arr,start,end); // arr배열의 start에서 end 까지의 합을 계산하는 메서드
+        int over = sum - 100; // 7명의 난쟁이의 키의 합이 100이기 때문에 100을 초과하는 초과량을 구한다
+        int idx1 = 0; // 빠져야 하는 난쟁이의 인덱스 1
+        int idx2 = 0; // 빠져야 하는 난쟁이의 인덱스 2
+        for(int i=0;i<yodle-1;i++){ // for 문
+            for(int j=i+1;j<yodle;j++){ // for 문 ( i+1 부터 돌면서 )
+                if(height[i] + height[j] == over){ // 2명의 난쟁이 키의 합이 초과량과 같으면
+                    idx1 = i; // 난쟁이 한명의 인덱스 저장
+                    idx2 = j; // 나머지 난쟁이의 인덱스 저장
+                    break; // 찾은 순간 break로 시간 절약
+                }
+            }
         }
+
+        for(int i=0;i<yodle;i++){
+            if(i != idx1 && i != idx2){ // 빠져야 하는 난쟁이의 인덱스가 둘 다 아니면 출력
+                System.out.println(height[i]);
+            }
+        }
+
+
 
     }
-
-    private static void checkSum(int[] arr, int start, int end) {
-        if(start == 1){ // start가 1이라는 건 end번 까지의 누적합을 구하라는 것
-            System.out.println(arr[end-1]); // 배열의 인덱스는 0부터 시작하기 때문에 end-1 번째를 구한다
-        }
-        else if( start == arr.length){ // start 가 arr의 크기와 같이 주어지면 end는 반드시 start와 같은 맨 끝이기 때문에
-                                       // 맨 마지막 원소를 가져오면된다.
-            System.out.println(arr[arr.length-1] - arr[arr.length-2]);
-            // arr은 누적합을 저장한 배열로 바뀌었기 때문에 가장 마지막 원소에서 바로 앞 원소를 뺀다
-        }
-        else{
-            System.out.println(arr[end-1] - arr[start-2]); // 배열의 중간에서 중간까지를 구하는 것
-        }
-    }
-
-    private static void accumlateSum(int[] arr) {
-        for(int i=1;i<arr.length;i++){
-            arr[i] += arr[i-1];
-        }
-    } // 누적합 계산 함수
-
-
 }

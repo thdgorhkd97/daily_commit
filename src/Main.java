fb7d2f738cf14d9297fea2a1b3f96e11442f27df
송해광 <thdgorhkd97@naver.com>
2022-06-08 23:31:58 +0900

    0608

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,72 @@
-package src; // daily 폴더를 source root로 인식시켰기 때문에
+package src;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+문제 : baekjoon 1520 내리막 길
+작성자 : 송해광( 2022 - 06 - 08)
+문제접근 : 처음에는 단순히 dfs만 활용해서 루트를 찾아가면서 내려갔는데 시간초과가 발생했고 알아보니 dp를 통해서 저장해 가면서 해야 했다
+걸린 시간 : 80분
+ */
+
 public class Main {
 
-    // JAVA baekjoon 2309 일곱 난쟁이
-    // 작성자 : 송해광 ( 2022 - 06 - 07 )
-    // 문제 설명 : 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
-    //           아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
-    // 문제 접근 : 처음에는 7명의 키를 더해서 100이 되는 걸 찾아야 하나 싶었는데 그렇게 하려면
-    //           더 힘들 것 같아서 2명을 빼서 합이 100이 되는 걸 찾으려 했다.
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dy = {0, 0, -1, 1}; // 상하좌우
+    static int[][] dp; // 이동 가능한 경로를 저장하기 위한 2차원 배열
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int yodle = 9; // 9명의 난쟁이 수
-        int[] height = new int[yodle]; // 9명의 난쟁이의 키를 저장하는 배열
-        int sum = 0 ; // 9명의 난쟁이의 키의 합
-        for(int i=0;i<yodle;i++){
-            height[i] = Integer.parseInt(br.readLine());
-            sum += height[i]; // 키를 더해나간다.
+        StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(stk.nextToken()); // 주어지는 배열의 행 수
+        int M = Integer.parseInt(stk.nextToken()); // 주어지는 배열의 열 수
+
+        int[][] map = new int[N+1][M+1]; // 1부터 인덱스를 활용하기 위해 +1 로 크기 설정
+        dp = new int[N+1][M+1]; // 1부터 인덱스를 활용하기 위해 +1 로 크기 설정
+
+        for (int i = 1; i <= N; i++) {
+            stk = new StringTokenizer(br.readLine(), " ");
+            for (int j = 1; j <= M; j++) {
+                map[i][j] = Integer.parseInt(stk.nextToken());
+                dp[i][j] = -1; // dp를 -1로 초기화( 나중에 중복 체크 위함 )
+            }
         }
 
-        Arrays.sort(height); // 난쟁이들의 키를 오름차순으로 정렬
+        dfs(map, 1, 1); // 시작점인 1,1 부터 시작
 
-        int over = sum - 100; // 7명의 난쟁이의 키의 합이 100이기 때문에 100을 초과하는 초과량을 구한다
-        int idx1 = 0; // 빠져야 하는 난쟁이의 인덱스 1
-        int idx2 = 0; // 빠져야 하는 난쟁이의 인덱스 2
-        for(int i=0;i<yodle-1;i++){ // for 문
-            for(int j=i+1;j<yodle;j++){ // for 문 ( i+1 부터 돌면서 )
-                if(height[i] + height[j] == over){ // 2명의 난쟁이 키의 합이 초과량과 같으면
-                    idx1 = i; // 난쟁이 한명의 인덱스 저장
-                    idx2 = j; // 나머지 난쟁이의 인덱스 저장
-                    break; // 찾은 순간 break로 시간 절약
+        System.out.println(dp[1][1]);
+    }
+
+    private static int dfs(int[][] map, int row, int column) {
+        if (row == map.length-1  && column == map[0].length-1 ) { // 마지막에 도달했으면 종료
+            return 1;
+        }
+
+        if(dp[row][column] != -1){ // dp[][]가 != -1이면 이미 방문했다는 것이므로 그냥 그 인덱스에 저장된 수 반환
+            return dp[row][column];
+        }
+        else {
+            dp[row][column] = 0; // -1이면 처음 접근하는 인덱스이므로 0으로 초기화(나중에 다시 방문 X)
+            for (int i = 0; i < dx.length; i++) {
+                int nextX = row + dx[i]; // 현재 인덱스에서 상하좌우 확인
+                int nextY = column + dy[i]; // 현재 인덱스에서 상하좌우 확인
+
+                if (nextX < 1 || nextY < 1 || nextX > map.length-1 || nextY > map[0].length-1) {
+                    continue;
+                } // 만약 현재 인덱스의 상하좌우로 이동한 지점이 배열의 크기를 벗어나면 continue
+
+                if (map[row][column] > map[nextX][nextY]) { // 이동한 점이 원래 지점보다 작다면 내리막길이니까
+                    dp[row][column] += dfs(map, nextX, nextY); // 현재 지점은 dfs로 재귀로 보내 현재 지점을 더한다
                 }
             }
         }
-
-        for(int i=0;i<yodle;i++){
-            if(i != idx1 && i != idx2){ // 빠져야 하는 난쟁이의 인덱스가 둘 다 아니면 출력
-                System.out.println(height[i]);
-            }
-        }
-
-
-
+        return dp[row][column];
     }
+
+
 }

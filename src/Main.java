7aebd702352733512040d29c827965660a0948b1
송해광 <thdgorhkd97@naver.com>
2022-06-11 23:52:37 +0900

    0611

--- src/Main.java
+++ src/Main.java
@@ -6,83 +6,51 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-문제 : JAVA baekjoon 10971 외판원 순회 2
-
-작성자 : 송해광 ( 2022 - 06 - 10 )
-
-문제정보 : 1번부터 N번까지 번호가 매겨져 있는 도시들이 있고, 도시들 사이에는 길이 있다. (길이 없을 수도 있다)
-이제 한 외판원이 어느 한 도시에서 출발해 N개의 도시를 모두 거쳐 다시 원래의 도시로 돌아오는 순회 여행 경로를 계획하려고 한다.
-단, 한 번 갔던 도시로는 다시 갈 수 없다. (맨 마지막에 여행을 출발했던 도시로 돌아오는 것은 예외) 이런 여행 경로는 여러 가지가 있을 수 있는데, 가장 적은 비용을 들이는 여행 계획을 세우고자 한다.
-걸린시간 : 40분
-
-문제접근 : 전체 마을 수(N)이 10이하기 때문에 순열로 모든 마을 이동 경로를 구한후에 cost[][] 배열을 통해서
-각각의 마을로 이동하는 경비를 더해가며 계산해도 괜찮다고 판단하였다.
-
+문제 : java programmers level 2 기능개발
+작성자 : 송해광 ( 2022 - 06 - 11 )
+문제 접근 : 각 기능별로 언제 완성되는지를 구하는 방식에서 나는 현재 진척도에서 하루하루의 진행률을 더해나가면서
+100을 넘는 순간이 언젠지를 알아냈는데 나눈 몫을 올림하는 것으로 처리하는 게 훨씬 빠르게 처리 가능하다.
  */
 
 public class Main {
 
-    static int tripCost = Integer.MAX_VALUE; // 각 여행경로 별 여행경비를 저장하는 변수
-    // Math.min()을 통해서 현재값과 비교해가면서 최소값을 갱신해 나간다.
-
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1,1,1,1,1,1};
 
-        int N = Integer.parseInt(br.readLine());
+        int[] finishDay = new int[progresses.length]; // 각 기능별로 완료되는 일자를 저장하는 배열
 
-        int[][] cost = new int[N+1][N+1]; // 1번 도시부터 시작하기위해 크기 지정 ( +1 )
+        for(int i=0;i< progresses.length;i++){
+            int day = 0; // 완료되는 일자
+            while(progresses[i] < 100){ // 100% 완료가 아닐때까지는
+                progresses[i] += speeds[i]; // 계속 작업을 진행해나간다
+                day ++; // 작업 일자를 더해나간다.
+            }
+            finishDay[i] = day; // 100% 완료될때까지 진행한 작업 일자
+        }
 
-        for(int i=1;i<=N;i++){
-            StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        List<Integer> result = new ArrayList<>(); // 정답을 넣을 리스트
 
-            for(int j=1;j<=N;j++){
-                cost[i][j] = Integer.parseInt(stk.nextToken());
+        int distributeWith = 1; // 함께 배포하는 기능의 수에 대한 변수
+        int standardDistributeDay = finishDay[0]; // 비교하는 기준 배포 날짜
+        for(int i=0;i< finishDay.length - 1; i++){
+            if(standardDistributeDay >= finishDay[i+1]){ // 기준 배포 날짜보다 먼저 만들어졌으면
+                distributeWith++; // 기준 배포날짜가 배포될 때 한번에 배포되기 때문에 함께 배포하는 기능의 수를 더한다.
+            }
+            else { // 기준 배포날짜보다 나중에 완성된다면
+                result.add(distributeWith); // 기준 배포날짜에 배포되는 기능의 수를 result에 넣어두고
+                distributeWith = 1; // 배포하는 기능의 수를 초기화 하고
+                standardDistributeDay = finishDay[i+1]; // 나중에 완성되는 기능이 완성되는 날짜로 기준 날짜를 바꾼다
             }
         }
 
-        int[] arr = new int[N]; // 순열로 모든 여행경로를 만들기 위해 배열 저장(N이 10이하이기 때문에 가능)
-        for(int i=0;i<N;i++){
-            arr[i] = i+1; // 1번 도시부터 시작하기 위해서 +1로 저장
+        result.add(distributeWith); // 마지막으로 저장되지 않은 배포 기능들을 넣는다.(for문의 마지막에 add를 안 할수도 있으니까)
+
+        int[] answer = new int[result.size()]; // result의 값을 answer로 옮긴다. 정답을 int[] 형태로 리턴해야 한다.
+        for(int i=0;i<answer.length;i++){
+            answer[i] = result.get(i);
         }
 
-        int[] result = new int[N]; // 순열을 저장하기 위한 배열
-        int depth = 0; // 순열의 depth 값
-        boolean[] visited = new boolean[N];
-        int r = N; // 구하고자하는 순열의 길이 값 ( N에 대한 순열 값이 필요 )
-        combination(arr,result,depth,visited,r,cost); // 순열
-
-        System.out.println(tripCost);
     }
-
-    private static void combination(int[] arr, int[] result, int depth, boolean[] visited, int r,int[][] cost) {
-        if(depth == r){ // depth가 r이면 여행경로에 대한 계산이 완료된 경우
-            costCalculate(result,cost); // 해당 여행경로에 대해 여행경비를 계산
-
-            return ;
-        }
-
-        for(int i=0;i<arr.length;i++){ // 순열 계산식
-            if(!visited[i]){
-                result[depth] = arr[i];
-                visited[i] = true;
-                combination(arr,result,depth+1,visited,r,cost); // depth+1로 재귀
-                visited[i] = false;
-            }
-        }
-    }
-
-    private static void costCalculate(int[] result, int[][] cost) {
-        int costSum = 0; // 각 여행경로에 대한 여행경비 저장 변수
-        for(int i=0;i<result.length-1;i++){ // 다음경로가 필요하므로 result.length-1까지만 일단 돌고
-            costSum += cost[result[i]][result[i+1]]; // 다음 마을에 대한 이동 경비를 더한다
-            if(cost[result[i]][result[i+1]] == 0) return ; // 단 cost[i][j] ==0 이면 i마을에서 j마을로 가는 경로가 존재하지 않는 것
-        }
-        costSum += cost[result[result.length-1]][result[0]]; // 마지막마을에서 다시 처음 마을로 돌아와야 하기 때문에
-        if(cost[result[result.length-1]][result[0]] == 0) return ; // 단 cost[i][j] ==0 이면 i마을에서 j마을로 가는 경로가 존재하지 않는 것
-
-        tripCost = Math.min(tripCost,costSum); // tripCost 값과 비교해서 최소값을 저장
-
-    } // 여행경로에 대한 여행경비 계산
 }
-

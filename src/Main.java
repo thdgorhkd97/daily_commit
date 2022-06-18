6d144b9b29bb686860e2528ea38b56eea8d669ea
송해광 <thdgorhkd97@naver.com>
2022-06-18 23:30:13 +0900

    0618

--- src/Main.java
+++ src/Main.java
@@ -4,67 +4,64 @@ import javax.swing.text.View;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 /*
-작성자 : 송해광 ( 2022 - 06 - 17 )
-공부 : dfs 공부 -> 재귀 & 스택 활용
+작성자 : 송해광 ( 2022 - 06 - 18 )
+문제 : java baekjoon 2606
+문제 접근 : 컴퓨터끼리 연결된 간선이 주어지면 1번 컴퓨터와 연결된 컴퓨터 대수를 구하는 문제
+          이므로 bfs(1) 을 해서 1번과 연결된 컴퓨터를 구한다.
  */
 
 public class Main {
 
-    static boolean[] visited = new boolean[6]; // 방문처리를 표현할 배열
-    static int[][] graph = new int[6][6]; // 그래프의 연결을 표현할 2차원 배열
-    static Stack<Integer> stack = new Stack<>(); // dfs에 쓰일 스택 자료구조
+    static int[][] graph;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
-        addLine(graph,0,1);
-        addLine(graph,0,2);
-        addLine(graph,0,3);
-        addLine(graph,0,4);
-        addLine(graph,2,5);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        /*
-        dfsByRecursion(0);
-        dfsByStack(0);
-         */
-    }
+        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+        int M = Integer.parseInt(br.readLine()); // 간선의 수
 
-    private static void dfsByRecursion(int start){
-        visited[start] = true; // 해당 정점을 방문처리하고
+        graph = new int[N+1][N+1]; // 컴퓨터의 번호는 1번부터 시작하므로
 
-        System.out.print(start + " ");
+        for(int i=0;i<M;i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        for(int i=start;i<6;i++){
-            if(!visited[i] && graph[start][i] == 1){ // 연결된 정점을 방문하지 않았으면
-                dfsByRecursion(i);
-            }
+            int computer1 = Integer.parseInt(stk.nextToken()); // 연결되는 한쪽 컴퓨터
+            int computer2 = Integer.parseInt(stk.nextToken()); // 연결된 나머지 컴퓨터
+
+            graph[computer1][computer2] = 1;
+            graph[computer2][computer1] = 1; // 인접행렬에 컴퓨터 연결을 저장
         }
-    } // dfs를 재귀로 구현
 
-    private static void dfsByStack(int start) {
+        boolean[] visited = new boolean[N+1];
+        bfs(1,visited);
 
-        stack.push(start);
+        System.out.println(answer-1); // 1번과 연결되어 있는 컴퓨터 대수가 필요한데 1번이 포함되니까 -1
+
+    }
+    private static void bfs(int start,boolean[] visited){
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
         visited[start] = true;
 
-        while(!stack.isEmpty()){
-            int v = stack.pop();
+        while(!que.isEmpty()){
+            int v = que.poll();
 
-            System.out.print(v + " ");
-
-            for(int i=start;i<6;i++){
-                if(!visited[i] && graph[start][i] == 1){ // 연결된 정점을 방문하지 않았으면
-                    dfsByStack(i);
+            answer++; // 1번과 연결된 다른 컴퓨터이므로 +1 ( v가 나올때마다)
+            for(int i=1;i<= graph.length-1;i++){
+                if(graph[v][i] == 1 && !visited[i]){
+                    que.add(i);
+                    visited[i] = true;
                 }
             }
         }
 
-    } // dfs를 스택으로 구현
-
-    private static void addLine(int[][] graph, int a, int b) {
-        graph[a][b] = 1;
-        graph[b][a] = 1;
     }
 }

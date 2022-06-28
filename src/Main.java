4fa660fd2e02ff55b532f666bc9ec59284fec672
송해광 <thdgorhkd97@naver.com>
2022-06-28 23:54:20 +0900

    0628

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,62 @@
-package src;
+package src; // daily 폴더를 source root로 인식시켰기 때문에
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 06 - 27 )
-문제 : java baekjoon 2559
-문제접근 : 총 N개의 수가 주어지면 K의 연속된 숫자의 합 중 최대를 구하라
+작성자 : 송해광 ( 2022 - 06 - 28 )
+문제 : JAVA programmers level 2 - 다리를 지나는 트럭
+문제접근 : 다리를 큐로 보는 건 생각했는데 다리가 지탱할 수 있는 무게를 초과하면 어떻게 할 지를
+         전혀 몰랐는데 알아보니 0을 큐에 넣어서 원래 있던 다리를 앞으로 미뤄주면 해결되는 거였다
  */
 
-class algorithm {
+public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        int answer = 0;
 
-        int N = Integer.parseInt(stk.nextToken()); // 온도를 측정한 전체 날짜의 수
-        int K = Integer.parseInt(stk.nextToken()); // 합을 구하기 위한 연속적 날짜의 수
+        Queue<Integer> que = new LinkedList<>(); // 다리
+        int sum = 0; // 무게
 
-        stk = new StringTokenizer(br.readLine()," ");
-        int[] arr = new int[N];
-        for(int i=0;i<N;i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
-        }
+        // queue 7 0 4 5 0 6 -> 0 6
+        // sum   7 0 4 9 5 0 6 -> 6
+        // time  1 2 3 4 5 6 -> 6
 
-        int answer = Integer.MIN_VALUE;
+        for(int truck : truck_weights){
 
-        for(int i=0;i<=N-K;i++){
-            int sum = 0;
-            for(int j=i;j<i+K;j++){
-                sum += arr[j];
+            while(true){
+                if(que.isEmpty()){ // 큐가 비어있다 = 다리가 비어있다
+                    que.add(truck);
+                    sum += truck;
+                    answer++;
+                    break;
+                }
+                else if(que.size() == bridge_length){ // 만약 다리가 꽉 차있다면
+                    sum -= que.poll();
+                }
+                else{
+                    if(sum + truck <= weight){ // 다리에 더 올라갈 수 있으면
+                        que.add(truck);
+                        sum += truck;
+                        answer++;
+                        break;
+                    }
+                    else{ // 다리에 더 올릴 수없다-> 지금 있는 트럭만 앞으로 보내자
+                        que.add(0);
+                        answer++;
+                    }
+                }
             }
-            answer = Math.max(sum,answer);
+
         }
 
-        System.out.println(answer);
+        // 트럭을 올리는 게 기준이기 때문에 건너야함
+//        return answer + bridge_length;
 
     }
 }
-

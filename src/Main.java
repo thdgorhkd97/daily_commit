3c8cf0e3aa976b5907493b141a2814e066d5b6cd
송해광 <thdgorhkd97@naver.com>
2022-06-30 20:19:19 +0900

    0629

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,54 @@
-package src; // daily 폴더를 source root로 인식시켰기 때문에
+package src;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 06 - 28 )
-문제 : JAVA programmers level 2 - 다리를 지나는 트럭
-문제접근 : 다리를 큐로 보는 건 생각했는데 다리가 지탱할 수 있는 무게를 초과하면 어떻게 할 지를
-         전혀 몰랐는데 알아보니 0을 큐에 넣어서 원래 있던 다리를 앞으로 미뤄주면 해결되는 거였다
+작성자 : 송해광 ( 2022 - 06 - 30 )
+문제 : java programmers level 2 - 땅따먹기
+문제접근 : 바로 아래줄로 내려올 수 없기때문에 한 줄씩 내려올 때마다 다른 열과의 합 중 최대값을 저장해나간다
+        ex) 0열이면 한 줄 위의 1,2,3 열과의 합 중 최대를 저장한다.
  */
 
-public class Main {
+class algorithm {
 
     public static void main(String[] args) throws IOException {
 
-        int bridge_length = 2;
-        int weight = 10;
-        int[] truck_weights = {7,4,5,6};
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
 
-        int answer = 0;
+        int[][] dp = new int[land.length][4]; // 열은 반드시 4로 고정
 
-        Queue<Integer> que = new LinkedList<>(); // 다리
-        int sum = 0; // 무게
-
-        // queue 7 0 4 5 0 6 -> 0 6
-        // sum   7 0 4 9 5 0 6 -> 6
-        // time  1 2 3 4 5 6 -> 6
-
-        for(int truck : truck_weights){
-
-            while(true){
-                if(que.isEmpty()){ // 큐가 비어있다 = 다리가 비어있다
-                    que.add(truck);
-                    sum += truck;
-                    answer++;
-                    break;
-                }
-                else if(que.size() == bridge_length){ // 만약 다리가 꽉 차있다면
-                    sum -= que.poll();
-                }
-                else{
-                    if(sum + truck <= weight){ // 다리에 더 올라갈 수 있으면
-                        que.add(truck);
-                        sum += truck;
-                        answer++;
-                        break;
-                    }
-                    else{ // 다리에 더 올릴 수없다-> 지금 있는 트럭만 앞으로 보내자
-                        que.add(0);
-                        answer++;
-                    }
-                }
-            }
-
+        for(int i=0;i<4;i++){
+            dp[0][i] = land[0][i]; // 0행 -> 첫 줄은 똑같이
         }
 
-        // 트럭을 올리는 게 기준이기 때문에 건너야함
-//        return answer + bridge_length;
+        for(int i=1;i<land.length;i++){
+            for(int j=0;j<4;j++){
+                if(j==0){
+                    dp[i][0] = Math.max(land[i][j] + dp[i-1][1],Math.max(land[i][j] + dp[i-1][2],land[i][j] + dp[i-1][3]));
+                }else if(j==3){
+                    dp[i][j] = Math.max(land[i][j] + dp[i-1][0],Math.max(land[i][j] + dp[i-1][1],land[i][j] + dp[i-1][2]));
+                }
+                else if(j==1){
+                    dp[i][j] = Math.max(land[i][j] + dp[i-1][0],Math.max(land[i][j] + dp[i-1][2],land[i][j] + dp[i-1][3]));
+                }
+                else{
+                    dp[i][j] = Math.max(land[i][j] + dp[i-1][0],Math.max(land[i][j] + dp[i-1][1],land[i][j] + dp[i-1][3]));
+                }
+            }
+        } // dp로 해당 열을 제외한 위 열과의 합 중 최대를 구한다.
 
+        int answer = 0;
+        int num = dp[land.length-1][0];
+        for(int i=0;i<4;i++){
+            if(num <= dp[land.length-1][i]){
+                num = dp[land.length-1][i];
+            }
+        } // 맨 마지막 행 중 최대값이 답이 된다.
+
+        answer = num;
+        System.out.println(answer);
     }
 }

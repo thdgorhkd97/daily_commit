0801dbc60bc8d5b81024528dbd1cdc3118a75385
송해광 <thdgorhkd97@naver.com>
2022-07-03 22:45:19 +0900

    0703

--- src/Main.java
+++ src/Main.java
@@ -6,48 +6,58 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 02 )
-문제 : java baekjoon 11478 서로 다른 부분 문자열의 개수
-문제접근 : 입력된 string의 부분 문자열 중에서 서로 다른 문자열의 개수를 구하는 문제
+작성자 : 송해광 ( 2022 - 07 - 03 )
+문제 : java baekjoon 1699 제곱수의 합
+문제접근 : 몇 개의 제곱수로 주어진 수를 표현할 수 있는가?
  */
 
 class Main {
+
+    static int answer = 0; // 더해지는 제곱수의 개수 ( 정답 )
+    static int N = 0; // 주어진 수
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        N = Integer.parseInt(br.readLine()); // N < 100,000
 
-        Set<String> subString = new HashSet<>();
+        int[] square = new int[317]; // 316의 제곱 < 100000 < 317의 제곱
 
-        for(int i=0;i<str.length();i++){
-            String sub = "";
-
-            for(int j=i;j< str.length();j++){
-                sub += str.substring(j,j+1);
-                subString.add(sub);
-            }
+        square[0] = 1;
+        square[1] = 1;
+        for(int i=1;i<square.length-1;i++){
+            square[i+1] = (int) Math.pow(i+1,2); // square 배열은 제곱수를 저장한 배열
         }
 
-        /*
-        int length = 1;
-        while(length <= str.length()-1){
-            for(int i=0;i<=str.length()-length;i++){
-                StringBuffer sb = new StringBuffer();
-
-                for(int j=i;j<i+length;j++){
-                    sb.append(str.charAt(j));
-                }
-
-                subString.add(sb.toString());
-            }
-            length++;
+        while(N != 0) { // N이 0이 될때까지 진행
+            BinarySearch(N, square); // 시간 효율성을 위해 이분탐색으로 가장 가깝게 작은 제곱수를 구한다
+            answer++; // 횟수 + 1
         }
-        subString.add(str);
-         */
 
-        System.out.println(subString.size());
+        System.out.println(answer);
 
+    }
+
+    private static void BinarySearch(int n, int[] square) {
+        int left = 1;
+        int right = square.length-1;
+        while(Math.abs(left - right) != 1){ // 이분탐색
+
+            int mid = (left + right) / 2;
+            if(square[mid] < n){
+                left = mid+1;
+            }
+            else if(square[mid] > n){
+                right = mid -1;
+            }
+            else{ // square[mid] == n
+                N -= square[mid];
+                return ;
+            }
+
+        }
+
+        N -= square[Math.min(left,right)];
     }
 }

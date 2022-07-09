750f8278a90e639d365d93492df8d419a623934e
송해광 <thdgorhkd97@naver.com>
2022-07-09 23:59:10 +0900

    0709

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,46 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 08 )
-문제 : java baekjoon 9375 패션왕 신해빈
-문제접근 : 입을 수 있는 옷의 종류가 주어질 때 이를 조합하여 한 가지 이상의 옷을 입을 수 있는 경우의 수
- */
+작성자 : 송해광 ( 2022 - 07 - 09 )
+문제 : java baekjoon 1834 나머지와 몫이 같은 수
+문제접근 : 처음에는 그저 for문을 돌리면 될 줄알았는데 그럼 무한히 반복되서 규칙을 찾기로 했다
+*/
 
-class algorithm {
+class Main {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i=0;i<T;i++){ // 테스트 케이스의 수만큼 반복
-            int N = Integer.parseInt(br.readLine()); // 주어지는 옷의 수
-            HashMap<String,Integer> map = new HashMap<>(); // <옷 종류,수량>을 받은 hashmap
-            int clothCase = 1;
+        long answer = 0;
 
-            for(int j=0;j<N;j++){ // 입력받는 옷들 
-                String str = br.readLine();
-                String[] cloth = str.split(" "); // 옷이름 옷종류로 주어진다
-                map.put(cloth[1],map.getOrDefault(cloth[1],0)+1); // 옷 종류에 따라 수를 +1 한다
-            }
+        // N answer
+        // 1 : X
+        // 2 : 3
+        // 3 : 4,8
+        // 4 : 5, 10, 15
 
-            for(int value : map.values()){ // 각 종류별로 저장된 옷 개수
-                clothCase *= (value + 1); // 해당 옷을 안 입을 수도 있기에 +1해서 곱한다
-            }
-
-            System.out.println(clothCase - 1); // 모든 종류를 안 입는 건 불가능하므로 -1
+        for(long i=1;i<N;i++){
+            answer += (N+1) * i;
         }
-
+        System.out.println(answer);
     }
 }
+        /* 원래 시도했던 방식 -> i의 범위가 정해진 거고 확인하는 거는 모든 자연수를 다 해야한다.
+        for(int i=1;i<=2000000;i++){
+            if(checkTrue(i,N)){
+                answer += i;
+            }
+        }
+
+        System.out.println(answer);
+    }
+    private static boolean checkTrue(int number,int N){
+        if(number / N == number % N){
+            return true;
+        }
+        else return false;
+    }
+}*/

31a65b8aedf58d2c54ba7ac8446b4ae69d756be0
송해광 <thdgorhkd97@naver.com>
2022-07-12 21:45:11 +0900

    0711

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 10 )
-문제 : java baekjoon 3273 두 수의 합
-문제접근 : 배열에서 2개 수의 합이 특정 수(x)가 되는 경우의 수를 구하라.
+작성자 : 송해광 ( 2022 - 07 - 11 )
+문제 : java baekjoon 13458 시험 감독
+문제접근 : 각 시험장의 정감독1명을 포함해 부감독까지 총 몇명이 있어야 커버가능한가
 */
 
 class Main {
@@ -17,43 +17,40 @@ class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
+        int[] tester = new int[N]; // N 개의 시험장에서 시험보는 인원
+
         StringTokenizer stk = new StringTokenizer(br.readLine()," ");
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(stk.nextToken());
+        for(int i=0;i<N;i++){
+            tester[i] = Integer.parseInt(stk.nextToken());
         }
 
-        int x = Integer.parseInt(br.readLine());
+        stk = new StringTokenizer(br.readLine()," ");
 
-        int answer = 0;
+        int B = Integer.parseInt(stk.nextToken()); // 정감독이 커버가능한 시험생
+        int C = Integer.parseInt(stk.nextToken()); // 부감독이 커버가능한 시험생
 
-        /* 가장 일반적으로 생각할 수 있는 2중 for문을 활용한 해결법
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i] + arr[j] == x){
-                    answer++;
-                }
+        long answer = 0;
+        for(int i=0;i<tester.length;i++){
+
+            if(tester[i] <= B){ // 정감독이 커버가능한 인원만 있다면
+                answer++; // 감독관수 + 1
+                continue; // for 문 반복
             }
-        }
-         */
+            else{
+                answer++; // 감독관수 +1
+                tester[i] -= B; // 남아있는 인원에서 정감독이 커버가능한 인원을 제외
 
-        Arrays.sort(arr); // 투포인터로 접근하기 위해 정렬
-
-        for(int left = 0; left < n-1; left++){ // 배열의 끝 -1 까지만
-            for(int right = left+1; right<n; right++){ // 중복이 아니게 하나 다음부터
-                if(arr[left] + arr[right] == x){ // 해당 수와 같으면
-                    answer++;
-                }
-                else if(arr[left] + arr[right] > x) break;
-                // 정렬되어 있기때문에 arr[i] + arr[j]가 x를 넘으면 arr[k](k>j)와 더하는 건 무조건 x가 아니다
-                // 따라서 x를 넘는 순간 그 다음부터는 보지 않아도 괜찮다.
+                // 부감독관 감시 가능 인원으로 배열값을 나눠 나머지가 0이면 그 값을 넣고
+                if(tester[i] % C == 0) answer += tester[i]/C;
+                // 안떨어지면 나눈 몫에 +1
+                else answer += tester[i]/C + 1;
             }
+
         }
 
         System.out.println(answer);
-
 
     }
 }

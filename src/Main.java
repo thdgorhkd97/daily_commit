b9d67cb45a540a145a6ab5564b62b98d6e58d65c
송해광 <thdgorhkd97@naver.com>
2022-07-15 17:31:22 +0900

    0714

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 13 )
-문제 : java baekjoon 2822 점수 계산
-문제접근 : 높은 점수 중 5개를 처음 주어진 배열에서 몇 번째 인덱스에 있는가
-*/
+작성자 : 송해광 ( 2022 - 07 - 15 )
+문제 : java baekjoon 11931
+문제접근 : 배열을 받아서 내림차순으로 정렬
+ */
 
 class Main {
 
@@ -17,40 +17,53 @@ class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] score = new int[8];
+        int N = Integer.parseInt(br.readLine());
 
-        for(int i=0;i<score.length;i++){
-            score[i] = Integer.parseInt(br.readLine());
+        int[] number = new int[N];
+
+        for(int i=0;i<N;i++){
+            number[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] clone = score;
+        /* Integer 형 배열로 Arrays.sort 처리
+        Integer[] clone = new Integer[N];
+        for(int i=0;i<clone.length;i++){
+            clone[i] = number[i];
+        }
 
-        Arrays.sort(clone); // score 배열을 정렬(오름차순)
+        Arrays.sort(clone,Comparator.reverseOrder());
 
-//        for(int i=0;i<clone.length;i++){
-//            System.out.print(clone[i] + " ");
-//        }
-//        System.out.println();
+        for(int i=0;i<clone.length;i++){
+            System.out.println(clone[i]);
+        }
+         */
 
-        int sum = 0;
-        int[] highScore5 = new int[5]; // 높은 점수 5개를 저장할 배열
-        int idx = 0;
-        for(int i=clone.length-5;i< clone.length; i++){
-            System.out.println("clone[i] " + clone[i] );
-            sum += clone[i];
-            for(int j=0;j<score.length;j++){
-                if(score[j] == clone[i]){
-                    highScore5[idx++] = j;
-                    break;
+
+        /* 2중 for문으로 비교해가면서 더 큰수를 앞으로
+        for(int i=0;i< number.length-1;i++){
+            for(int j=i+1;j< number.length;j++){
+                if(number[i] < number[j]){
+                    int tmp = number[i];
+                    number[i] = number[j];
+                    number[j] = tmp;
                 }
             }
         }
 
-        System.out.println(sum);
-        for(int i=0;i< highScore5.length;i++){
-            System.out.print(highScore5[i] +" ");
+        for(int i=0;i<number.length;i++){
+            System.out.println(number[i]);
+        }
+         */
+
+        Arrays.sort(number);
+        for(int i=number.length-1;i>=0;i--){
+            System.out.println(number[i]);
         }
 
 
+
+
+
     }
+
 }

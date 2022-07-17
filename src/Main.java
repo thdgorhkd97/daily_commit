14b373c6276ebfb444dea8a5871cd47fc19f19ba
송해광 <thdgorhkd97@naver.com>
2022-07-17 23:50:25 +0900

    0717

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 16 )
-문제 : java baekjoon 9655 돌 게임
-문제접근 : 탁자 위에 돌 N개가 있다. 상근이와 창영이는 턴을 번갈아가면서 돌을 가져가며, 돌은 1개 또는 3개 가져갈 수 있다. 마지막 돌을 가져가는 사람이 게임을 이기게 된다.
-
+작성자 : 송해광 ( 2022 - 07 - 17 )
+문제 : java baekjoon 1032 명령 프롬프트
+문제접근 :  검색 결과가 먼저 주어졌을 때, 패턴으로 뭘 쳐야 그 결과가 나오는지를 출력하는 문제
  */
 
 class Main {
@@ -20,17 +19,33 @@ class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        boolean[] rock = new boolean[1001];
-        for(int i=1;i<=1000;i++){
-            rock[i] = false;
-            if((i-1 >= 0 && !rock[i-1]) ||  (i-3 >= 0 && !rock[i-3])){
-                rock[i] = true;
+        String[] fileName = new String[N]; // 입력받는 string들을 담을 배열
+
+        for(int i=0;i<N;i++){
+            fileName[i] = br.readLine(); // string 입력
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        for(int i=0;i<fileName[0].length();i++){ // 첫번째 STRING을 for문으로 돌면서
+            char ch = fileName[0].charAt(i); // 첫번째 string의 하나하나
+            boolean flag = true;
+            for(int j=0;j<fileName.length;j++){ // 다른 string 들의 i번째 문자를 모두 확인
+                if(fileName[j].charAt(i) != ch){ // 그 중 다른 문자가 있다면 ?로 넣어야 한다.
+                    flag = false; // 해당 위치가 모두 같지 않다
+                    break;
+                }
             }
+
+            if(flag){ // 모든 string의 해당 위치 문자가 같으면
+                sb.append(ch); // 해당 문자를 넣고
+            } // 모든 string의 해당 위치가 모두 같지 않으면
+            else sb.append("?"); // 물음표를 넣는다.
 
         }
 
-        System.out.println(rock[N] ? "SK" : "CY");
+        System.out.println(sb.toString());
+
 
     }
-
 }

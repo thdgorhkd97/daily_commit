94b416f63c64c322306e1cef0a234c0d7dbe2506
송해광 <thdgorhkd97@naver.com>
2022-07-22 20:19:19 +0900

    0721

--- src/Main.java
+++ src/Main.java
@@ -4,49 +4,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 20 )
-문제 : java baekjoon 1051 숫자 정사각형
-문제접근 : 직사각형에서 꼭짓점에 쓰여 있는 수가 모두 같은 가장 큰 정사각형을 찾는 프로그램을 작성
+작성자 : 송해광 ( 2022 - 07 - 22 )
+문제 : java baekjoon 2606 바이러스
+문제접근 : 컴퓨터끼리의 연결 정보가 주어지면 1번과 연결된 컴퓨터의 수 구하기
 */
 
 class Main {
+
+    static int[][] graph;
+    static boolean[] visited;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
 
-        int N = Integer.parseInt(stk.nextToken());
-        int M = Integer.parseInt(stk.nextToken());
+        visited = new boolean[N+1];
+        graph = new int[N+1][N+1];
 
-        char[][] rectangle = new char[N][M];
-        for(int i=0;i<N;i++){
-            rectangle[i] = br.readLine().toCharArray(); // 배열에 char로 넣고
+        int M = Integer.parseInt(br.readLine()); // 연결되어 있는 컴퓨터 쌍의 수
+
+        for(int i=0;i<M;i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+
+            int one = Integer.parseInt(stk.nextToken());
+            int two = Integer.parseInt(stk.nextToken());
+            graph[one][two] = 1;
+            graph[two][one] = 1;
         }
 
-        int min = Math.min(N,M); // N,M 중 작은 걸로
-        int maxArea = Integer.MIN_VALUE;
-        int area = 0;
+        bfs(1); // 1번과 연결된 컴퓨터의 수 구하기
 
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                for(int k=0; k<min; k++) { // 아무리 커봤자 가로,세로 중 작은 값을 넘어설 순 없다
-                    if(i+k < N && j+k < M) {	// 배열 범위 이내
+        System.out.println(answer-1);
+    }
 
-                        // 4개의 꼭짓점이 정사각형이 되는 조건
-                        if(rectangle[i][j] == rectangle[i][j+k] && rectangle[i][j] == rectangle[i+k][j] && rectangle[i][j] == rectangle[i+k][j+k]) {
-                            area = (k+1) * (k+1); // k는 인덱스이므로 + 1
-                            maxArea = Math.max(maxArea, area);
-                        }
-                    }
+    private static void bfs(int start){
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        visited[start] = true;
+
+        while(!que.isEmpty()){
+            int v = que.poll();
+            System.out.println(v);
+            answer++;
+
+
+            for(int i=1;i< graph.length;i++){
+                if(graph[v][i] == 1 && !visited[i]){
+                    que.add(i);
+                    visited[i] = true;
                 }
             }
         }
 
-        System.out.println(maxArea);
     }
 }

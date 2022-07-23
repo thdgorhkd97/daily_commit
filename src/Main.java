a4adb3d2049a50fc91437d892fa92e767beb0600
송해광 <thdgorhkd97@naver.com>
2022-07-23 23:54:13 +0900

    0723

--- src/Main.java
+++ src/Main.java
@@ -9,59 +9,92 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
-작성자 : 송해광 ( 2022 - 07 - 22 )
-문제 : java baekjoon 2606 바이러스
-문제접근 : 컴퓨터끼리의 연결 정보가 주어지면 1번과 연결된 컴퓨터의 수 구하기
+작성자 : 송해광 ( 2022 - 07 - 23 )
+문제 : java baekjoon 1743 음식물 피하기
+문제접근 : 2차원 배열에서 음식물위치가 주어지면 가장 큰 덩어리의 크기를 구하기
 */
 
 class Main {
 
-    static int[][] graph;
-    static boolean[] visited;
-    static int answer = 0;
+    static int[][] hallway; // 음식물의 위치가 주어지는 2차원 배열
+    static boolean[][] visited; // 방문처리할 2차원 배열
+    static int answer = Integer.MIN_VALUE;
+    static int[] dx = {-1,1,0,0}; // 상하좌우
+    static int[] dy = {0,0,-1,1}; // 상하좌우
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); // 컴퓨터의 수
+        StringTokenizer stk = new StringTokenizer(br.readLine()," ");
 
-        visited = new boolean[N + 1];
-        graph = new int[N + 1][N + 1];
+        int N = Integer.parseInt(stk.nextToken()); // 통로의 세로
+        int M = Integer.parseInt(stk.nextToken()); // 통로의 가로
+        int K = Integer.parseInt(stk.nextToken()); // 음식물 쓰레기의 개수
 
-        int M = Integer.parseInt(br.readLine()); // 연결되어 있는 컴퓨터 쌍의 수
+        hallway = new int[N+1][M+1];
+        visited = new boolean[N+1][M+1];
 
-        for (int i = 0; i < M; i++) {
-            StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+        for(int i=0;i<K;i++){
+            stk = new StringTokenizer(br.readLine()," ");
 
-            int one = Integer.parseInt(stk.nextToken());
-            int two = Integer.parseInt(stk.nextToken());
-            graph[one][two] = 1;
-            graph[two][one] = 1;
+            int r = Integer.parseInt(stk.nextToken()); // 음식물이 떨어진 좌표의 행
+            int c = Integer.parseInt(stk.nextToken()); // 음식물이 떨어진 좌표의 열
+
+            hallway[r][c] = 1;
+            hallway[c][r] = 1;
+
         }
 
-        bfs(1); // 1번과 연결된 컴퓨터의 수 구하기
-
-        System.out.println(answer - 1);
-    }
-
-    private static void bfs(int start) {
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
-        visited[start] = true;
-
-        while (!que.isEmpty()) {
-            int v = que.poll();
-            System.out.println(v);
-            answer++;
-
-
-            for (int i = 1; i < graph.length; i++) {
-                if (graph[v][i] == 1 && !visited[i]) {
-                    que.add(i);
-                    visited[i] = true;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(hallway[i][j]== 1 && !visited[i][j]){ // 음식물이 위치해있고 방문하지 않았다면
+                    bfs(i,j); // 해당 위치를 기준으로 BFS 시작
                 }
             }
         }
+
+        System.out.println(answer-1);
+
+
+    }
+
+    public static void bfs(int row,int column){
+        Queue<int[]> que = new LinkedList<>();
+        addque(row,column,que); // que에 row,column을 가지는 int[] 배열 넣기
+        visited[row][column] = true;
+
+        int cnt = 1;
+
+        while(!que.isEmpty()){
+            int[] v = que.poll();
+
+            for(int i=0;i<dx.length;i++){
+                int nextX = v[0] + dx[i]; // 다음 이동할 행
+                int nextY = v[1] + dy[i]; // 다음 이동할 열
+
+                if(nextX < 1 || nextY < 1 || nextX >= hallway.length || nextY >= hallway[0].length){
+                    continue;
+                } // 복도의 크기를 벗어나면 그대로 진행
+                else{
+                    if(hallway[nextX][nextY] == 1 && !visited[nextX][nextY]){
+                        addque(nextX,nextY,que);
+                        visited[nextX][nextY] = true;
+                        cnt++;
+                    }
+                }
+            }
+        }
+
+        answer = Math.max(answer,cnt);
+
+
+    }
+
+    public static void addque(int row,int column,Queue<int[]> que){
+        int[] add = new int[2];
+        add[0] = row;
+        add[1] = column;
+        que.add(add);
     }
 }
